cfb4fa2f781dc2c7cabfceb3d0fb618098e22c81
Arpad Ryszka <arpad.ryszka@zalando.de>
2015-06-10 14:37:41 +0200

    add etcd settings types, mock them

--- src/skipper/etcd.go
+++ src/skipper/etcd.go
@@ -1,19 +1,61 @@
 package main
 
+type endpointType string
+
+const (
+    ephttp endpointType = "http"
+    ephttps endpointType = "https"
+)
+
+type server struct {
+    url string
+}
+
+type backend struct {
+    typ endpointType
+    servers []server
+}
+
+type frontend struct {
+    typ endpointType
+    backendId string
+    route string
+    middleware []string
+}
+
+type middleware struct {
+    id string
+    priority int
+    typ string // ???
+    args interface{}
+}
+
 type settings interface {
-    url() string
+    getBackends() []backend
+    getFrontends() []frontend
+    getMiddleware() []middleware
 }
 
 type settingsStruct struct {
-    u string
+    backends []backend
+    frontends []frontend
+    middleware []middleware
 }
 
 type etcdClient struct {
     settings chan settings
 }
 
-func (s *settingsStruct) url() string {
-    return s.u
+func (s *settingsStruct) getBackends() []backend {
+    return s.backends
+}
+
+func (s *settingsStruct) getFrontends() []frontend {
+    return s.frontends
+}
+
+func (s *settingsStruct) getMiddleware() []middleware {
+    return s.middleware
 }
 
 func makeEtcdClient() *etcdClient {
@@ -21,12 +63,23 @@ func makeEtcdClient() *etcdClient {
 }
 
 func (ec *etcdClient) feedSettings() {
-    s := &settingsStruct{"http://localhost:9999/slow"}
+    testSettings := &settingsStruct{
+        backends: []backend{
+            backend{
+                typ: ephttp,
+                servers: []server{
+                    server{url: "http://localhost:9999/slow"}}}},
+        frontends: []frontend{},
+        middleware: []middleware{}}
     for {
-        ec.settings <- s
+        ec.settings <- testSettings
     }
 }
 
 func (ec *etcdClient) start() {
     go ec.feedSettings()
+}
+
+func (ec *etcdClient) getSettings() <-chan settings {
+    return ec.settings
 }
--- src/skipper/proxy.go
+++ src/skipper/proxy.go
@@ -1,10 +1,12 @@
 package main
 
+import "fmt"
 import "io"
 import "log"
 import "net/http"
 
-const bufferSize = 8192;
+const proxyBufferSize = 8192;
+const proxyErrorFmt = "proxy: %s"
 
 type flusherWriter interface {
     http.Flusher
@@ -13,6 +15,10 @@ type flusherWriter interface {
 
 type proxy struct {
     etcd *etcdClient
+}
+
+func proxyError(m string) error {
+    return fmt.Errorf(proxyErrorFmt, m)
 }
 
 func copyHeader(to, from http.Header) {
@@ -29,7 +35,7 @@ func cloneHeader(h http.Header) http.Header {
 
 func copyStream(to flusherWriter, from io.Reader) error {
     for {
-        b := make([]byte, bufferSize)
+        b := make([]byte, proxyBufferSize)
 
         l, err := from.Read(b)
         if err != nil {
@@ -49,17 +55,33 @@ func copyStream(to flusherWriter, from io.Reader) error {
     }
 }
 
+func mapRequest(r *http.Request, s settings) (*http.Request, error) {
+    if s == nil {
+        return nil, proxyError("missing settings")
+    }
+
+    if len(s.getBackends()) == 0 {
+        return nil, proxyError("missing backend settings")
+    }
+
+    b := s.getBackends()[0]
+    if len(b.servers) == 0 {
+        return nil, proxyError("missing backend server settings")
+    }
+
+    return http.NewRequest(r.Method, b.servers[0].url, r.Body)
+}
+
 func (p *proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
     hterr := func (err error) {
         http.Error(w, http.StatusText(404), 404)
         log.Println(err)
     }
 
-    settings := <-p.etcd.settings
-
-    rr, err := http.NewRequest("GET", settings.url(), r.Body)
+    rr, err := mapRequest(r, <-p.etcd.getSettings())
     if err != nil {
         hterr(err)
+        return
     }
 
     rr.Header = cloneHeader(r.Header)
@@ -81,4 +103,5 @@ func (p *proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
     copyHeader(w.Header(), rs.Header)
     w.WriteHeader(rs.StatusCode)
     copyStream(w.(flusherWriter), rs.Body)
+
 }
