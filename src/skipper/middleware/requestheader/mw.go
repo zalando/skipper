93d2fc7bcc34cb1f118b6f6ebf77c8b453f03956
Arpad Ryszka <arpad.ryszka@zalando.de>
2015-06-24 22:08:33 +0200

    add code documentation

--- src/skipper/middleware/requestheader/mw.go
+++ src/skipper/middleware/requestheader/mw.go
@@ -1,3 +1,7 @@
+// middleware to set a preconfigured header for a request.
+// the name of the header is expected in the 'key' field of the filter config, and the value of the header in the
+// 'value' field.
+// if the header key is called 'Host', it sets the request object's Host field, too.
 package requestheader
 
 import (
@@ -11,14 +15,17 @@ type impl struct {
 	simpleheader.Type
 }
 
+// creates the middleware instance
 func Make() skipper.Middleware {
 	return &impl{}
 }
 
+// returns the name of the middleware
 func (mw *impl) Name() string {
 	return name
 }
 
+// creates a filter instance with the provided header key and value in the filter config.
 func (mw *impl) MakeFilter(id string, config skipper.MiddlewareConfig) (skipper.Filter, error) {
 	f := &impl{}
 	err := f.InitFilter(id, config)
@@ -29,6 +36,7 @@ func (mw *impl) MakeFilter(id string, config skipper.MiddlewareConfig) (skipper.
 	return f, nil
 }
 
+// sets the configured header for the request
 func (f *impl) Request(ctx skipper.FilterContext) {
 	req := ctx.Request()
 	if f.Key() == "Host" {
