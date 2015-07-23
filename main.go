0c1e67d0ff3f9df83c998e0b97d347f688c7d163
Arpad Ryszka <arpad.ryszka@zalando.de>
2015-07-23 12:46:01 +0200

    remove connection pooling for benchmarking purpose

--- main.go
+++ main.go
@@ -1,14 +1,6 @@
 // skipper program main
 //
-// command line flags:
-//
-// -etcd-urls:
-// one or more urls where the etcd service to be used can be found
-//
-// -insecure:
-// if set, skipper accepts invalid certificates from backend hosts
-//
-// (see the skipper package for an overview of the program structure)
+// for a summary about skipper, please see the readme file.
 package main
 
 import (
@@ -21,24 +13,28 @@ import (
 const (
 	defaultAddress  = ":9090"
 	defaultEtcdUrls = "http://127.0.0.1:2379,http://127.0.0.1:4001"
+    defaultStorageRoot = "/skipper"
 	addressUsage    = "address where skipper should listen on"
 	etcdUrlsUsage   = "urls where etcd can be found"
 	insecureUsage   = "set this flag to allow invalid certificates for tls connections"
+    storageRootUsage = "prefix for skipper related data in the provided etcd storage"
 )
 
 var (
 	address  string
 	etcdUrls string
 	insecure bool
+    storageRoot string
 )
 
 func init() {
 	flag.StringVar(&address, "address", defaultAddress, addressUsage)
 	flag.StringVar(&etcdUrls, "etcd-urls", defaultEtcdUrls, etcdUrlsUsage)
 	flag.BoolVar(&insecure, "insecure", false, insecureUsage)
+    flag.StringVar(&storageRoot, "storage-root", defaultStorageRoot, storageRootUsage)
 	flag.Parse()
 }
 
 func main() {
-	log.Fatal(run.Run(address, strings.Split(etcdUrls, ","), insecure))
+	log.Fatal(run.Run(address, strings.Split(etcdUrls, ","), storageRoot, insecure))
 }
