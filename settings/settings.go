5c4b816d5630533bd514882a5dff8a0830874283
Arpad Ryszka <arpad.ryszka@gmail.com>
2015-09-06 15:07:25 +0200

    cleanup request matcher (router)

--- settings/settings.go
+++ settings/settings.go
@@ -2,10 +2,13 @@ package settings
 
 import (
 	"errors"
+	"fmt"
 	"github.com/zalando/eskip"
-	"github.com/zalando/skipper/routematcher"
+	"github.com/zalando/skipper/requestmatch"
 	"github.com/zalando/skipper/skipper"
+	"log"
 	"net/http"
+	"net/url"
 )
 
 type backend struct {
@@ -29,7 +32,92 @@ type routedef struct {
 }
 
 type settings struct {
-	matcher *routematcher.Matcher
+	matcher *requestmatch.Matcher
+}
+
+func makeBackend(r *eskip.Route) (*backend, error) {
+	if r.Shunt {
+		return &backend{isShunt: true}, nil
+	}
+
+	bu, err := url.ParseRequestURI(r.Backend)
+	if err != nil {
+		return nil, err
+	}
+
+	return &backend{scheme: bu.Scheme, host: bu.Host}, nil
+}
+
+func makeFilterId(routeId, filterName string, index int) string {
+	return fmt.Sprintf("%s-%s-%d", routeId, filterName, index)
+}
+
+func makeFilter(id string, ref *eskip.Filter, fr skipper.FilterRegistry) (skipper.Filter, error) {
+	spec := fr.Get(ref.Name)
+	if spec == nil {
+		return nil, errors.New(fmt.Sprintf("filter not found: '%s' '%s'", id, ref.Name))
+	}
+
+	return spec.MakeFilter(id, skipper.FilterConfig(ref.Args))
+}
+
+func makeFilters(r *eskip.Route, fr skipper.FilterRegistry) ([]skipper.Filter, error) {
+	fs := make([]skipper.Filter, len(r.Filters))
+	for i, fspec := range r.Filters {
+		f, err := makeFilter(makeFilterId(r.Id, fspec.Name, i), fspec, fr)
+		if err != nil {
+			return nil, err
+		}
+
+		fs[i] = f
+	}
+
+	return fs, nil
+}
+
+func makeRouteDefinition(r *eskip.Route, fr skipper.FilterRegistry) (*routedef, error) {
+	b, err := makeBackend(r)
+	if err != nil {
+		return nil, err
+	}
+
+	fs, err := makeFilters(r, fr)
+	if err != nil {
+		return nil, err
+	}
+
+	rt := &route{b, fs}
+	return &routedef{r, rt}, nil
+}
+
+func makeMatcher(routes []*eskip.Route, fr skipper.FilterRegistry, ignoreTrailingSlash bool) *requestmatch.Matcher {
+	routeDefinitions := make([]requestmatch.Definition, len(routes))
+	for i, r := range routes {
+		rd, err := makeRouteDefinition(r, fr)
+		if err != nil {
+			log.Println(err)
+		}
+
+		routeDefinitions[i] = rd
+	}
+
+	router, errs := requestmatch.Make(routeDefinitions, ignoreTrailingSlash)
+	for _, err := range errs {
+		log.Println(err)
+	}
+
+	return router
+}
+
+func processRaw(rd skipper.RawData, fr skipper.FilterRegistry, ignoreTrailingSlash bool) (skipper.Settings, error) {
+	d, err := eskip.Parse(rd.Get())
+	if err != nil {
+		return nil, err
+	}
+
+	matcher := makeMatcher(d, fr, ignoreTrailingSlash)
+	s := &settings{matcher}
+	return s, nil
 }
 
 func (b *backend) Scheme() string { return b.scheme }
