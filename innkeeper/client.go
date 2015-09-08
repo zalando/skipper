96c574f8b57bcf7cb678d5471657cd54a5eb41a0
Dan Persa <dan.persa@zalando.de>
2015-09-08 16:03:49 +0200

    My first go commit :P

--- innkeeper/client.go
+++ innkeeper/client.go
@@ -10,7 +10,13 @@ import (
 	"net/http"
 	"strings"
 	"time"
+	"crypto/tls"
+	"io"
+	"errors"
 )
+
+
+const authFailedMessage = "Authentication failed"
 
 // serialization object for innkeeper data
 //
@@ -22,12 +28,17 @@ type routeData struct {
 	Route   string `json:"route"`
 }
 
+type apiError struct {
+	Message string `json:"message"`
+}
+
 type routeDoc string
 
 type client struct {
 	pollUrl    string
 	httpClient *http.Client
 	dataChan   chan skipper.RawData
+	authToken  string
 
 	// store the routes for comparison during
 	// the subsequent polls
@@ -36,10 +47,12 @@ type client struct {
 
 // Creates an innkeeper client.
 func Make(pollUrl string, pollTimeout time.Duration) skipper.DataClient {
+
+	tr := &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}}
 	c := &client{
 		pollUrl,
-		&http.Client{},
-		make(chan skipper.RawData),
+		&http.Client{Transport: tr},
+		make(chan skipper.RawData),"",
 		make(map[int64]string)}
 
 	// start a polling loop
@@ -56,14 +69,61 @@ func Make(pollUrl string, pollTimeout time.Duration) skipper.DataClient {
 	return c
 }
 
+func parseApiError(r io.Reader) (string, error) {
+	message, err := ioutil.ReadAll(r)
+
+	if err != nil {
+		return "", err
+	}
+
+	ae := apiError{}
+	if err := json.Unmarshal(message, &ae); err != nil {
+		return "", err
+	}
+
+	return ae.Message, nil
+}
+
+func (c *client) authenticate() error {
+	c.authToken = "99daaa44-2e63-4bb1-a9e1-47099ca6c930"
+	return nil
+}
+
+
 // makes a request to innkeeper for the latest data
-func (c *client) getData() ([]*routeData, error) {
-	response, err := c.httpClient.Get(c.pollUrl)
+func (c *client) getData(retry bool) ([]*routeData, error) {
+
+	req, err := http.NewRequest("GET", c.pollUrl, nil)
+
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Authorization", c.authToken)
+
+	response, err := c.httpClient.Do(req)
 	if err != nil {
 		return nil, err
 	}
 
 	defer response.Body.Close()
+
+	if response.StatusCode == 401 {
+		message, err := parseApiError(response.Body)
+
+		if err != nil {
+			return nil, err
+		}
+
+		if message == authFailedMessage && retry {
+			err := c.authenticate()
+			if err != nil {
+				return nil, err
+			}
+			return c.getData(false)
+		}
+
+		return nil, errors.New("unknown auth error")
+	}
 
 	if response.StatusCode >= 400 {
 		return nil, fmt.Errorf("failed to receive data: %s", response.Status)
@@ -73,6 +133,8 @@ func (c *client) getData() ([]*routeData, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	println(string(data))
 
 	var parsed []*routeData
 	err = json.Unmarshal(data, &parsed)
@@ -106,7 +168,7 @@ func updateDoc(doc map[int64]string, data []*routeData) bool {
 // if there were any changes. If yes, then returns
 // true, otherwise false.
 func (c *client) poll() bool {
-	data, err := c.getData()
+	data, err := c.getData(true)
 	if err == nil {
 		return updateDoc(c.doc, data)
 	} else {
