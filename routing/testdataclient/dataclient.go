892d8ebb4a3287365b737cf473dc5bc93ddae447
Arpad Ryszka <arpad.ryszka@zalando.de>
2015-10-06 20:00:45 +0200

    simplify merging data sources

--- routing/testdataclient/dataclient.go
+++ routing/testdataclient/dataclient.go
@@ -6,66 +6,13 @@ import (
     "log"
 )
 
-type feed struct {
-    insert string
-    del []string
-    reset bool
+type T struct {}
+
+func New() DataClient {
 }
 
-type TestDataClient struct {
-    routes chan []*eskip.Route
-    updates chan *routing.DataUpdate
-    feed chan feed
-}
+func (c *T) GetInitial() ([]*eskip.Route, error) {}
 
-func New(data string) *TestDataClient {
-    dc := &TestDataClient{
-        routes: make(chan []*eskip.Route),
-        updates: make(chan *routing.DataUpdate),
-        feed: make(chan feed)}
-
-    go func () {
-        routes := make(map[string]*eskip.Route)
-        var routeList []*eskip.Route
-        receive: for {
-            select {
-            case dc.routes <- routeList:
-            case feed := <-dc.feed:
-                newRoutes, err := eskip.Parse(feed.insert)
-                if err != nil {
-                    log.Println(err)
-                    continue receive
-                }
-
-                for _, id := range feed.del {
-                    delete(routes, id)
-                }
-
-                for _, r := range newRoutes {
-                    routes[r.Id] = r
-                }
-
-                routeList = make([]*eskip.Route, len(routes))
-                i := 0
-                for _, r := range routes {
-                    routeList[i] = r
-                    i++
-                }
-
-                dc.updates <- &routing.DataUpdate{newRoutes, feed.del, feed.reset}
-            }
-        }
-    }()
-
-    dc.Feed(data, nil, false)
-    <-dc.updates
-    return dc
-}
-
-func (dc *TestDataClient) Receive() ([]*eskip.Route, <-chan *routing.DataUpdate) {
-    return <-dc.routes, dc.updates
-}
-
-func (dc *TestDataClient) Feed(insertDoc string, deleteIds []string, reset bool) {
-    dc.feed <- feed{insertDoc, deleteIds, reset}
-}
+func (c *T) GetUpdate() ([]*eskip.Route, []string, error) {}
+func (c *T) Update(upsert []*eskip.Route, deleteIds []string) {}
+func (c *T) FailNext() {}
