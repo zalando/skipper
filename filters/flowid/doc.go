58ae0d8ebdb092a62aced239c9d6d9ece77768d7
lmineiro <luis.mineiro@zalando.de>
2015-10-16 14:04:15 +0200

    Converted documentation to godoc and fixed the regex MustCompile

--- filters/flowid/doc.go
+++ filters/flowid/doc.go
@@ -1,45 +1,49 @@
-# Flow ID Filter
-
+/*
 Flow Ids let you correlate router logs for a given request against the upstream application logs for that same request.
 If your upstream application makes other requests to other services it can provide the same Flow Id value so that all
 of those logs can be correlated.
 
-## How it works
+How it works
 
-Skipper generates a unique Flow ID for every HTTP request that it receives. The Flow ID is then passed to your 
-upstream application as an HTTP header called `X-Flow-Id`.
+Skipper generates a unique Flow Id for every HTTP request that it receives. The Flow ID is then passed to your
+upstream application as an HTTP header called X-Flow-Id.
 
 The filter takes 2 optional parameters:
+	1. Accept existing X-Flow-Id header
+	2. Flow Id length
 
-1. Accept existing `X-Flow-Id` header
-2. Flow Id length
-	 
 The first parameter is a boolean parameter that, when set to true, will make the filter skip the generation of
 a new flow id. If the existing header value is not a valid flow id it is ignored and a new flow id is also generated.
 
 The second parameter is a number that defines the length of the generated flow ids. Valid options are any even number
 between 8 and 254.
 
-## Usage
+Usage
 
-The filter can be used with many different combinations of parameters. It can also be used without any parameter, since 
+The filter can be used with many different combinations of parameters. It can also be used without any parameter, since
 both are options.
 
-### Default parameters
-	FlowId()
-Without any parameters, the filter doesn't reuse existing `X-Flow-Id` headers and generates new ones with 16 bytes.
+Default parameters
 
-### Reuse existing flow id
+	FlowId()
+
+Without any parameters, the filter doesn't reuse existing X-Flow-Id headers and generates new ones with 16 bytes.
+
+Reuse existing flow id
+
 	FlowId(true)
-With only the first parameter with the boolean value `true` the filter will accept existing `X-Flow-Id` headers, if 
+
+With only the first parameter with the boolean value `true` the filter will accept existing X-Flow-Id headers, if
 they're present in the request.
 
-### Generate bigger flow ids
+Generate bigger flow ids
+
 	FlowId(false, 64)
-This example doesn't accept existing `X-Flow-Id` headers and will always generate new flow ids with 64 bytes.
+
+This example doesn't accept existing X-Flow-Id headers and will always generate new flow ids with 64 bytes.
 
 
-## Some benchmarks
+Some benchmarks
 
 To decide upon which hashing mechanism to use we tested some versions of UUID v1 - v4 and some other implementations.
 The results are as follow:
@@ -52,8 +56,8 @@ The results are as follow:
 	BenchmarkSha1-4 	            1000000	      2188 ns/op
 	BenchmarkMd5-4  	            1000000	      2076 ns/op
 	BenchmarkFnv-4  	            500000	      2223 ns/op
-	
-The current implementation just gets len / 2 (hex.DecodedLen) bytes from the PRNG and hex encodes them. 
+
+The current implementation just gets len / 2 (hex.DecodedLen) bytes from the PRNG and hex encodes them.
 Its performance is only dependent on the length of the generated FlowId, according to the following benchmarks:
 
 	BenchmarkFlowIdLen8-4 	 1000000	      1157 ns/op
@@ -64,4 +68,6 @@ Its performance is only dependent on the length of the generated FlowId, accordi
 	BenchmarkFlowIdLen32-4	 1000000	      1957 ns/op
 	BenchmarkFlowIdLen64-4	  300000	      3520 ns/op
 
-As you can see, starting at len = 32 (16 random bytes) the performance starts dropping dramatically. 
+Starting at len = 32 (16 random bytes) the performance starts dropping dramatically.
+*/
+package flowid
