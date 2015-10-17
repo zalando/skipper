36edcc734a606cd896ad02bb10f17329365a072c
lmineiro <luis.mineiro@zalando.de>
2015-10-17 23:41:05 +0200

    Fixed comment for the newFlowId() function

--- filters/flowid/hashing.go
+++ filters/flowid/hashing.go
@@ -22,8 +22,8 @@ var (
 // newFlowId returns a random flowId using the flowIdAlphabet with length l
 // The alphabet is limited to 64 elements and requires a random 6 bit value to index any of them
 // The cost to rnd.IntXX is not very relevant but the bit shifting operations are faster
-// For this reason a single call to rnd.Int63 is used and its bits are mapped up to 8 chunks of 8 bits each
-// A final right shift truncates the extra unnecessary bits
+// For this reason a single call to rnd.Int63 is used and its bits are mapped up to 10 chunks of 6 bits each
+// The byte data type carries 2 additional bits for the next chunk which are cleared with the alphabet bit mask
 func newFlowId(l int) (string, error) {
 	if l < minLength || l > maxLength {
 		return "", ErrInvalidLen
