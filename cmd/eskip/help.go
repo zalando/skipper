fa0f810765c77a5109bfa6eeb56e342c89eaa257
Arpad Ryszka <arpad.ryszka@gmail.com>
2015-10-26 01:29:06 +0100

    eskip tool: arg parsing, checking, printing

---
+++ cmd/eskip/help.go
@@ -0,0 +1,20 @@
+package main
+
+import "os"
+
+const helpHint = "To print eskip usage, enter:\n\neskip help"
+
+func hint() {
+	printStderr(helpHint)
+}
+
+func usage() {
+	flags.SetOutput(os.Stderr)
+	flags.PrintDefaults()
+	flags.SetOutput(nowrite)
+}
+
+func helpCmd(in, out *medium) error {
+	usage()
+	return nil
+}
