508179e2e677ace0236d870cc4b9b6dce3faee0d
Arpad Ryszka <arpad.ryszka@zalando.de>
2015-10-27 18:31:45 +0100

    review fixes

--- proxy/example_test.go
+++ proxy/example_test.go
@@ -16,6 +16,7 @@ package proxy_test
 
 import (
 	"fmt"
+	"github.com/zalando/skipper/eskip"
 	"github.com/zalando/skipper/filters"
 	"github.com/zalando/skipper/filters/builtin"
 	"github.com/zalando/skipper/proxy"
@@ -83,4 +84,35 @@ func Example() {
 
 	// Output:
 	// Hello, world!
+}
+
+type priorityRoute struct{}
+
+func (p *priorityRoute) Match(request *http.Request) (*routing.Route, map[string]string) {
+	if request.URL.Path != "/disabled-page" {
+		return nil, nil
+	}
+
+	return &routing.Route{Route: eskip.Route{Shunt: true}}, nil
+}
+
+func ExamplePriorityRoute() {
+	// create a routing doc forwarding all requests,
+	// and load it in a data client:
+	routeDoc := `Any() -> "https://www.example.org"`
+	dataClient, err := testdataclient.NewDoc(routeDoc)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// create a priority route making exceptions:
+	pr := &priorityRoute{}
+
+	// create an http.Handler:
+	proxy.New(
+		routing.New(routing.Options{
+			FilterRegistry: builtin.MakeRegistry(),
+			DataClients:    []routing.DataClient{dataClient}}),
+		false,
+		pr)
 }
