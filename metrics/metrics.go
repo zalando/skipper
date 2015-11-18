5fc7638d78551253610277506f8d28e4341947f8
Arpad Ryszka <arpad.ryszka@zalando.de>
2015-11-18 14:27:53 +0100

    move metrics doc to separate file, remove outdated description

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -1,23 +1,3 @@
-/*
-Package metrics implements collection of common performance metrics.
-
-It uses the Go implementation of the Coda Hale metrics library:
-
-https://github.com/dropwizard/metrics
-
-The collected metrics include the total request processing time, the
-time of looking up routes, the time spent with processing all filters
-and every single filter, the time waiting for the response from the
-backend services, and the time spent with forwarding the response to the
-client.
-
-For the keys used for the different metrics, please, see the Key*
-variables.
-
-To enable metrics, it needs to be initialized with a Listener address.
-In this case, Skipper will start an additional http listener, where the
-current metrics values can be downloaded.
-*/
 package metrics
 
 import (
