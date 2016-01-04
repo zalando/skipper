1cea850d3279e93dfc1071ca8d41b52f7a902894
Dan Persa <dan.persa@gmail.com>
2016-01-04 13:46:52 +0100

    Remove code duplication

--- cmd/eskip/writeclient.go
+++ cmd/eskip/writeclient.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"github.com/zalando/skipper/eskip"
 	etcdclient "github.com/zalando/skipper/etcd"
-	innkeeperclient "github.com/zalando/skipper/innkeeper"
 )
 
 type writeClient interface {
@@ -23,17 +22,7 @@ func createWriteClient(out *medium) (writeClient, error) {
 
 	switch out.typ {
 	case innkeeper:
-		auth := innkeeperclient.CreateInnkeeperAuthentication(innkeeperclient.AuthOptions{InnkeeperAuthToken: out.oauthToken})
-
-		ic, err := innkeeperclient.New(innkeeperclient.Options{
-			Address:        out.urls[0].String(),
-			Insecure:       false,
-			Authentication: auth})
-
-		if err != nil {
-			return nil, err
-		}
-		return ic, nil
+		return createInnkeeperClient(out)
 	case etcd:
 		client := etcdclient.New(urlsToStrings(out.urls), out.path)
 		return client, nil
