1fc88d24a32fb7548e151126340853a613453d73
Moritz Grauel <mo@notadomain.com>
2016-02-10 15:00:24 +0100

    added testcases for IPv6

--- predicates/source/source_test.go
+++ predicates/source/source_test.go
@@ -23,20 +23,28 @@ func TestCreate(t *testing.T) {
 		[]interface{}{"127.0.0.1/32", 1},
 		true,
 	}, {
-		"arg 1 not CIDR",
+		"arg 1 not netmask",
 		[]interface{}{"all the things"},
 		true,
 	}, {
-		"one valid CIDR",
+		"one valid netmask",
 		[]interface{}{"1.2.3.4/32"},
 		false,
 	}, {
-		"two valid CIDR",
+		"two valid netmasks",
 		[]interface{}{"1.2.3.4/32", "1.2.3.4/32"},
 		false,
 	}, {
 		"no net mask should default to /32",
 		[]interface{}{"1.2.3.4"},
+		false,
+	}, {
+		"should handle IPv6 addresses",
+		[]interface{}{"C0:FF::EE"},
+		false,
+	}, {
+		"should handle IPv6 with mask",
+		[]interface{}{"C0:FF::EE/32"},
 		false,
 	}} {
 		_, err := (&spec{}).Create(ti.args)
@@ -92,6 +100,21 @@ func TestMatching(t *testing.T) {
 		[]interface{}{"8.8.8.8"},
 		&http.Request{RemoteAddr: "127.0.0.1", Header: http.Header{"X-Forwarded-For": []string{"8.8.8.8", "7.7.7.7", "6.6.6.6"}}},
 		true,
+	}, {
+		"should work for IPv6",
+		[]interface{}{"C0:FF::EE"},
+		&http.Request{RemoteAddr: "C0:FF::EE"},
+		true,
+	}, {
+		"should work for IPv6 with mask - pass",
+		[]interface{}{"C0:FF::EE/127"},
+		&http.Request{RemoteAddr: "C0:FF::EF"},
+		true,
+	}, {
+		"should work for IPv6 with mask - reject",
+		[]interface{}{"C0:FF::EE/127"},
+		&http.Request{RemoteAddr: "C0:FF::EC"},
+		false,
 	}} {
 		pred, err := (&spec{}).Create(ti.args)
 		if err != nil {
