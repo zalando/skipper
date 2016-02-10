1188617072346ddb7315f81b2a9448a5a43a3346
Moritz Grauel <mo@notadomain.com>
2016-02-10 14:49:05 +0100

    First draft of a source predicate
    This is a first draft of a source predicate, that enables routing
    decisions based on the source of a request.
    
    Right now it is build (and registered) as a custom predicate. We could
    discuss adding it to the built in predicates or leaving it like that as
    a nice and simple example of how to add custom predicates to skipper.
    
    Also, the test cases might not be complete. I'm open to suggestions for
    more test cases/corner cases, that are not yet tested.

---
+++ util/net_test.go
@@ -0,0 +1,46 @@
+package netutils
+
+import (
+	"net"
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+var netTests = []struct {
+	input  string
+	want   net.IP
+	fwdHdr string
+}{
+	{"127.0.0.1", net.IPv4(127, 0, 0, 1), ""},
+	{"1.2.3.4", net.IPv4(1, 2, 3, 4), ""},
+	{"100.200.300.400", nil, ""},
+	{"127.0.0.1:8080", net.IPv4(127, 0, 0, 1), ""},
+	{"127.0.0.1", net.IPv4(172, 16, 0, 1), "172.16.0.1"},
+	{"127.0.0.1", net.IPv4(127, 0, 0, 1), "invalid header"},
+	{"127.0.0.1", net.IPv4(172, 16, 0, 1), "172.16.0.1, 1.2.3.4, 8.7.6.5"}, // X-Forwarded-For with proxies in it
+	{"2001:4860:0:2001::68", net.ParseIP("2001:4860:0:2001::68"), ""},
+	{"127.0.0.1", net.ParseIP("2001:4860:0:2001::68"), "2001:4860:0:2001::68"},
+}
+
+func TestRemoteHost(t *testing.T) {
+	for _, test := range netTests {
+		r := &http.Request{RemoteAddr: test.input, Header: make(http.Header)}
+		if test.fwdHdr != "" {
+			r.Header.Set("x-forwarded-for", test.fwdHdr)
+		}
+		got := RemoteHost(r)
+
+		if !reflect.DeepEqual(got, test.want) {
+			t.Errorf("Unexpected IP address '%v'. Wanted '%v", got, test.want)
+		}
+	}
+}
+
+func BenchmarkRemoteHost(b *testing.B) {
+	r := &http.Request{RemoteAddr: "1.2.3.4"}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		RemoteHost(r)
+	}
+}
