edbbcb0668142f4a76d9e80c503213eeff3ed83e
Evgeniy Sokolov <ewgraf@gmail.com>
2016-03-10 15:18:38 +0100

    when timeout - rotate to next checkpoint

--- etcd/etcd.go
+++ etcd/etcd.go
@@ -204,7 +204,11 @@ func (c *Client) tryEndpoints(mreq func(string) (*http.Request, error)) (*http.R
 		}
 
 		if err == nil || isTimeoutError {
-			if index != 0 {
+			if (isTimeoutError) {
+				index++
+			}
+
+			if index != 0 && index != len(c.endcheckpoints) {
 				c.endpoints = append(c.endpoints[index:], c.endpoints[:index]...)
 			}
 
