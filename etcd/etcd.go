1d336bbc5724517757ea231346036b6c1a0a420c
Evgeniy Sokolov <ewgraf@gmail.com>
2016-03-14 12:31:37 +0100

    tryenpoints optimization - drop never true check

--- etcd/etcd.go
+++ etcd/etcd.go
@@ -204,7 +204,7 @@ func (c *Client) tryEndpoints(mreq func(string) (*http.Request, error)) (*http.R
 		}
 
 		if err == nil || isTimeoutError {
-			if index != 0 && index != len(c.endpoints) {
+			if index != 0 {
 				c.endpoints = append(c.endpoints[index:], c.endpoints[:index]...)
 			}
 
