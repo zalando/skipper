1fb740f20a207e66699837d615ee96b6874f2f54
Arpad Ryszka <arpad.ryszka@zalando.de>
2016-06-01 18:04:21 +0200

    generate lorem ipsum html

--- filters/builtin/testing/benchmark-compress.sh
+++ filters/builtin/testing/benchmark-compress.sh
@@ -18,6 +18,30 @@ cd $cwd
 go install github.com/zalando/skipper/...
 if [ $? -ne 0 ]; then exit -1; fi
 
+loremHead='<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Lorem</title>
+	</head>
+	<body>'
+
+loremP='
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+		et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+		aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+		dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+		officia deserunt mollit anim id est laborum.</p>'
+loremTail='
+	</body>
+</html>'
+
+lorem() {
+	echo -n $loremHead > "$cwd"/lorem.html
+	for i in $(seq 1 360); do echo -n $loremP >> "$cwd"/lorem.html; done
+	echo -n $loremTail >> "$cwd"/lorem.html
+}
+
 pids=
 
 cleanup() {
@@ -47,18 +71,22 @@ bench() {
 
 trap cleanup SIGINT
 
-echo [starting servers]
+echo [generating content]
+lorem
+echo [content generated]
+
+echo; echo [starting servers]
 skp :9990 static.eskip
 ngx
 skp :9090 proxy.eskip
 echo [servers started, wait 1 sec]
 sleep 1
 
-# echo; echo '[warmup]'
+echo; echo '[warmup]'
 warmup :9990
 warmup :9080
 warmup :9090
-# echo '[warmup done]'
+echo '[warmup done]'
 
 echo; echo '[benchmarking nginx]'
 bench :9080
