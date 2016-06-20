4224e60a1731d52e0e0ed5db4f8183605e8753f8
Arpad Ryszka <arpad.ryszka@zalando.de>
2016-06-20 14:42:26 +0200

    add innkeeper test script

---
+++ skptesting/ikc.sh
@@ -0,0 +1,84 @@
+#! /bin/bash
+
+log() {
+	echo $@ >&2
+}
+
+cmd=routes
+if [ -n "$1" ]; then cmd=$1; fi
+shift
+
+flagCount=0
+while getopts "h:s:n:p:" arg; do
+	eval $arg="$OPTARG"
+	flagCount=$(expr $flagCount + 2)
+done
+if [ $flagCount -gt 0 ]; then
+	for i in {1..$flagCount}; do shift; done
+fi
+host=${h:-http://localhost:9080}
+select=${s:-.}
+
+auth=Authorization:\ Bearer\ token-user~1-employees-route
+ctype=Content-Type:\ application/json
+
+now() {
+	date -u +%Y-%m-%dT%H:%M:%S
+}
+
+get() {
+	curl -sH "$auth".read "$host"/"$1"
+}
+
+put() {
+	curl -sH "$auth".write "$host"/"$1" -H Content-Type:\ application/json -d "$2"
+}
+
+routes() {
+	get routes
+}
+
+paths() {
+	get paths
+}
+
+hosts() {
+	get hosts
+}
+
+hostids() {
+	hosts | jq --compact-output '[.[] | .id]'
+}
+
+mkpath() {
+	put paths '{"uri": "'$1'", "host_ids": '"$(hostids)"'}'
+}
+
+mkroute() {
+	name="$n"
+	if [ -z "$name" ]; then
+		log missing name
+		exit 1
+	fi
+
+	path="$p"
+	if [ -z "$path" ]; then
+		log missing path
+		exit 1
+	fi
+
+	predicates="$1"
+	if [ -z "$predicates" ]; then
+		predicates='[]'
+	fi
+
+	put routes '{
+		"name": "'$name'",
+		"activate_at": "'$(now)'",
+		"path_id": '$path',
+		"uses_common_filters": true,
+		"route": {}
+	}'
+}
+
+$cmd $@ | jq --monochrome-output $select
