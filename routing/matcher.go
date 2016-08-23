ccc0b2ecfa5cde0d65a8eb84f391d59703f78bf1
Arpad Ryszka <arpad@squareroundforest.org>
2016-08-23 23:27:57 +0200

    allow negative priority

--- routing/matcher.go
+++ routing/matcher.go
@@ -1,17 +1,3 @@
-// Copyright 2015 Zalando SE
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package routing
 
 import (
@@ -191,14 +177,19 @@ func freeWildcardParam(path string) string {
 }
 
 func routePriority(r *Route) Priority {
-	prio := Priority(0)
+	var prio *Priority
+
 	for _, p := range r.Predicates {
-		if pp, ok := p.(Priority); ok && pp > prio {
-			prio = pp
+		if pp, ok := p.(Priority); ok && (prio == nil || pp > *prio) {
+			prio = &pp
 		}
 	}
 
-	return prio
+	if prio != nil {
+		return *prio
+	}
+
+	return 0
 }
 
 // constructs a matcher based on the provided definitions.
