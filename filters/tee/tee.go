bf9f7acf6763d0ef12584f132c6bf4ba1ec935f1
Arpad Ryszka <arpad@squareroundforest.org>
2017-03-16 11:00:04 +0100

    tee filter fixes (#300)
    * tee filter fixes: forward headers, requests without body, sync tests
    
    * move fix to its place from the proxy to the tee

--- filters/tee/tee.go
+++ filters/tee/tee.go
@@ -41,6 +41,22 @@ type teeTie struct {
 	w *io.PipeWriter
 }
 
+// Hop-by-hop headers. These are removed when sent to the backend.
+// http://www.w3.org/Protocols/rfc2616/rfc2616-sec13.html
+// and
+// https://golang.org/src/net/http/httputil/reverseproxy.go
+var hopHeaders = []string{
+	"Connection",
+	"Proxy-Connection", // non-standard but still sent by libcurl and rejected by e.g. google
+	"Keep-Alive",
+	"Proxy-Authenticate",
+	"Proxy-Authorization",
+	"Te",      // canonicalized version of "TE"
+	"Trailer", // not Trailers per URL above; http://www.rfc-editor.org/errata_search.php?eid=4522
+	"Transfer-Encoding",
+	"Upgrade",
+}
+
 // Returns a new tee filter Spec, whose instances execute the exact same Request against a shadow backend.
 // parameters: shadow backend url, optional - the path(as a regexp) to match and the replacement string.
 //
@@ -111,7 +127,7 @@ func (r *tee) Request(fc filters.FilterContext) {
 // copies requests changes URL and Host in request.
 // If 2nd and 3rd params are given path is also modified by applying regexp
 // Returns the cloned request and the tee body to be used on the main request.
-func cloneRequest(t *tee, req *http.Request) (*http.Request, *teeTie, error) {
+func cloneRequest(t *tee, req *http.Request) (*http.Request, io.ReadCloser, error) {
 	u := new(url.URL)
 	*u = *req.URL
 	u.Host = t.host
@@ -125,18 +141,30 @@ func cloneRequest(t *tee, req *http.Request) (*http.Request, *teeTie, error) {
 		h[k] = v
 	}
 
-	pr, pw := io.Pipe()
-	tr := &teeTie{req.Body, pw}
+	for _, k := range hopHeaders {
+		h.Del(k)
+	}
 
-	clone, err := http.NewRequest(req.Method, u.String(), pr)
+	var teeBody io.ReadCloser
+	mainBody := req.Body
+
+	// see proxy.go:231
+	if req.ContentLength != 0 {
+		pr, pw := io.Pipe()
+		teeBody = pr
+		mainBody = &teeTie{mainBody, pw}
+	}
+
+	clone, err := http.NewRequest(req.Method, u.String(), teeBody)
 	if err != nil {
 		return nil, nil, err
 	}
 
+	clone.Header = h
 	clone.Host = t.host
 	clone.ContentLength = req.ContentLength
 
-	return clone, tr, nil
+	return clone, mainBody, nil
 }
 
 // Creates out tee Filter
