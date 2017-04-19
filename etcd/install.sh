0aa2c5c907eb27871ee861aab6b7d990d85ed098
Arpad Ryszka <arpad.ryszka@gmail.com>
2017-04-19 18:42:37 +0200

    fix etcd install script: allow using etcd versions, fail early

--- etcd/install.sh
+++ etcd/install.sh
@@ -1,9 +1,21 @@
-pwd=$(pwd)
+#! /bin/bash
+
+set -e 
+
+ETCD_VERSION=master
+if [  $# -gt 0 ]; then
+    ETCD_VERSION="$1"
+fi
+
 mkdir -p $GOPATH/src/github.com/coreos
 cd $GOPATH/src/github.com/coreos
-git clone https://github.com/coreos/etcd.git
+git clone https://github.com/coreos/etcd.git || echo etcd repository exists
 cd etcd
+git checkout master
+git pull
+git checkout $ETCD_VERSION
+rm -rf gopath
+echo building etcd $ETCD_VERSION
 ./build
 mkdir -p $GOPATH/bin
 mv bin/etcd $GOPATH/bin
-cd $pwd
