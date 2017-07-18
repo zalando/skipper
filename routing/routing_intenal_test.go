901f950d1cbdcee360b1a5439721fe0e93b3ed4a
Sandor Szücs <sandor.szuecs@zalando.de>
2017-07-18 11:28:54 +0200

    Add internal tests for routing package (#373)
    * add some base tests for internal function in routing package

---
+++ routing/routing_intenal_test.go
@@ -0,0 +1,77 @@
+package routing
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/zalando/skipper/eskip"
+)
+
+func TestSlice(t *testing.T) {
+	tests := []struct {
+		message string
+		r       []*eskip.Route
+		offset  int
+		limit   int
+		expect  []*eskip.Route
+	}{
+		{
+			"empty routes",
+			[]*eskip.Route{},
+			0,
+			0,
+			[]*eskip.Route{},
+		},
+		{
+			"to big offset routes",
+			[]*eskip.Route{},
+			1,
+			0,
+			[]*eskip.Route{},
+		},
+		{
+			"to big offset and limit routes",
+			[]*eskip.Route{},
+			1,
+			1,
+			[]*eskip.Route{},
+		},
+		{
+			"find all in len()=1 with offset 0",
+			[]*eskip.Route{&eskip.Route{Id: "route1"}},
+			0,
+			1,
+			[]*eskip.Route{&eskip.Route{Id: "route1"}},
+		},
+		{
+			"find all in len()=3 with offset 0",
+			[]*eskip.Route{&eskip.Route{Id: "route1"}, &eskip.Route{Id: "route2"}, &eskip.Route{Id: "route3"}},
+			0,
+			3,
+			[]*eskip.Route{&eskip.Route{Id: "route1"}, &eskip.Route{Id: "route2"}, &eskip.Route{Id: "route3"}},
+		},
+		{
+			"find all in len()=3 with offset 1",
+			[]*eskip.Route{&eskip.Route{Id: "route1"}, &eskip.Route{Id: "route2"}, &eskip.Route{Id: "route3"}},
+			1,
+			3,
+			[]*eskip.Route{&eskip.Route{Id: "route2"}, &eskip.Route{Id: "route3"}},
+		},
+		{
+			"find all in len()=3 with offset 3",
+			[]*eskip.Route{&eskip.Route{Id: "route1"}, &eskip.Route{Id: "route2"}, &eskip.Route{Id: "route3"}},
+			3,
+			3,
+			[]*eskip.Route{},
+		},
+	}
+
+	for _, ti := range tests {
+		t.Run(ti.message, func(t *testing.T) {
+			res := slice(ti.r, ti.offset, ti.limit)
+			if !cmp.Equal(res, ti.expect) {
+				t.Fatalf("Failed test case '%s', got %v and expected %v", ti.message, res, ti.expect)
+			}
+		})
+	}
+}
