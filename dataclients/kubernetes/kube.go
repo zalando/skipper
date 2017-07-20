98d0dbc5d4a46369e330f90c1aa9569bbe5f31b9
s-klose <stefan.klose@zalando.de>
2017-07-20 18:48:52 +0200

    use proper netmasks for private subnets
    refer e.g. to https://en.wikipedia.org/wiki/Localhost#Name_resolution

--- dataclients/kubernetes/kube.go
+++ dataclients/kubernetes/kube.go
@@ -75,9 +75,9 @@ var internalIPs = []interface{}{
 	"10.0.0.0/8",
 	"192.168.0.0/16",
 	"172.16.0.0/12",
-	"127.0.0.1/32",
+	"127.0.0.1/8",
 	"fd00::/8",
-	"::1/32",
+	"::1/128",
 }
 
 // Options is used to initialize the Kubernetes DataClient.
