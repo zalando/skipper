c949ab6d9b14fe95a5958984ed8ff1d843e830cc
Sathish Gampa <Sathish.Gampa@zalando.de>
2017-12-19 17:39:52 +0100

    adding testutil for testing swarm

---
+++ swarm/swarmtest/swarmtestutil.go
@@ -0,0 +1,104 @@
+package swarmtest
+
+import (
+	"errors"
+	"fmt"
+	"github.com/hashicorp/memberlist"
+	"log"
+	"time"
+)
+
+type nodeStateType int
+
+const (
+	alive nodeStateType = iota
+	stateSuspect
+	exit
+)
+
+type TestNode struct {
+	name          string
+	addr          string
+	port          int
+	state         nodeStateType
+	ShutDownAfter time.Duration
+	transport     *CustomNetTransport
+	list          *memberlist.Memberlist
+}
+
+func NewTestNode(name string, addr string, port int) (*TestNode, error) {
+	node := TestNode{
+		name: name,
+		addr: addr,
+		port: port,
+	}
+	config := createConfig(name, addr, port)
+	ctransport, found := config.Transport.(*CustomNetTransport)
+	if !found {
+		return nil, errors.New("failed to launch the node")
+	}
+	node.transport = ctransport
+	list, err := memberlist.Create(config)
+	if err != nil {
+		return nil, err
+	}
+	node.list = list
+	node.state = alive
+	return &node, nil
+}
+
+func (node *TestNode) Join(nodesToJoin []string) error {
+	n, err := node.list.Join(nodesToJoin)
+	if err != nil {
+		return err
+	}
+	if len(nodesToJoin) != n {
+		log.Println("failed to join %d nodes from the given list", len(nodesToJoin)-n)
+	}
+	return nil
+}
+
+func (node *TestNode) ListMembers() error {
+	if node.state != alive {
+		return errors.New(fmt.Sprintf("cannot list members of a node with %s state", node.state))
+	}
+
+	for _, mem := range node.list.Members() {
+		log.Println(fmt.Sprintf("Node:%s Name: %s, IP:%s", node.name, mem.Name, mem.Addr))
+	}
+	return nil
+}
+
+func (node *TestNode) Exit() error {
+	if node.state != alive {
+		return errors.New(fmt.Sprintf("cannot exit a node from %s state", node.state))
+	}
+	node.transport.Exit()
+	return nil
+}
+
+func (node *TestNode) ShutDown() error {
+	return node.transport.Shutdown()
+}
+
+func (node *TestNode) Addr() string {
+	return fmt.Sprintf("%s:%d", node.addr, node.port)
+}
+
+func createConfig(hostname string, addr string, port int) *memberlist.Config {
+	config := memberlist.DefaultLocalConfig()
+	nc := &memberlist.NetTransportConfig{
+		BindAddrs: []string{addr},
+		BindPort:  port,
+	}
+	config.BindAddr = addr
+	config.BindPort = port
+	config.Name = hostname
+	transport, err := NewCustomNetTransport(nc)
+	if err != nil {
+		panic("failed to create memberlist config" + err.Error())
+	}
+	config.Transport = transport
+	config.DisableTcpPings = true
+	return config
+}
