92065c570f058a0f02d8d383905c9ca33e8a968f
Sandor Szuecs <sandor.szuecs@zalando.de>
2018-01-11 00:30:24 +0100

    implement reversed way of finding origin in the X-Forwarded-For

--- net/net.go
+++ net/net.go
@@ -17,7 +17,8 @@ func stripPort(address string) string {
 
 func parse(addr string) net.IP {
 	if addr != "" {
-		return net.ParseIP(stripPort(addr))
+		res := net.ParseIP(stripPort(addr))
+		return res
 	}
 	return nil
 }
@@ -26,11 +27,22 @@ func parse(addr string) net.IP {
 // header is set, then it is used instead.
 func RemoteHost(r *http.Request) net.IP {
 	ffs := r.Header.Get("X-Forwarded-For")
-	a := strings.Split(ffs, ",")
-	ff := a[len(a)-1]
+	ff := strings.Split(ffs, ",")[0]
 	if ffh := parse(ff); ffh != nil {
 		return ffh
 	}
 
+	return parse(r.RemoteAddr)
+}
+
+// The remote address of the client. When the 'X-Forwarded-For'
+// header is set, then it is used instead.
+func RemoteHostFromLast(r *http.Request) net.IP {
+	ffs := r.Header.Get("X-Forwarded-For")
+	a := strings.Split(ffs, ",")
+	ff := strings.TrimLeft(a[len(a)-1], " ")
+	if ffh := parse(ff); ffh != nil {
+		return ffh
+	}
 	return parse(r.RemoteAddr)
 }
--- net/net_test.go
+++ net/net_test.go
@@ -18,7 +18,7 @@ var netTests = []struct {
 	{"127.0.0.1:8080", net.IPv4(127, 0, 0, 1), ""},
 	{"127.0.0.1", net.IPv4(172, 16, 0, 1), "172.16.0.1"},
 	{"127.0.0.1", net.IPv4(127, 0, 0, 1), "invalid header"},
-	{"127.0.0.1", net.IPv4(8, 7, 6, 5), "172.16.0.1, 1.2.3.4, 8.7.6.5"}, // X-Forwarded-For with proxies in it
+	{"127.0.0.1", net.IPv4(172, 16, 0, 1), "172.16.0.1, 1.2.3.4, 8.7.6.5"}, // X-Forwarded-For with proxies in it
 	{"2001:4860:0:2001::68", net.ParseIP("2001:4860:0:2001::68"), ""},
 	{"127.0.0.1", net.ParseIP("2001:4860:0:2001::68"), "2001:4860:0:2001::68"},
 }
@@ -42,5 +42,27 @@ func BenchmarkRemoteHost(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		RemoteHost(r)
+	}
+}
+
+func TestRemoteHostFromLast(t *testing.T) {
+	var localNetTests = []struct {
+		input  string
+		want   net.IP
+		fwdHdr string
+	}{
+
+		{"127.0.0.1", net.IPv4(8, 7, 6, 5), "172.16.0.1, 1.2.3.4, 8.7.6.5"},
+	}
+	for _, test := range localNetTests {
+		r := &http.Request{RemoteAddr: test.input, Header: make(http.Header)}
+		if test.fwdHdr != "" {
+			r.Header.Set("x-forwarded-for", test.fwdHdr)
+		}
+		got := RemoteHostFromLast(r)
+
+		if !reflect.DeepEqual(got, test.want) {
+			t.Errorf("Unexpected IP address '%v'. Wanted '%v", got, test.want)
+		}
 	}
 }
