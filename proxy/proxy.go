fdee23539e32a747dbcb5fa2f89a6db3565d053a
Pedro Alves <pedro.alves@zalando.de>
2018-02-26 16:49:47 +0100

    gofmt

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -266,7 +266,7 @@ func copyHeader(to, from http.Header) {
 
 func copyHeaderExcluding(to, from http.Header, excludeHeaders map[string]bool) {
 	for k, v := range from {
-		if _,ok := excludeHeaders[k]; !ok {
+		if _, ok := excludeHeaders[k]; !ok {
 			to[http.CanonicalHeaderKey(k)] = v
 		}
 	}
