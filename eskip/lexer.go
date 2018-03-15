060e92a955d37124fa81beb45647148c1e46d858
Sami Nurminen <snurmine@gmail.com>
2018-03-15 18:08:07 +0200

    Using regex delimiter fails in character list in eskip #134 (#578)

--- eskip/lexer.go
+++ eskip/lexer.go
@@ -151,8 +151,46 @@ func scanEscaped(delimiter byte, code string) ([]byte, string) {
 	return b, code
 }
 
+func scanRegexp(code string) ([]byte, string) {
+	var b []byte
+	escaped := false
+	var insideGroup = false
+	for len(code) > 0 {
+		c := code[0]
+		isDelimiter := c == '/'
+		isEscapeChar := c == escapeChar
+
+		//Check if starting [... or ending ...]. Ignore if group character is escaped i.e. \[ or \]
+		if !escaped && !insideGroup && c == '[' {
+			insideGroup = true
+		} else if !escaped && insideGroup && c == ']' {
+			insideGroup = false
+		}
+
+		if escaped {
+			//delimeter / is escaped in PathRegexp so that it means no end PathRegexp(/\//)
+			if !isDelimiter && !isEscapeChar {
+				b = append(b, escapeChar)
+			}
+			b = append(b, c)
+			escaped = false
+		} else {
+			if isDelimiter && !insideGroup {
+				return b, code
+			}
+			if isEscapeChar {
+				escaped = true
+			} else {
+				b = append(b, c)
+			}
+		}
+		code = code[1:]
+	}
+	return b, code
+}
+
 func scanRegexpLiteral(code string) (t token, rest string, err error) {
-	b, rest := scanEscaped('/', code[1:])
+	b, rest := scanRegexp(code[1:])
 	if len(rest) == 0 {
 		err = incompleteToken
 		return
