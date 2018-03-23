61394492294ef4f1526378702b3163a687b471c3
Arpad Ryszka <arpad.ryszka@gmail.com>
2018-03-23 15:20:08 +0100

    simplify plugin flags

--- cmd/skipper/filterFlags.go
+++ cmd/skipper/filterFlags.go
@@ -4,13 +4,17 @@ import (
 	"strings"
 )
 
-const filterPluginUsage = "set a custom filter plugins to load, a comma separated list of name and arguments"
+const (
+	filterPluginUsage     = "set a custom filter plugins to load, a comma separated list of name and arguments"
+	predicatePluginUsage  = "set a custom predicate plugins to load, a comma separated list of name and arguments"
+	dataclientPluginUsage = "set a custom dataclient plugins to load, a comma separated list of name and arguments"
+)
 
-type filterFlags struct {
+type pluginFlags struct {
 	values [][]string
 }
 
-func (f *filterFlags) String() string {
+func (f *pluginFlags) String() string {
 	var ret []string
 	for _, val := range f.values {
 		ret = append(ret, strings.Join(val, ","))
@@ -18,9 +22,9 @@ func (f *filterFlags) String() string {
 	return strings.Join(ret, " ")
 }
 
-func (f *filterFlags) Set(value string) error {
+func (f *pluginFlags) Set(value string) error {
 	if f == nil {
-		f = &filterFlags{}
+		f = &pluginFlags{}
 	}
 	for _, v := range strings.Split(value, " ") {
 		f.values = append(f.values, strings.Split(v, ","))
@@ -28,62 +32,6 @@ func (f *filterFlags) Set(value string) error {
 	return nil
 }
 
-func (f *filterFlags) Get() [][]string {
-	return f.values
-}
-
-const predicatePluginUsage = "set a custom predicate plugins to load, a comma separated list of name and arguments"
-
-type predicateFlags struct {
-	values [][]string
-}
-
-func (f *predicateFlags) String() string {
-	var ret []string
-	for _, val := range f.values {
-		ret = append(ret, strings.Join(val, ","))
-	}
-	return strings.Join(ret, " ")
-}
-
-func (f *predicateFlags) Set(value string) error {
-	if f == nil {
-		f = &predicateFlags{}
-	}
-	for _, v := range strings.Split(value, " ") {
-		f.values = append(f.values, strings.Split(v, ","))
-	}
-	return nil
-}
-
-func (f *predicateFlags) Get() [][]string {
-	return f.values
-}
-
-const dataclientPluginUsage = "set a custom dataclient plugins to load, a comma separated list of name and arguments"
-
-type dataclientFlags struct {
-	values [][]string
-}
-
-func (f *dataclientFlags) String() string {
-	var ret []string
-	for _, val := range f.values {
-		ret = append(ret, strings.Join(val, ","))
-	}
-	return strings.Join(ret, " ")
-}
-
-func (f *dataclientFlags) Set(value string) error {
-	if f == nil {
-		f = &dataclientFlags{}
-	}
-	for _, v := range strings.Split(value, " ") {
-		f.values = append(f.values, strings.Split(v, ","))
-	}
-	return nil
-}
-
-func (f *dataclientFlags) Get() [][]string {
+func (f *pluginFlags) Get() [][]string {
 	return f.values
 }
