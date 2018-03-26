aa93c68e38cbe1081704bba87dade02d38f330df
Hanno Hecker <hanno@zalando.de>
2018-03-26 14:24:49 +0200

    drop unused stuff

--- cmd/skipper/pluginFlags.go
+++ cmd/skipper/pluginFlags.go
@@ -8,6 +8,7 @@ const (
 	filterPluginUsage     = "set a custom filter plugins to load, a comma separated list of name and arguments"
 	predicatePluginUsage  = "set a custom predicate plugins to load, a comma separated list of name and arguments"
 	dataclientPluginUsage = "set a custom dataclient plugins to load, a comma separated list of name and arguments"
+	multiPluginUsage      = "set a custom multitype plugins to load, a comma separated list of name and arguments"
 )
 
 type pluginFlags struct {
@@ -30,33 +31,5 @@ func (f *pluginFlags) Set(value string) error {
 }
 
 func (f *pluginFlags) Get() [][]string {
-	return f.values
-}
-
-const multiPluginUsage = "set a custom multitype plugins to load, a comma separated list of name and arguments"
-
-type multiPluginFlags struct {
-	values [][]string
-}
-
-func (f *multiPluginFlags) String() string {
-	var ret []string
-	for _, val := range f.values {
-		ret = append(ret, strings.Join(val, ","))
-	}
-	return strings.Join(ret, " ")
-}
-
-func (f *multiPluginFlags) Set(value string) error {
-	if f == nil {
-		f = &multiPluginFlags{}
-	}
-	for _, v := range strings.Split(value, " ") {
-		f.values = append(f.values, strings.Split(v, ","))
-	}
-	return nil
-}
-
-func (f *multiPluginFlags) Get() [][]string {
 	return f.values
 }
