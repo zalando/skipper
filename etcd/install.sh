339ec4e77949e32ada3d190b9bf75452f5c59a59
Mikkel Oscar Lyderik Larsen <mikkel.larsen@zalando.de>
2018-09-04 20:01:32 +0200

    Speedup CI buildtime
    Signed-off-by: Mikkel Oscar Lyderik Larsen <mikkel.larsen@zalando.de>

--- etcd/install.sh
+++ etcd/install.sh
@@ -1,21 +1,20 @@
 #! /bin/bash
-
-set -e 
+set -eo pipefail
 
 ETCD_VERSION=master
 if [  $# -gt 0 ]; then
     ETCD_VERSION="$1"
 fi
 
-mkdir -p $GOPATH/src/github.com/coreos
-cd $GOPATH/src/github.com/coreos
-git clone https://github.com/coreos/etcd.git || echo etcd repository exists
-cd etcd
-git checkout master
-git pull
-git checkout $ETCD_VERSION
-rm -rf gopath
-echo building etcd $ETCD_VERSION
-./build
-mkdir -p $GOPATH/bin
-mv bin/etcd $GOPATH/bin
+if [ -z "${GOBIN}" ]; then
+    LOCAL_GOBIN="$GOPATH/bin"
+else
+    LOCAL_GOBIN="$GOBIN"
+fi
+
+mkdir -p "$LOCAL_GOBIN"
+wget \
+    "https://github.com/etcd-io/etcd/releases/download/${ETCD_VERSION}/etcd-${ETCD_VERSION}-linux-amd64.tar.gz" \
+    -O /tmp/etcd.tar.gz
+tar -xzf /tmp/etcd.tar.gz --strip-components=1 \
+    -C "$LOCAL_GOBIN" "etcd-${ETCD_VERSION}-linux-amd64/etcd"
