zalando/skipper

commit b7fdb1291d857d64b3c6337d34170b9ffbd3bdcd

Feature/access log filter (#752)
* Move access log handling to proxy

Introduce accessLog filter to be able to override global setting

* Fix readme.md

* Renamed accessLog -> accessLogDisabled, inverted logic, added more tests and docs

* create table tests for accessLogDisabled

logging/loggingwriter.go
@@ -7,38 +7,41 @@ import (
 	"net/http"
 )
 
-type loggingWriter struct {
+type LoggingWriter struct {
 	writer http.ResponseWriter
-	code   int
 	bytes  int64
 }
 
-func (lw *loggingWriter) Write(data []byte) (count int, err error) {
+func NewLoggingWriter(writer http.ResponseWriter) *LoggingWriter {
+	return &LoggingWriter{writer: writer}
+}
+
+func (lw *LoggingWriter) Write(data []byte) (count int, err error) {
 	count, err = lw.writer.Write(data)
 	lw.bytes += int64(count)
 	return
 }
 
-func (lw *loggingWriter) WriteHeader(code int) {
+func (lw *LoggingWriter) WriteHeader(code int) {
 	lw.writer.WriteHeader(code)
-	if code == 0 {
-		code = 200
-	}
-	lw.code = code
 }
 
-func (lw *loggingWriter) Header() http.Header {
+func (lw *LoggingWriter) Header() http.Header {
 	return lw.writer.Header()
 }
 
-func (lw *loggingWriter) Flush() {
+func (lw *LoggingWriter) Flush() {
 	lw.writer.(http.Flusher).Flush()
 }
 
-func (lw *loggingWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
+func (lw *LoggingWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
 	hij, ok := lw.writer.(http.Hijacker)
 	if ok {
 		return hij.Hijack()
 	}
 	return nil, nil, fmt.Errorf("could not hijack connection")
+}
+
+func (lw *LoggingWriter) GetBytes() int64 {
+	return lw.bytes
 }

proxy/proxy.go
@@ -18,6 +18,7 @@ import (
 	"github.com/opentracing/opentracing-go/ext"
 	"github.com/zalando/skipper/circuit"
 	"github.com/zalando/skipper/eskip"
+	"github.com/zalando/skipper/filters/accesslog"
 	circuitfilters "github.com/zalando/skipper/filters/circuit"
 	ratelimitfilters "github.com/zalando/skipper/filters/ratelimit"
 	tracingfilter "github.com/zalando/skipper/filters/tracing"
@@ -148,6 +149,9 @@ type Params struct {
 	// Default: "ingress".
 	OpenTracingInitialSpan string
 
+	// When set, no access log is printed.
+	AccessLogDisabled bool
+
 	// Loadbalancer to report unhealthy or dead backends to
 	LoadBalancer *loadbalancer.LB
 
@@ -240,6 +244,7 @@ type Proxy struct {
 	openTracer             ot.Tracer
 	openTracingInitialSpan string
 	lb                     *loadbalancer.LB
+	accessLogDisabled      bool
 }
 
 // proxyError is used to wrap errors during proxying and to indicate
@@ -515,6 +520,7 @@ func WithParams(p Params) *Proxy {
 		log:                    &logging.DefaultLog{},
 		defaultHTTPStatus:      defaultHTTPStatus,
 		openTracer:             p.OpenTracer,
+		accessLogDisabled:      p.AccessLogDisabled,
 		openTracingInitialSpan: openTracingInitialSpan,
 		lb:                     p.LoadBalancer,
 	}
@@ -1005,6 +1011,8 @@ func (p *Proxy) errorResponse(ctx *context, err error) {
 
 // http.Handler implementation
 func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	lw := logging.NewLoggingWriter(w)
+
 	p.metrics.IncCounter("incoming." + r.Proto)
 	var ctx *context
 
@@ -1026,7 +1034,7 @@ func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	p.setCommonSpanInfo(r.URL, r, span)
 	r = r.WithContext(ot.ContextWithSpan(r.Context(), span))
 
-	ctx = newContext(w, r, p.flags.PreserveOriginal(), p.metrics, p.routing.Get())
+	ctx = newContext(lw, r, p.flags.PreserveOriginal(), p.metrics, p.routing.Get())
 	ctx.startServe = time.Now()
 	ctx.tracer = p.openTracer
 
@@ -1053,6 +1061,24 @@ func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		ctx.response.StatusCode,
 		ctx.startServe,
 	)
+
+	accessLogDisabled, ok := ctx.stateBag[accesslog.AccessLogDisabledKey].(bool)
+
+	if !ok {
+		accessLogDisabled = p.accessLogDisabled
+	}
+
+	if !accessLogDisabled {
+		entry := &logging.AccessEntry{
+			Request:      r,
+			ResponseSize: lw.GetBytes(),
+			StatusCode:   ctx.response.StatusCode,
+			RequestTime:  ctx.startServe,
+			Duration:     time.Since(ctx.startServe),
+		}
+
+		logging.LogAccess(entry)
+	}
 }
 
 // Close causes the proxy to stop closing idle

logging/loggingwriter_test.go
@@ -6,9 +6,9 @@ import (
 	"testing"
 )
 
-func TestWritesAndCounts(t *testing.T) {
+func TestWrites(t *testing.T) {
 	rr := httptest.NewRecorder()
-	w := &loggingWriter{writer: rr}
+	w := &LoggingWriter{writer: rr}
 
 	body := "Hello, world!"
 	w.Write([]byte(body))
@@ -25,21 +25,17 @@ func TestWritesAndCounts(t *testing.T) {
 
 func TestWritesAndStoresStatusCode(t *testing.T) {
 	rr := httptest.NewRecorder()
-	w := &loggingWriter{writer: rr}
+	w := &LoggingWriter{writer: rr}
 	w.WriteHeader(http.StatusTeapot)
 
 	if rr.Code != http.StatusTeapot {
 		t.Error("failed to write status code")
 	}
-
-	if w.code != http.StatusTeapot {
-		t.Error("failed to store status code")
-	}
 }
 
 func TestReturnsUnderlyingHeader(t *testing.T) {
 	rr := httptest.NewRecorder()
-	w := &loggingWriter{writer: rr}
+	w := &LoggingWriter{writer: rr}
 	w.Header().Set("X-Test-Header", "test-value")
 	if rr.Header().Get("X-Test-Header") != "test-value" {
 		t.Error("failed to set the header")
@@ -48,20 +44,10 @@ func TestReturnsUnderlyingHeader(t *testing.T) {
 
 func TestFlushesPartialPayload(t *testing.T) {
 	rr := httptest.NewRecorder()
-	w := &loggingWriter{writer: rr}
+	w := &LoggingWriter{writer: rr}
 	w.Write([]byte("Hello, world!"))
 	w.Flush()
 	if !rr.Flushed {
 		t.Error("failed to flush underlying writer")
-	}
-}
-
-func TestSets200OnMissingStatus(t *testing.T) {
-	rr := httptest.NewRecorder()
-	w := &loggingWriter{writer: rr}
-	w.WriteHeader(0)
-
-	if w.code != http.StatusOK {
-		t.Errorf("failed to overwrite status code. Expected 200 but got %d", w.code)
 	}
 }

proxy/proxy_test.go
@@ -20,6 +20,7 @@ import (
 	"github.com/zalando/skipper/filters"
 	"github.com/zalando/skipper/filters/builtin"
 	"github.com/zalando/skipper/loadbalancer"
+	"github.com/zalando/skipper/logging"
 	"github.com/zalando/skipper/logging/loggingtest"
 	"github.com/zalando/skipper/routing"
 	"github.com/zalando/skipper/routing/testdataclient"
@@ -1287,6 +1288,137 @@ func TestHopHeaderRemoval(t *testing.T) {
 
 	if w.Code != http.StatusOK {
 		t.Error("wrong status", w.Code)
+	}
+}
+
+func TestLogsAccess(t *testing.T) {
+	var accessLog bytes.Buffer
+	logging.Init(logging.Options{AccessLogOutput: &accessLog})
+
+	response := "7 bytes"
+
+	u, _ := url.ParseRequestURI("https://www.example.org/hello")
+	r := &http.Request{
+		URL:    u,
+		Method: "GET",
+		Header: http.Header{"Connection": []string{"token"}}}
+	w := httptest.NewRecorder()
+
+	doc := fmt.Sprintf(`hello: Path("/hello") -> status(%d) -> inlineContent("%s") -> <shunt>`, http.StatusTeapot, response)
+
+	tp, err := newTestProxy(doc, FlagsNone)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	defer tp.close()
+
+	tp.proxy.ServeHTTP(w, r)
+
+	output := accessLog.String()
+	if !strings.Contains(output, fmt.Sprintf(`"%s - -" %d %d "-" "-" 0 - - -`, r.Method, http.StatusTeapot, len(response))) {
+		t.Error("failed to log access", output)
+	}
+}
+
+func TestDisableAccessLog(t *testing.T) {
+	var buf bytes.Buffer
+	logging.Init(logging.Options{
+		AccessLogOutput: &buf})
+
+	response := "7 bytes"
+
+	u, _ := url.ParseRequestURI("https://www.example.org/hello")
+	r := &http.Request{
+		URL:    u,
+		Method: "GET",
+		Header: http.Header{"Connection": []string{"token"}}}
+	w := httptest.NewRecorder()
+
+	doc := fmt.Sprintf(`hello: Path("/hello") -> status(%d) -> inlineContent("%s") -> <shunt>`, http.StatusTeapot, response)
+
+	tp, err := newTestProxyWithParams(doc, Params{
+		AccessLogDisabled: true,
+	})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	defer tp.close()
+
+	tp.proxy.ServeHTTP(w, r)
+
+	if buf.Len() != 0 {
+		t.Error("failed to disable access log")
+	}
+}
+
+func TestDisableAccessLogWithFilter(t *testing.T) {
+	var buf bytes.Buffer
+	logging.Init(logging.Options{
+		AccessLogOutput: &buf})
+
+	response := "7 bytes"
+
+	u, _ := url.ParseRequestURI("https://www.example.org/hello")
+	r := &http.Request{
+		URL:    u,
+		Method: "GET",
+		Header: http.Header{"Connection": []string{"token"}}}
+	w := httptest.NewRecorder()
+
+	doc := fmt.Sprintf(`hello: Path("/hello") -> accessLogDisabled("true") -> status(%d) -> inlineContent("%s") -> <shunt>`, http.StatusTeapot, response)
+
+	tp, err := newTestProxyWithParams(doc, Params{
+		AccessLogDisabled: false,
+	})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	defer tp.close()
+
+	tp.proxy.ServeHTTP(w, r)
+
+	if buf.Len() != 0 {
+		t.Error("failed to disable access log")
+	}
+}
+
+func TestEnableAccessLogWithFilter(t *testing.T) {
+	var buf bytes.Buffer
+	logging.Init(logging.Options{
+		AccessLogOutput: &buf})
+
+	response := "7 bytes"
+
+	u, _ := url.ParseRequestURI("https://www.example.org/hello")
+	r := &http.Request{
+		URL:    u,
+		Method: "GET",
+		Header: http.Header{"Connection": []string{"token"}}}
+	w := httptest.NewRecorder()
+
+	doc := fmt.Sprintf(`hello: Path("/hello") -> accessLogDisabled("false") -> status(%d) -> inlineContent("%s") -> <shunt>`, http.StatusTeapot, response)
+
+	tp, err := newTestProxyWithParams(doc, Params{
+		AccessLogDisabled: true,
+	})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	defer tp.close()
+
+	tp.proxy.ServeHTTP(w, r)
+
+	output := buf.String()
+	if !strings.Contains(output, fmt.Sprintf(`"%s - -" %d %d "-" "-" 0 - - -`, r.Method, http.StatusTeapot, len(response))) {
+		t.Error("failed to log access", output)
 	}
 }
 

dataclients/kubernetes/kube_test.go
@@ -1397,16 +1397,16 @@ func TestConvertPathRule(t *testing.T) {
 
 		api.services = services{
 			"namespace1": map[string]*service{
-				"svcname1": &service{
+				"svcname1": {
 					Spec: &serviceSpec{
 						ClusterIP: "10.3.2.1",
 						Ports: []*servicePort{
-							&servicePort{
+							{
 								Name:       "svcname1",
 								Port:       8080,
 								TargetPort: &backendPort{value: 8080},
 							},
-							&servicePort{
+							{
 								Name:       "svcname1",
 								Port:       8181,
 								TargetPort: &backendPort{value: 8181},
@@ -1430,11 +1430,11 @@ func TestConvertPathRule(t *testing.T) {
 						ServicePort: backendPort{value: 8080},
 					},
 					Rules: []*rule{
-						&rule{
+						{
 							Host: "host1",
 							Http: &httpRule{
 								Paths: []*pathRule{
-									&pathRule{
+									{
 										Path: "/",
 										Backend: &backend{
 											ServiceName: "svcname1",
@@ -1444,11 +1444,11 @@ func TestConvertPathRule(t *testing.T) {
 								},
 							},
 						},
-						&rule{
+						{
 							Host: "host2",
 							Http: &httpRule{
 								Paths: []*pathRule{
-									&pathRule{
+									{
 										Path: "/",
 										Backend: &backend{
 											ServiceName: "svcname1",

filters/accesslog/disable.go
@@ -1,0 +1,45 @@
+package accesslog
+
+import (
+	"github.com/zalando/skipper/filters"
+)
+
+const (
+	// AccessLogDisabledName is the filter name seen by the user
+	AccessLogDisabledName = "accessLogDisabled"
+
+	// AccessLogDisabledKey is the key used in the state bag to pass the access log state to the proxy.
+	AccessLogDisabledKey = "statebag:access_log:proxy:disabled"
+)
+
+type accessLogDisabled struct {
+	disabled bool
+}
+
+// NewAccessLogDisabled creates a filter spec for overriding the state of the AccessLogDisabled setting. (By default global setting is used.)
+//
+// 	accessLogDisabled("false")
+func NewAccessLogDisabled() filters.Spec {
+	return &accessLogDisabled{}
+}
+
+func (*accessLogDisabled) Name() string { return AccessLogDisabledName }
+
+func (*accessLogDisabled) CreateFilter(args []interface{}) (filters.Filter, error) {
+	if len(args) != 1 {
+		return nil, filters.ErrInvalidFilterParameters
+	}
+
+	if a, ok := args[0].(string); ok && a == "true" || a == "false" {
+		return &accessLogDisabled{a == "true"}, nil
+	} else {
+		return nil, filters.ErrInvalidFilterParameters
+	}
+}
+
+func (al *accessLogDisabled) Request(ctx filters.FilterContext) {
+	bag := ctx.StateBag()
+	bag[AccessLogDisabledKey] = al.disabled
+}
+
+func (*accessLogDisabled) Response(filters.FilterContext) {}

filters/accesslog/disable_test.go
@@ -1,0 +1,70 @@
+package accesslog
+
+import (
+	"testing"
+
+	"github.com/zalando/skipper/filters"
+	"github.com/zalando/skipper/filters/filtertest"
+)
+
+func TestAccessLogDisabled(t *testing.T) {
+	for _, ti := range []struct {
+		msg    string
+		state  []interface{}
+		result bool
+		err    error
+	}{
+		{
+			msg:    "false-value-enables-access-log",
+			state:  []interface{}{"false"},
+			result: false,
+			err:    nil,
+		},
+		{
+			msg:    "true-value-disables-access-log",
+			state:  []interface{}{"true"},
+			result: true,
+			err:    nil,
+		},
+		{
+			msg:    "unknown-argument-leads-to-error",
+			state:  []interface{}{"unknownValue"},
+			result: false,
+			err:    filters.ErrInvalidFilterParameters,
+		},
+		{
+			msg:    "no-arguments-lead-to-error",
+			state:  []interface{}{},
+			result: false,
+			err:    filters.ErrInvalidFilterParameters,
+		},
+		{
+			msg:    "multiple-arguments-lead-to-error",
+			state:  []interface{}{"true", "second"},
+			result: false,
+			err:    filters.ErrInvalidFilterParameters,
+		},
+	} {
+		t.Run(ti.msg, func(t *testing.T) {
+			f, err := NewAccessLogDisabled().CreateFilter(ti.state)
+
+			if err != ti.err {
+				t.Errorf("error is not equal to expected '%v': %v", ti.err, err)
+				return
+			}
+
+			if err != nil {
+				return
+			}
+
+			var ctx filtertest.Context
+			ctx.FStateBag = make(map[string]interface{})
+
+			f.Request(&ctx)
+			bag := ctx.StateBag()
+			if bag[AccessLogDisabledKey] != ti.result {
+				t.Errorf("access log state is not equal to expected '%v': %v", ti.result, bag[AccessLogDisabledKey])
+			}
+		})
+	}
+}

filters/accesslog/doc.go
@@ -1,0 +1,15 @@
+/*
+Package accesslog provides a request filter that gives ability to override AccessLogDisabled setting.
+
+How It Works
+
+The filter accepts one argument of "true" or "false" value. If the argument value is "true" access log entries for this
+route won't be produced even if global AccessLogDisabled is false. Otherwise, if argument value is "false" access log
+entries for this route will be produced even if global AccessLogDisabled is true.
+
+Usage
+
+	accessLogDisabled("true")
+	accessLogDisabled("false")
+*/
+package accesslog

commit 4c053cfa5645c111358f44c592caf9914c0a1f3c

Fix logging regression after #752
Fixes #802

logging/loggingwriter.go
@@ -10,6 +10,7 @@ import (
 type LoggingWriter struct {
 	writer http.ResponseWriter
 	bytes  int64
+	code   int
 }
 
 func NewLoggingWriter(writer http.ResponseWriter) *LoggingWriter {
@@ -24,6 +25,10 @@ func (lw *LoggingWriter) Write(data []byte) (count int, err error) {
 
 func (lw *LoggingWriter) WriteHeader(code int) {
 	lw.writer.WriteHeader(code)
+	if code == 0 {
+		code = 200
+	}
+	lw.code = code
 }
 
 func (lw *LoggingWriter) Header() http.Header {
@@ -44,4 +49,8 @@ func (lw *LoggingWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
 
 func (lw *LoggingWriter) GetBytes() int64 {
 	return lw.bytes
+}
+
+func (lw *LoggingWriter) GetCode() int {
+	return lw.code
 }

proxy/proxy.go
@@ -1041,6 +1041,26 @@ func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		span.Finish()
 	}()
 
+	defer func() {
+		accessLogEnabled, ok := ctx.stateBag[accesslog.AccessLogEnabledKey].(bool)
+
+		if !ok {
+			accessLogEnabled = !p.accessLogDisabled
+		}
+
+		if accessLogEnabled {
+			entry := &logging.AccessEntry{
+				Request:      r,
+				ResponseSize: lw.GetBytes(),
+				StatusCode:   lw.GetCode(),
+				RequestTime:  ctx.startServe,
+				Duration:     time.Since(ctx.startServe),
+			}
+
+			logging.LogAccess(entry)
+		}
+	}()
+
 	ext.SpanKindRPCServer.Set(span)
 	p.setCommonSpanInfo(r.URL, r, span)
 	r = r.WithContext(ot.ContextWithSpan(r.Context(), span))
@@ -1072,24 +1092,6 @@ func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		ctx.response.StatusCode,
 		ctx.startServe,
 	)
-
-	accessLogEnabled, ok := ctx.stateBag[accesslog.AccessLogEnabledKey].(bool)
-
-	if !ok {
-		accessLogEnabled = !p.accessLogDisabled
-	}
-
-	if accessLogEnabled {
-		entry := &logging.AccessEntry{
-			Request:      r,
-			ResponseSize: lw.GetBytes(),
-			StatusCode:   ctx.response.StatusCode,
-			RequestTime:  ctx.startServe,
-			Duration:     time.Since(ctx.startServe),
-		}
-
-		logging.LogAccess(entry)
-	}
 }
 
 // Close causes the proxy to stop closing idle

logging/loggingwriter_test.go
@@ -31,6 +31,10 @@ func TestWritesAndStoresStatusCode(t *testing.T) {
 	if rr.Code != http.StatusTeapot {
 		t.Error("failed to write status code")
 	}
+
+	if w.GetCode() != http.StatusTeapot {
+		t.Error("failed to get status code")
+	}
 }
 
 func TestReturnsUnderlyingHeader(t *testing.T) {

proxy/proxy_test.go
@@ -1422,6 +1422,51 @@ func TestEnableAccessLogWithFilter(t *testing.T) {
 	}
 }
 
+func TestAccessLogOnFailedRequest(t *testing.T) {
+	var buf bytes.Buffer
+	logging.Init(logging.Options{
+		AccessLogOutput: &buf})
+
+	s := httptest.NewServer(http.HandlerFunc(func(http.ResponseWriter, *http.Request) {}))
+	s.Close()
+
+	p, err := newTestProxy(fmt.Sprintf(`* -> "%s"`, s.URL), 0)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	defer p.proxy.Close()
+
+	ps := httptest.NewServer(p.proxy)
+	defer ps.Close()
+
+	rsp, err := http.Get(ps.URL)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	defer rsp.Body.Close()
+
+	if rsp.StatusCode != http.StatusBadGateway {
+		t.Error("failed to return 502 Bad Gateway on failing backend connection")
+	}
+
+	output := buf.String()
+
+	proxyURL, err := url.Parse(ps.URL)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	expected := fmt.Sprintf(`"GET / HTTP/1.1" %d %d "-" "Go-http-client/1.1" 0 %s - -`, http.StatusBadGateway, rsp.ContentLength, proxyURL.Host)
+	if !strings.Contains(output, expected) {
+		t.Error("failed to log access", output, expected)
+	}
+}
+
 func TestHopHeaderRemovalDisabled(t *testing.T) {
 	payload := []byte("Hello World!")
 