9cdce9fead18430ed5dcb39aadfcde260c9d3626
Sandor Szücs <sandor.szuecs@zalando.de>
2018-12-03 16:55:56 +0100

    add configurable timeouts for missing available in http.Transport timeouts for backend calls
    Signed-off-by: Sandor Szücs <sandor.szuecs@zalando.de>

--- cmd/skipper/main.go
+++ cmd/skipper/main.go
@@ -51,14 +51,16 @@ const (
 	defaultApplicationLogLevel  = "INFO"
 
 	// connections, timeouts:
-	defaultReadTimeoutServer          = 5 * time.Minute
-	defaultReadHeaderTimeoutServer    = 60 * time.Second
-	defaultWriteTimeoutServer         = 60 * time.Second
-	defaultIdleTimeoutServer          = 60 * time.Second
-	defaultTimeoutBackend             = 60 * time.Second
-	defaultKeepaliveBackend           = 30 * time.Second
-	defaultTLSHandshakeTimeoutBackend = 60 * time.Second
-	defaultMaxIdleConnsBackend        = 0
+	defaultReadTimeoutServer            = 5 * time.Minute
+	defaultReadHeaderTimeoutServer      = 60 * time.Second
+	defaultWriteTimeoutServer           = 60 * time.Second
+	defaultIdleTimeoutServer            = 60 * time.Second
+	defaultTimeoutBackend               = 60 * time.Second
+	defaultKeepaliveBackend             = 30 * time.Second
+	defaultTLSHandshakeTimeoutBackend   = 60 * time.Second
+	defaultResponseHeaderTimeoutBackend = 60 * time.Second
+	defaultExpectContinueTimeoutBackend = 30 * time.Second
+	defaultMaxIdleConnsBackend          = 0
 
 	// Auth:
 	defaultOAuthTokeninfoTimeout          = 2 * time.Second
@@ -161,22 +163,24 @@ const (
 	apiUsageMonitoringDefaultClientTrackingPatternUsage = "regular expression to default to when API usage monitoring filter configuration does not provide `client_tracking_pattern`"
 
 	// connections, timeouts:
-	idleConnsPerHostUsage           = "maximum idle connections per backend host"
-	closeIdleConnsPeriodUsage       = "period of closing all idle connections in seconds or as a duration string. Not closing when less than 0"
-	backendFlushIntervalUsage       = "flush interval for upgraded proxy connections"
-	experimentalUpgradeUsage        = "enable experimental feature to handle upgrade protocol requests"
-	experimentalUpgradeAuditUsage   = "enable audit logging of the request line and the messages during the experimental web socket upgrades"
-	readTimeoutServerUsage          = "set ReadTimeout for http server connections"
-	readHeaderTimeoutServerUsage    = "set ReadHeaderTimeout for http server connections"
-	writeTimeoutServerUsage         = "set WriteTimeout for http server connections"
-	idleTimeoutServerUsage          = "set IdleTimeout for http server connections"
-	maxHeaderBytesUsage             = "set MaxHeaderBytes for http server connections"
-	enableConnMetricsServerUsage    = "enables connection metrics for http server connections"
-	timeoutBackendUsage             = "sets the TCP client connection timeout for backend connections"
-	keepaliveBackendUsage           = "sets the keepalive for backend connections"
-	enableDualstackBackendUsage     = "enables DualStack for backend connections"
-	tlsHandshakeTimeoutBackendUsage = "sets the TLS handshake timeout for backend connections"
-	maxIdleConnsBackendUsage        = "sets the maximum idle connections for all backend connections"
+	idleConnsPerHostUsage             = "maximum idle connections per backend host"
+	closeIdleConnsPeriodUsage         = "period of closing all idle connections in seconds or as a duration string. Not closing when less than 0"
+	backendFlushIntervalUsage         = "flush interval for upgraded proxy connections"
+	experimentalUpgradeUsage          = "enable experimental feature to handle upgrade protocol requests"
+	experimentalUpgradeAuditUsage     = "enable audit logging of the request line and the messages during the experimental web socket upgrades"
+	readTimeoutServerUsage            = "set ReadTimeout for http server connections"
+	readHeaderTimeoutServerUsage      = "set ReadHeaderTimeout for http server connections"
+	writeTimeoutServerUsage           = "set WriteTimeout for http server connections"
+	idleTimeoutServerUsage            = "set IdleTimeout for http server connections"
+	maxHeaderBytesUsage               = "set MaxHeaderBytes for http server connections"
+	enableConnMetricsServerUsage      = "enables connection metrics for http server connections"
+	timeoutBackendUsage               = "sets the TCP client connection timeout for backend connections"
+	keepaliveBackendUsage             = "sets the keepalive for backend connections"
+	enableDualstackBackendUsage       = "enables DualStack for backend connections"
+	tlsHandshakeTimeoutBackendUsage   = "sets the TLS handshake timeout for backend connections"
+	responseHeaderTimeoutBackendUsage = "sets the HTTP response header timeout for backend connections"
+	expectContinueTimeoutBackendUsage = "sets the HTTP expect continue timeout for backend connections"
+	maxIdleConnsBackendUsage          = "sets the maximum idle connections for all backend connections"
 
 	// swarm:
 	enableSwarmUsage                       = "enable swarm communication between nodes in a skipper fleet"
@@ -291,22 +295,24 @@ var (
 	apiUsageMonitoringDefaultClientTrackingPattern string
 
 	// connections, timeouts:
-	idleConnsPerHost           int
-	closeIdleConnsPeriod       string
-	backendFlushInterval       time.Duration
-	experimentalUpgrade        bool
-	experimentalUpgradeAudit   bool
-	readTimeoutServer          time.Duration
-	readHeaderTimeoutServer    time.Duration
-	writeTimeoutServer         time.Duration
-	idleTimeoutServer          time.Duration
-	maxHeaderBytes             int
-	enableConnMetricsServer    bool
-	timeoutBackend             time.Duration
-	keepaliveBackend           time.Duration
-	enableDualstackBackend     bool
-	tlsHandshakeTimeoutBackend time.Duration
-	maxIdleConnsBackend        int
+	idleConnsPerHost             int
+	closeIdleConnsPeriod         string
+	backendFlushInterval         time.Duration
+	experimentalUpgrade          bool
+	experimentalUpgradeAudit     bool
+	readTimeoutServer            time.Duration
+	readHeaderTimeoutServer      time.Duration
+	writeTimeoutServer           time.Duration
+	idleTimeoutServer            time.Duration
+	maxHeaderBytes               int
+	enableConnMetricsServer      bool
+	timeoutBackend               time.Duration
+	keepaliveBackend             time.Duration
+	enableDualstackBackend       bool
+	tlsHandshakeTimeoutBackend   time.Duration
+	responseHeaderTimeoutBackend time.Duration
+	expectContinueTimeoutBackend time.Duration
+	maxIdleConnsBackend          int
 
 	// swarm:
 	enableSwarm                       bool
@@ -434,6 +440,8 @@ func init() {
 	flag.DurationVar(&keepaliveBackend, "keepalive-backend", defaultKeepaliveBackend, keepaliveBackendUsage)
 	flag.BoolVar(&enableDualstackBackend, "enable-dualstack-backend", true, enableDualstackBackendUsage)
 	flag.DurationVar(&tlsHandshakeTimeoutBackend, "tls-timeout-backend", defaultTLSHandshakeTimeoutBackend, tlsHandshakeTimeoutBackendUsage)
+	flag.DurationVar(&responseHeaderTimeoutBackend, "response-header-timeout-backend", defaultResponseHeaderTimeoutBackend, responseHeaderTimeoutBackendUsage)
+	flag.DurationVar(&expectContinueTimeoutBackend, "expect-continue-timeout-backend", defaultExpectContinueTimeoutBackend, expectContinueTimeoutBackendUsage)
 	flag.IntVar(&maxIdleConnsBackend, "max-idle-connection-backend", defaultMaxIdleConnsBackend, maxIdleConnsBackendUsage)
 	flag.BoolVar(&enableSwarm, "enable-swarm", false, enableSwarmUsage)
 	flag.StringVar(&swarmKubernetesNamespace, "swarm-namespace", swarm.DefaultNamespace, swarmKubernetesNamespaceUsage)
@@ -621,22 +629,24 @@ func main() {
 		WebhookTimeout:                 webhookTimeout,
 
 		// connections, timeouts:
-		IdleConnectionsPerHost:     idleConnsPerHost,
-		CloseIdleConnsPeriod:       time.Duration(clsic) * time.Second,
-		BackendFlushInterval:       backendFlushInterval,
-		ExperimentalUpgrade:        experimentalUpgrade,
-		ExperimentalUpgradeAudit:   experimentalUpgradeAudit,
-		ReadTimeoutServer:          readTimeoutServer,
-		ReadHeaderTimeoutServer:    readHeaderTimeoutServer,
-		WriteTimeoutServer:         writeTimeoutServer,
-		IdleTimeoutServer:          idleTimeoutServer,
-		MaxHeaderBytes:             maxHeaderBytes,
-		EnableConnMetricsServer:    enableConnMetricsServer,
-		TimeoutBackend:             timeoutBackend,
-		KeepAliveBackend:           keepaliveBackend,
-		DualStackBackend:           enableDualstackBackend,
-		TLSHandshakeTimeoutBackend: tlsHandshakeTimeoutBackend,
-		MaxIdleConnsBackend:        maxIdleConnsBackend,
+		IdleConnectionsPerHost:       idleConnsPerHost,
+		CloseIdleConnsPeriod:         time.Duration(clsic) * time.Second,
+		BackendFlushInterval:         backendFlushInterval,
+		ExperimentalUpgrade:          experimentalUpgrade,
+		ExperimentalUpgradeAudit:     experimentalUpgradeAudit,
+		ReadTimeoutServer:            readTimeoutServer,
+		ReadHeaderTimeoutServer:      readHeaderTimeoutServer,
+		WriteTimeoutServer:           writeTimeoutServer,
+		IdleTimeoutServer:            idleTimeoutServer,
+		MaxHeaderBytes:               maxHeaderBytes,
+		EnableConnMetricsServer:      enableConnMetricsServer,
+		TimeoutBackend:               timeoutBackend,
+		KeepAliveBackend:             keepaliveBackend,
+		DualStackBackend:             enableDualstackBackend,
+		TLSHandshakeTimeoutBackend:   tlsHandshakeTimeoutBackend,
+		ResponseHeaderTimeoutBackend: responseHeaderTimeoutBackend,
+		ExpectContinueTimeoutBackend: expectContinueTimeoutBackend,
+		MaxIdleConnsBackend:          maxIdleConnsBackend,
 
 		// swarm:
 		EnableSwarm:                       enableSwarm,
