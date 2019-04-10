c159daf81c4006ef5e7b7ce03838e0db851fe816
Sandor Szücs <sandor.szuecs@zalando.de>
2019-04-10 16:34:57 +0200

    add get secret test
    Signed-off-by: Sandor Szücs <sandor.szuecs@zalando.de>

--- filters/auth/encrypter_test.go
+++ filters/auth/encrypter_test.go
@@ -1,9 +1,13 @@
 package auth
 
 import (
-	"github.com/stretchr/testify/assert"
+	"fmt"
+	"os"
+	"reflect"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 type testingSecretSource struct {
@@ -48,4 +52,47 @@ func TestCipherRefreshing(t *testing.T) {
 	enc.close()
 	<-enc.closedHook
 	assert.True(t, sSource.getCount >= 3, "secret fetched less than 3 time in 15 seconds")
+}
+
+func Test_getSecret(t *testing.T) {
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Errorf("Failed to get CWD: %v", err)
+	}
+
+	for _, tt := range []struct {
+		name    string
+		args    string
+		want    [][]byte
+		wantErr bool
+	}{
+		{
+			name:    "secret file does not exist",
+			args:    "does-not-exist",
+			want:    [][]byte{},
+			wantErr: true,
+		},
+		{
+			name: "secret file that exists",
+			args: fmt.Sprintf("%s/../../skptesting/enckey", wd),
+			want: [][]byte{
+				[]byte("very secure"),
+			},
+			wantErr: false,
+		},
+	} {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			fss := &fileSecretSource{fileName: tt.args}
+			got, err := fss.getSecret()
+
+			if (tt.wantErr && err == nil) || (!tt.wantErr && err != nil) {
+				t.Errorf("Got error but does not want an error, or the other way around: wantErr: %v, err: %v", tt.wantErr, err)
+			}
+			if err == nil && !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Failed to getSecret: Want %v, got %v, err %v", tt.want, got, err)
+			}
+		})
+	}
+
 }
