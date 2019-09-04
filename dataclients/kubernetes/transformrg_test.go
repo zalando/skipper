1ad0c88eeccf62299738aea793bb490afebfbbfe
Arpad Ryszka <arpad.ryszka@gmail.com>
2019-09-04 13:27:55 +0200

    Feature/routegroup crd transform (#1176)
    * initial stubs for the CRD client
    
    Signed-off-by: Arpad Ryszka <arpad.ryszka@gmail.com>
    
    * rename CRD to route group
    
    Signed-off-by: Arpad Ryszka <arpad.ryszka@gmail.com>
    
    * canonical route comparison in eskip
    
    Signed-off-by: Arpad Ryszka <arpad.ryszka@gmail.com>

--- dataclients/kubernetes/transformrg_test.go
+++ dataclients/kubernetes/transformrg_test.go
@@ -6,6 +6,33 @@ import (
 	"github.com/zalando/skipper/eskip"
 )
 
+const singleRouteGroup = `
+{"items": [
+{
+  "apiVersion": "zalando.org/v1",
+  "kind": "RouteGroup",
+  "metadata": {
+    "name": "my-routes"
+  },
+  "spec": {
+    "hosts": [
+      "foo.example.org"
+    ],
+    "backends": [
+      {
+        "serviceName": "foo-service",
+        "servicePort": 80
+      }
+    ],
+    "paths": [
+      {
+        "path": "/"
+      }
+    ]
+  }
+}]}
+`
+
 type stringClient string
 
 func (c stringClient) loadRouteGroups() ([]byte, error) {
@@ -13,23 +40,46 @@ func (c stringClient) loadRouteGroups() ([]byte, error) {
 }
 
 func TestTransformRouteGroups(t *testing.T) {
-	const allRouteGroupsJSON = `{"routeGroups": []}`
+	t.Skip()
 
-	dc, err := NewRouteGroupClient(RouteGroupsOptions{
-		Kubernetes: Options{},
-		apiClient:  stringClient(allRouteGroupsJSON),
+	t.Run("empty doc", func(t *testing.T) {
+		const allRouteGroupsJSON = `{"items": []}`
+
+		dc, err := NewRouteGroupClient(RouteGroupsOptions{
+			Kubernetes: Options{},
+			apiClient:  stringClient(allRouteGroupsJSON),
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		r, err := dc.LoadAll()
+		if err != nil {
+			t.Error("data client failed to convert an empty route group document", err)
+		}
+
+		if len(r) != 0 {
+			t.Error("data client returned unexpected routes")
+			t.Log(eskip.String(r...))
+		}
 	})
-	if err != nil {
-		t.Fatal(err)
-	}
 
-	r, err := dc.LoadAll()
-	if err != nil {
-		t.Error("data client failed to convert an empty route group document", err)
-	}
+	t.Run("single route gropu", func(t *testing.T) {
+		dc, err := NewRouteGroupClient(RouteGroupsOptions{
+			Kubernetes: Options{},
+			apiClient:  stringClient(singleRouteGroup),
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	if len(r) != 0 {
-		t.Error("data client returned unexpected routes")
-		t.Log(eskip.String(r...))
-	}
+		r, err := dc.LoadAll()
+		if err != nil {
+			t.Error("data client failed to convert route groups", err)
+		}
+
+		if len(r) != 1 {
+			t.Error("failed to transform a single route")
+		}
+	})
 }
