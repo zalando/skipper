345e1aa0ba4fcb868f8500c323851fb639c52b47
Sandor Szücs <sandor.szuecs@zalando.de>
2019-12-12 18:45:36 +0100

    add example
    Signed-off-by: Sandor Szücs <sandor.szuecs@zalando.de>

---
+++ net/httpclient_example_test.go
@@ -0,0 +1,32 @@
+package net_test
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/lightstep/lightstep-tracer-go"
+	"github.com/zalando/skipper/net"
+)
+
+func ExampleTransport() {
+	quit := make(chan struct{})
+	tracer := lightstep.NewTracer(lightstep.Options{})
+
+	cli := net.NewTransport(net.Options{
+		Tracer: tracer,
+	}, quit)
+	cli = net.WithSpanName(cli, "myspan")
+	cli = net.WithBearerToken(cli, "mytoken")
+
+	u := "http://127.0.0.1:12345/foo"
+	req, err := http.NewRequest("GET", u, nil)
+	if err != nil {
+		log.Fatalf("Failed to create request: %v", err)
+	}
+
+	rsp, err := cli.Do(req)
+	if err != nil {
+		log.Fatalf("Failed to do request: %v", err)
+	}
+	log.Printf("rsp code: %v", rsp.StatusCode)
+}
