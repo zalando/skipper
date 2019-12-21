21c2d541b7352cae8bc4436f3ec24006a4f502f4
Daniel Middlecote <dlmiddlecote@gmail.com>
2019-12-21 21:35:01 +0000

    Forward headers from webhook filter response to continuing request
    Signed-off-by: Daniel Middlecote <dlmiddlecote@gmail.com>

--- filters/auth/webhook.go
+++ filters/auth/webhook.go
@@ -1,11 +1,14 @@
 package auth
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/opentracing/opentracing-go"
 	log "github.com/sirupsen/logrus"
+	"golang.org/x/net/http/httpguts"
 
 	"github.com/zalando/skipper/filters"
 )
@@ -25,7 +28,8 @@ type (
 		options WebhookOptions
 	}
 	webhookFilter struct {
-		authClient *authClient
+		authClient                *authClient
+		forwardResponseHeaderKeys []string
 	}
 )
 
@@ -48,9 +52,10 @@ func (*webhookSpec) Name() string {
 }
 
 // CreateFilter creates an auth filter. The first argument is an URL
-// string.
+// string. The second, optional, argument is a comma separated list of
+// headers to forward from from webhook response.
 //
-//     s.CreateFilter("https://my-auth-service.example.org/auth")
+//     s.CreateFilter("https://my-auth-service.example.org/auth", "X-Auth-User,X-Auth-User-Roles")
 //
 func (ws *webhookSpec) CreateFilter(args []interface{}) (filters.Filter, error) {
 	if l := len(args); l == 0 || l > 2 {
@@ -62,12 +67,32 @@ func (ws *webhookSpec) CreateFilter(args []interface{}) (filters.Filter, error) 
 		return nil, filters.ErrInvalidFilterParameters
 	}
 
+	forwardResponseHeaderKeys := make([]string, 0)
+
+	if len(args) > 1 {
+		// Capture headers that should be forwarded from webhook responses.
+		headerKeysOption, ok := args[1].(string)
+		if !ok {
+			return nil, filters.ErrInvalidFilterParameters
+		}
+
+		headerKeys := strings.Split(headerKeysOption, ",")
+
+		for _, header := range headerKeys {
+			valid := httpguts.ValidHeaderFieldName(header)
+			if !valid {
+				return nil, fmt.Errorf("header %s is invalid", header)
+			}
+			forwardResponseHeaderKeys = append(forwardResponseHeaderKeys, http.CanonicalHeaderKey(header))
+		}
+	}
+
 	ac, err := newAuthClient(s, webhookSpanName, ws.options.Timeout, ws.options.MaxIdleConns, ws.options.Tracer)
 	if err != nil {
 		return nil, filters.ErrInvalidFilterParameters
 	}
 
-	return &webhookFilter{authClient: ac}, nil
+	return &webhookFilter{authClient: ac, forwardResponseHeaderKeys: forwardResponseHeaderKeys}, nil
 }
 
 func copyHeader(to, from http.Header) {
@@ -77,15 +102,22 @@ func copyHeader(to, from http.Header) {
 }
 
 func (f *webhookFilter) Request(ctx filters.FilterContext) {
-	statusCode, err := f.authClient.getWebhook(ctx)
+	resp, err := f.authClient.getWebhook(ctx)
 	if err != nil {
 		log.Errorf("Failed to make authentication webhook request: %v.", err)
 	}
 
 	// errors, redirects, auth errors, webhook errors
-	if err != nil || statusCode >= 300 {
+	if err != nil || resp.StatusCode >= 300 {
 		unauthorized(ctx, "", invalidAccess, f.authClient.url.Hostname(), WebhookName)
 		return
+	}
+
+	// copy required headers from webhook response into the current request
+	for _, hk := range f.forwardResponseHeaderKeys {
+		if h, ok := resp.Header[hk]; ok {
+			ctx.Request().Header[hk] = h
+		}
 	}
 
 	authorized(ctx, WebhookName)
