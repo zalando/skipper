b224909954ae6fc7cf43f564c1e7dd526a928658
Mariano Carballal <mariano.carballal@zalando.de>
2020-02-22 11:51:41 +0100

    Add check for nil body
    Signed-off-by: Mariano Carballal <mariano.carballal@zalando.de>

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -1086,7 +1086,10 @@ func (p *Proxy) do(ctx *context) error {
 		p.log.Debugf("deprecated shunting detected in route: %s", ctx.route.Id)
 		return &proxyError{handled: true}
 	} else if ctx.shunted() || ctx.route.Shunt || ctx.route.BackendType == eskip.ShuntBackend {
-		io.Copy(ioutil.Discard, ctx.request.Body)
+		// consume the body to prevent goroutine leaks
+		if ctx.request.Body != nil {
+			io.Copy(ioutil.Discard, ctx.request.Body)
+		}
 		ctx.ensureDefaultResponse()
 	} else if ctx.route.BackendType == eskip.LoopBackend {
 		loopCTX := ctx.clone()
