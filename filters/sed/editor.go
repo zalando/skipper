0051124ba3c3e6951d907395a77b00a252bde935
Arpad Ryszka <arpad.ryszka@gmail.com>
2020-03-05 10:02:27 +0100

    refactor editor code
    Signed-off-by: Arpad Ryszka <arpad.ryszka@gmail.com>

--- filters/sed/editor.go
+++ filters/sed/editor.go
@@ -7,6 +7,11 @@ import (
 	"regexp"
 )
 
+// TODO:
+// - we need to document that only non-zero matches are replaced
+// - we can document that it is currently not supported, together
+//   with the templating
+
 const (
 	readBufferSize             = 8 << 10
 	defaultMaxEditorBufferSize = 2 << 20
@@ -21,7 +26,7 @@ type editor struct {
 	prefix        []byte
 	ready         []byte
 	pending       []byte
-	pendingMatch  []int
+	pendingMatch  bool
 	readBuffer    []byte
 	err           error
 	closed        bool
@@ -52,218 +57,192 @@ func newEditor(
 	}
 }
 
-func (b *editor) Read(p []byte) (int, error) {
-	if b.closed {
+func (e *editor) editChunk(chunk []byte) {
+	for len(chunk) > 0 {
+		match := e.pattern.FindIndex(chunk)
+		if len(match) == 0 || match[0] == 0 && match[1] == 0 {
+			e.ready = append(e.ready, chunk...)
+			break
+		}
+
+		e.ready = append(e.ready, chunk[:match[0]]...)
+		if match[1] > match[0] {
+			e.ready = append(e.ready, e.replacement...)
+		}
+
+		chunk = chunk[match[1]:]
+	}
+}
+
+func (e *editor) finalize() {
+	if len(e.delimiter) == 0 {
+		if e.pendingMatch {
+			e.ready = e.replacement
+		} else {
+			e.ready = e.pending
+		}
+	} else {
+		e.editChunk(e.pending)
+	}
+
+	e.pending = nil
+	e.pendingMatch = false
+}
+
+func (e *editor) readSource(readSize int) int {
+	var n, count int
+	for i := 0; i < readSize; i++ {
+		n, e.err = e.source.Read(e.readBuffer)
+		e.pending = append(e.pending, e.readBuffer[:n]...)
+		count += n
+		if n == 0 || e.err != nil {
+			break
+		}
+	}
+
+	return count
+}
+
+func (e *editor) editUnbound() bool {
+	var hasProcessed bool
+	for {
+		if len(e.prefix) > 0 && len(e.pending) >= len(e.prefix) {
+			skip := bytes.Index(e.pending, e.prefix)
+			if skip > 0 {
+				e.ready = append(e.ready, e.pending[:skip]...)
+				e.pending = e.pending[skip:]
+				hasProcessed = true
+			}
+		}
+
+		match := e.pattern.FindIndex(e.pending)
+		if len(match) == 0 {
+			e.pendingMatch = false
+			break
+		}
+
+		if match[0] > 0 {
+			hasProcessed = true
+		}
+
+		e.ready = append(e.ready, e.pending[:match[0]]...)
+		e.pending = e.pending[match[0]:]
+		match[1] -= match[0]
+		match[0] = 0
+
+		if match[1] == 0 {
+			e.pendingMatch = false
+			break
+		}
+
+		if match[1] == len(e.pending) {
+			e.pendingMatch = true
+			break
+		}
+
+		e.ready = append(e.ready, e.replacement...)
+		e.pending = e.pending[match[1]:]
+		hasProcessed = true
+	}
+
+	return hasProcessed
+}
+
+func (e *editor) editDelimited() bool {
+	var hasProcessed bool
+	for {
+		endChunk := bytes.Index(e.pending, e.delimiter)
+		if endChunk < 0 {
+			break
+		}
+
+		hasProcessed = true
+		chunk := e.pending[:endChunk+len(e.delimiter)]
+		e.pending = e.pending[len(chunk):]
+		e.editChunk(chunk)
+	}
+
+	return hasProcessed
+}
+
+func (e *editor) trimPending() {
+	if len(e.delimiter) > 0 {
+		e.editChunk(e.pending)
+		e.pending = nil
+		return
+	}
+
+	if e.pendingMatch {
+		e.ready = append(e.ready, e.replacement...)
+		e.pending = nil
+		e.pendingMatch = false
+		return
+	}
+
+	e.ready = append(e.ready, e.pending[:e.maxBufferSize]...)
+	e.pending = e.pending[e.maxBufferSize:]
+}
+
+func (e *editor) Read(p []byte) (int, error) {
+	if e.closed {
 		return 0, ErrClosed
 	}
 
 	var count int
 	readSize := 1
 	for {
-		// If we have something ready, return it.
-		n := copy(p, b.ready)
-		p, b.ready = p[n:], b.ready[n:]
+		n := copy(p, e.ready)
+		p, e.ready = p[n:], e.ready[n:]
 		count += n
 		if len(p) == 0 {
 			return count, nil
 		}
 
-		// If we received an error from the underlying reader, e.g. EOF,
-		// check if we still can return something, or return the error.
-		//
-		// Not mandatory, but let's postpone the error until nothing else
-		// was left to read.
-		//
-		// We can have pending data only in two possible states at this point:
-		//
-		// - no match -> return the data
-		// - match full -> return the replacement
-		//
-		if b.err != nil {
-			if len(b.pending) == 0 {
-				if count > 0 {
-					return count, nil
-				}
-
-				return 0, b.err
+		if e.err != nil {
+			if len(e.pending) > 0 {
+				e.finalize()
+				continue
 			}
 
-			if len(b.delimiter) == 0 {
-				if len(b.pendingMatch) != 0 {
-					b.ready = b.replacement
-				} else {
-					b.ready = b.pending
-				}
-			} else {
-				chunk := b.pending
-				for len(chunk) > 0 {
-					match := b.pattern.FindIndex(chunk)
-					if len(match) == 0 || match[0] == 0 && match[1] == 0 {
-						b.ready = append(b.ready, chunk...)
-						break
-					}
-
-					b.ready = append(b.ready, chunk[:match[0]]...)
-					if match[1] > match[0] {
-						b.ready = append(b.ready, b.replacement...)
-					}
-
-					chunk = chunk[match[1]:]
-				}
+			if count > 0 {
+				return count, nil
 			}
 
-			b.pending = nil
-			b.pendingMatch = nil
-			continue
+			return 0, e.err
 		}
 
-		// There is no more data ready, we need to read more input.
-		var rn, rcount int
-		for i := 0; i < readSize; i++ {
-			rn, b.err = b.source.Read(b.readBuffer)
-			b.pending = append(b.pending, b.readBuffer[:rn]...)
-			rcount += rn
-			if rn == 0 || b.err != nil {
-				break
-			}
-		}
-
-		// If there was no data on the input, we may have received an
-		// error, or the input is simply non-blocking. In the former
-		// case, continue with the read error handling. In the latter
-		// case, return what could read, and leave the decision to the
-		// consumer how to proceed.
-		if rcount == 0 {
-			if b.err != nil {
+		if rcount := e.readSource(readSize); rcount == 0 {
+			if e.err != nil {
 				continue
 			}
 
 			return count, nil
 		}
 
-		// In case we couldn't process any data, we increase the read size
-		// exponentially. This way we can limit the number repeated scans
-		// of the same data from N to logN.
 		readSize *= 2
-
-		if len(b.delimiter) == 0 {
-			for {
-				// Scanning for the known prefix, if there is any, prevents
-				// unnecessary scanning of the pending data when the editor
-				// buffer grows large.
-				if len(b.prefix) > 0 && len(b.pending) >= len(b.prefix) {
-					skip := bytes.Index(b.pending, b.prefix)
-					if skip > 0 {
-						b.ready = append(b.ready, b.pending[:skip]...)
-						b.pending = b.pending[skip:]
-						readSize = 1
-					}
-				}
-
-				b.pendingMatch = b.pattern.FindIndex(b.pending)
-				if len(b.pendingMatch) == 0 {
-					break
-				}
-
-				// We reset the read size to the default, if we know that we
-				// can process some data.
-				if b.pendingMatch[0] > 0 {
-					readSize = 1
-				}
-
-				b.ready = append(b.ready, b.pending[:b.pendingMatch[0]]...)
-				b.pending = b.pending[b.pendingMatch[0]:]
-				b.pendingMatch[1] -= b.pendingMatch[0]
-				b.pendingMatch[0] = 0
-
-				// TODO:
-				// - we need to document that only non-zero matches are replaced
-				// - we can document that it is currently not supported, together
-				//   with the templating
-				if b.pendingMatch[1] == 0 {
-					b.pendingMatch = nil
-					break
-				}
-
-				if b.pendingMatch[1] == len(b.pending) {
-					break
-				}
-
-				b.ready = append(b.ready, b.replacement...)
-				b.pending = b.pending[b.pendingMatch[1]:]
+		if len(e.delimiter) == 0 {
+			if e.editUnbound() {
 				readSize = 1
 			}
 		} else {
-			for {
-				endChunk := bytes.Index(b.pending, b.delimiter)
-				if endChunk < 0 {
-					break
-				}
-
-				// When we have a non-zero delimited chunk, we always process
-				// it before reading more data from the underlying reader.
+			if e.editDelimited() {
 				readSize = 1
-
-				// Include the delimiter, this way it can be controlled
-				// in the expression whether to keep it or not.
-				// TODO: test both cases
-				chunk := b.pending[:endChunk+len(b.delimiter)]
-				b.pending = b.pending[len(chunk):]
-
-				for len(chunk) > 0 {
-					match := b.pattern.FindIndex(chunk)
-					if len(match) == 0 || match[0] == 0 && match[1] == 0 {
-						b.ready = append(b.ready, chunk...)
-						break
-					}
-
-					b.ready = append(b.ready, chunk[:match[0]]...)
-					if match[1] > match[0] {
-						b.ready = append(b.ready, b.replacement...)
-					}
-
-					chunk = chunk[match[1]:]
-				}
 			}
 		}
 
-		for len(b.pending) > b.maxBufferSize {
-			if len(b.delimiter) == 0 {
-				if len(b.pendingMatch) == 0 {
-					b.ready = append(b.ready, b.pending[:b.maxBufferSize]...)
-					b.pending = b.pending[b.maxBufferSize:]
-				} else {
-					b.ready = append(b.ready, b.replacement...)
-					b.pending = nil
-					b.pendingMatch = nil
-				}
-			} else {
-				chunk := b.pending
-				for len(chunk) > 0 {
-					match := b.pattern.FindIndex(chunk)
-					if len(match) == 0 || match[0] == 0 && match[1] == 0 {
-						b.ready = append(b.ready, chunk...)
-						break
-					}
-
-					b.ready = append(b.ready, chunk[:match[0]]...)
-					if match[1] > match[0] {
-						b.ready = append(b.ready, b.replacement...)
-					}
-
-					chunk = chunk[match[1]:]
-				}
-
-				b.pending = nil
+		if len(e.pending) > e.maxBufferSize {
+			readSize = 1
+			for len(e.pending) > e.maxBufferSize {
+				e.trimPending()
 			}
 		}
 	}
 }
 
 // It closed the undelrying reader.
-func (b *editor) Close() error {
-	b.closed = true
-	if c, ok := b.source.(io.Closer); ok {
+func (e *editor) Close() error {
+	e.closed = true
+	if c, ok := e.source.(io.Closer); ok {
 		return c.Close()
 	}
 
