6f1c2f18500beca61fb31dda3f14e615aae69589
Dmitriy Kubyshkin <dmitriy.kubyshkin@zalando.de>
2020-03-24 15:50:19 +0100

    Add `inlineContentIfStatus` filter
    Signed-off-by: Dmitriy Kubyshkin <dmitriy.kubyshkin@zalando.de>

--- filters/builtin/builtin.go
+++ filters/builtin/builtin.go
@@ -46,23 +46,24 @@ const (
 	SetDynamicBackendScheme           = "setDynamicBackendScheme"
 	SetDynamicBackendUrl              = "setDynamicBackendUrl"
 
-	HealthCheckName        = "healthcheck"
-	ModPathName            = "modPath"
-	SetPathName            = "setPath"
-	ModRequestHeaderName   = "modRequestHeader"
-	RedirectToName         = "redirectTo"
-	RedirectToLowerName    = "redirectToLower"
-	StaticName             = "static"
-	StripQueryName         = "stripQuery"
-	PreserveHostName       = "preserveHost"
-	SetFastCgiFilenameName = "setFastCgiFilename"
-	StatusName             = "status"
-	CompressName           = "compress"
-	SetQueryName           = "setQuery"
-	DropQueryName          = "dropQuery"
-	InlineContentName      = "inlineContent"
-	HeaderToQueryName      = "headerToQuery"
-	QueryToHeaderName      = "queryToHeader"
+	HealthCheckName           = "healthcheck"
+	ModPathName               = "modPath"
+	SetPathName               = "setPath"
+	ModRequestHeaderName      = "modRequestHeader"
+	RedirectToName            = "redirectTo"
+	RedirectToLowerName       = "redirectToLower"
+	StaticName                = "static"
+	StripQueryName            = "stripQuery"
+	PreserveHostName          = "preserveHost"
+	SetFastCgiFilenameName    = "setFastCgiFilename"
+	StatusName                = "status"
+	CompressName              = "compress"
+	SetQueryName              = "setQuery"
+	DropQueryName             = "dropQuery"
+	InlineContentName         = "inlineContent"
+	InlineContentIfStatusName = "inlineContentIfStatus"
+	HeaderToQueryName         = "headerToQuery"
+	QueryToHeaderName         = "queryToHeader"
 )
 
 // Returns a Registry object initialized with the default set of filter
@@ -92,6 +93,7 @@ func MakeRegistry() filters.Registry {
 		NewRedirectLower(),
 		NewStripQuery(),
 		NewInlineContent(),
+		NewInlineContentIfStatus(),
 		flowid.New(),
 		PreserveHost(),
 		NewSetFastCgiFilename(),
---
+++ filters/builtin/inlinecontentifstatus.go
@@ -0,0 +1,92 @@
+package builtin
+
+import (
+	"bytes"
+	log "github.com/sirupsen/logrus"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+
+	"github.com/zalando/skipper/filters"
+)
+
+type inlineContentIfStatus struct {
+	statusCode int
+	text       string
+	mime       string
+}
+
+// Creates a filter spec for the inlineContent() filter.
+//
+//     * -> inlineContentIfStatus(401, "{\"foo\": 42}", "application/json") -> "https://www.example.org"
+//
+// It accepts three arguments: the statusCode code to match, the content and the optional content type.
+// When the content type is not set, it tries to detect it using http.DetectContentType.
+//
+// The filter replaces the response coming from the backend or the following filters.
+//
+func NewInlineContentIfStatus() filters.Spec {
+	return &inlineContentIfStatus{}
+}
+
+func (c *inlineContentIfStatus) Name() string { return InlineContentIfStatusName }
+
+func (c *inlineContentIfStatus) CreateFilter(args []interface{}) (filters.Filter, error) {
+	if len(args) < 2 || len(args) > 3 {
+		return nil, filters.ErrInvalidFilterParameters
+	}
+
+	var (
+		f  inlineContentIfStatus
+		ok bool
+	)
+
+	f.statusCode, ok = args[0].(int)
+	if !ok {
+		floatStatusCode, ok := args[0].(float64)
+		if !ok {
+			return nil, filters.ErrInvalidFilterParameters
+		}
+		f.statusCode = int(floatStatusCode)
+	}
+
+	if f.statusCode < 100 || f.statusCode >= 600 {
+		return nil, filters.ErrInvalidFilterParameters
+	}
+
+	f.text, ok = args[1].(string)
+	if !ok {
+		return nil, filters.ErrInvalidFilterParameters
+	}
+
+	if len(args) == 3 {
+		f.mime, ok = args[2].(string)
+		if !ok {
+			return nil, filters.ErrInvalidFilterParameters
+		}
+	} else {
+		f.mime = http.DetectContentType([]byte(f.text))
+	}
+
+	return &f, nil
+}
+
+func (c *inlineContentIfStatus) Request(filters.FilterContext) {}
+
+func (c *inlineContentIfStatus) Response(ctx filters.FilterContext) {
+	if ctx.Response().StatusCode != c.statusCode {
+		return
+	}
+	rsp := ctx.Response()
+
+	err := rsp.Body.Close()
+	if err != nil {
+		log.Error(err)
+	}
+
+	contentLength := len(c.text)
+	rsp.ContentLength = int64(contentLength)
+	rsp.Header.Set("Content-Type", c.mime)
+	rsp.Header.Set("Content-Length", strconv.Itoa(contentLength))
+	rsp.Body = ioutil.NopCloser(bytes.NewBufferString(c.text))
+}
