921ba20095d22d572382c9b224b639582b4f665c
Arpad Ryszka <arpad.ryszka@gmail.com>
2020-04-02 10:50:33 +0200

    create filter for setting the X-Forwarded-* headers (#1383)
    * create filter for setting the X-Forwarded-* headers
    
    Signed-off-by: Arpad Ryszka <arpad.ryszka@gmail.com>
    
    * add the xforward filters to the registry
    
    Signed-off-by: Arpad Ryszka <arpad.ryszka@gmail.com>
    
    * review fixes: missing doc, better variable naming
    
    Signed-off-by: Arpad Ryszka <arpad.ryszka@gmail.com>

--- filters/builtin/builtin.go
+++ filters/builtin/builtin.go
@@ -19,6 +19,7 @@ import (
 	"github.com/zalando/skipper/filters/sed"
 	"github.com/zalando/skipper/filters/tee"
 	"github.com/zalando/skipper/filters/tracing"
+	"github.com/zalando/skipper/filters/xforward"
 	"github.com/zalando/skipper/script"
 )
 
@@ -103,6 +104,8 @@ func MakeRegistry() filters.Registry {
 		NewInlineContent(),
 		NewInlineContentIfStatus(),
 		flowid.New(),
+		xforward.New(),
+		xforward.NewFirst(),
 		PreserveHost(),
 		NewSetFastCgiFilename(),
 		NewStatus(),
