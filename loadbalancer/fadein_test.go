e0202700131b7fb760edbdc0b38f8a193b98673f
Sandor Szücs <sandor.szuecs@zalando.de>
2020-07-27 16:46:22 +0200

    CSV output for lb algorithm fadein tests
    Signed-off-by: Sandor Szücs <sandor.szuecs@zalando.de>

--- loadbalancer/fadein_test.go
+++ loadbalancer/fadein_test.go
@@ -23,78 +23,87 @@ const (
 
 func testFadeIn(
 	t *testing.T,
+	name string,
 	algorithm func([]string) routing.LBAlgorithm,
 	endpointAges ...time.Duration,
 ) {
-	var detectionTimes []time.Time
-	now := time.Now()
-	for _, ea := range endpointAges {
-		detectionTimes = append(detectionTimes, now.Add(-ea))
-	}
+	t.Run(name, func(t *testing.T) {
 
-	var ep []string
-	for i := range endpointAges {
-		ep = append(ep, string('a'+i))
-	}
+		var detectionTimes []time.Time
+		now := time.Now()
+		for _, ea := range endpointAges {
+			detectionTimes = append(detectionTimes, now.Add(-ea))
+		}
 
-	a := algorithm(ep)
+		var ep []string
+		for i := range endpointAges {
+			ep = append(ep, string('a'+i))
+		}
 
-	ctx := &routing.LBContext{
-		Route: &routing.Route{
-			LBFadeInDuration: fadeInDuration,
-			LBFadeInEase:     1,
-		},
-	}
+		a := algorithm(ep)
 
-	for i := range ep {
-		ctx.Route.LBEndpoints = append(ctx.Route.LBEndpoints, routing.LBEndpoint{
-			Host:     ep[i],
-			Detected: detectionTimes[i],
-		})
-	}
+		ctx := &routing.LBContext{
+			Route: &routing.Route{
+				LBFadeInDuration: fadeInDuration,
+				LBFadeInEase:     1,
+			},
+		}
 
-	t.Log("test start", time.Now())
-	var stats []string
-	stop := time.After(fadeInDuration)
-	func() {
-		for {
-			// try with copy-on-write
-			ep := a.Apply(ctx)
-			stats = append(stats, ep.Host)
-			select {
-			case <-stop:
-				return
-			default:
+		for i := range ep {
+			ctx.Route.LBEndpoints = append(ctx.Route.LBEndpoints, routing.LBEndpoint{
+				Host:     ep[i],
+				Detected: detectionTimes[i],
+			})
+		}
+
+		t.Log("test start", time.Now())
+		var stats []string
+		stop := time.After(fadeInDuration)
+		func() {
+			for {
+				// try with copy-on-write
+				ep := a.Apply(ctx)
+				stats = append(stats, ep.Host)
+				select {
+				case <-stop:
+					return
+				default:
+				}
 			}
-		}
-	}()
+		}()
 
-	t.Log("test done", time.Now())
-	bucketSize := len(stats) / bucketCount
-	for i := 0; i < bucketCount; i++ {
-		bucketStats := make(map[string]int)
-		for j := i * bucketSize; j < (i+1)*bucketSize; j++ {
-			bucketStats[stats[j]]++
-		}
-
-		var showStats []string
+		t.Log("test done", time.Now())
+		var header []string
 		for _, epi := range ep {
-			showStats = append(showStats, fmt.Sprintf("%s=%d", epi, bucketStats[epi]))
+			header = append(header, epi)
 		}
+		t.Log("CSV " + strings.Join(header, ","))
+		bucketSize := len(stats) / bucketCount
+		for i := 0; i < bucketCount; i++ {
+			bucketStats := make(map[string]int)
+			for j := i * bucketSize; j < (i+1)*bucketSize; j++ {
+				bucketStats[stats[j]]++
+			}
 
-		t.Log(strings.Join(showStats, ", "))
-		// TODO: verify results
-	}
+			var showStats []string
+			for _, epi := range ep {
+				showStats = append(showStats, fmt.Sprintf("%d", bucketStats[epi]))
+			}
+
+			t.Log("CSV " + strings.Join(showStats, ","))
+			// TODO: verify results
+		}
+	})
 }
 
 func TestFadeIn(t *testing.T) {
 	old := 2 * fadeInDuration
 	// testFadeIn(t, newRoundRobin, , old)
 	// testFadeIn(t, newRoundRobin, 0, 0)
-	testFadeIn(t, newRoundRobin, old, 0)
-	testFadeIn(t, newRoundRobin, old, old, old, 0)
-	testFadeIn(t, newRoundRobin, old, old, old, 0, 0, 0)
-	testFadeIn(t, newRoundRobin, old, 0, 0, 0)
+	testFadeIn(t, "1st", newRoundRobin, old, 0)
+	testFadeIn(t, "2nd", newRoundRobin, old, old, old, 0)
+	testFadeIn(t, "3rd", newRoundRobin, old, old, old, 0, 0, 0)
+	testFadeIn(t, "4th", newRoundRobin, old, 0, 0, 0)
 
 	// testFadeIn(t, newRandom, old, old)
 	// testFadeIn(t, newRandom, 0, 0)
