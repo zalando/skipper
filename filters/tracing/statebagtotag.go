daf9862459a3a50bbc7d701246391016cc60998b
Gregor Zeitlinger <gregor.zeitlinger@zalando.de>
2020-08-27 13:10:19 +0200

    add state bag to tag filter (#1512)
    Signed-off-by: Gregor Zeitlinger <gregor.zeitlinger@zalando.de>

---
+++ filters/tracing/statebagtotag.go
@@ -0,0 +1,67 @@
+package tracing
+
+import (
+	"fmt"
+
+	"github.com/opentracing/opentracing-go"
+
+	"github.com/zalando/skipper/filters"
+)
+
+const (
+	StateBagToTagFilterName = "stateBagToTag"
+)
+
+type stateBagToTagSpec struct{}
+
+type stateBagToTagFilter struct {
+	stateBagItemName string
+	tagName          string
+}
+
+func (stateBagToTagSpec) Name() string {
+	return StateBagToTagFilterName
+}
+
+func (stateBagToTagSpec) CreateFilter(args []interface{}) (filters.Filter, error) {
+	if len(args) < 1 {
+		return nil, filters.ErrInvalidFilterParameters
+	}
+
+	stateBagItemName, ok := args[0].(string)
+	if !ok || stateBagItemName == "" {
+		return nil, filters.ErrInvalidFilterParameters
+	}
+
+	tagName := stateBagItemName
+	if len(args) > 1 {
+		tagNameArg, ok := args[1].(string)
+		if !ok || tagNameArg == "" {
+			return nil, filters.ErrInvalidFilterParameters
+		}
+		tagName = tagNameArg
+	}
+
+	return stateBagToTagFilter{
+		stateBagItemName: stateBagItemName,
+		tagName:          tagName,
+	}, nil
+}
+
+func NewStateBagToTag() filters.Spec {
+	return stateBagToTagSpec{}
+}
+
+func (f stateBagToTagFilter) Request(ctx filters.FilterContext) {
+	span := opentracing.SpanFromContext(ctx.Request().Context())
+	if span == nil {
+		return
+	}
+	value, ok := ctx.StateBag()[f.stateBagItemName]
+	if !ok {
+		return
+	}
+	span.SetTag(f.tagName, fmt.Sprint(value))
+}
+
+func (stateBagToTagFilter) Response(ctx filters.FilterContext) {}
---
+++ filters/tracing/statebagtotag_test.go
@@ -0,0 +1,75 @@
+package tracing
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/opentracing/opentracing-go"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/zalando/skipper/filters"
+	"github.com/zalando/skipper/filters/filtertest"
+	"github.com/zalando/skipper/tracing/tracingtest"
+)
+
+func TestStateBagToTag(t *testing.T) {
+	req := &http.Request{Header: http.Header{}}
+
+	span := tracingtest.NewSpan("start_span")
+	req = req.WithContext(opentracing.ContextWithSpan(req.Context(), span))
+	ctx := &filtertest.Context{FRequest: req, FStateBag: make(map[string]interface{})}
+	ctx.StateBag()["item"] = "val"
+
+	f, err := NewStateBagToTag().CreateFilter([]interface{}{"item", "tag"})
+	require.NoError(t, err)
+
+	f.Request(ctx)
+
+	assert.Equal(t, "val", span.Tags["tag"])
+}
+
+func TestStateBagToTag_CreateFilter(t *testing.T) {
+	for _, ti := range []struct {
+		msg      string
+		args     []interface{}
+		stateBag string
+		tag      string
+		err      error
+	}{
+		{
+			msg:      "state bag and tag",
+			args:     []interface{}{"state_bag", "tag"},
+			stateBag: "state_bag",
+			tag:      "tag",
+		},
+		{
+			msg:      "only state bag",
+			args:     []interface{}{"state_bag"},
+			stateBag: "state_bag",
+			tag:      "state_bag",
+		},
+		{
+			msg:  "no args",
+			args: []interface{}{},
+			err:  filters.ErrInvalidFilterParameters,
+		},
+		{
+			msg:  "empty arg",
+			args: []interface{}{""},
+			err:  filters.ErrInvalidFilterParameters,
+		},
+	} {
+		t.Run(ti.msg, func(t *testing.T) {
+			f, err := NewStateBagToTag().CreateFilter(ti.args)
+
+			assert.Equal(t, ti.err, err)
+			if err == nil {
+				ff := f.(stateBagToTagFilter)
+
+				assert.Equal(t, ti.stateBag, ff.stateBagItemName)
+				assert.Equal(t, ti.tag, ff.tagName)
+			}
+		})
+	}
+}
