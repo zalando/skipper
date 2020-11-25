bd9c7c748314b1fa45bf7c1c6415c327aecb67bd
Olli Salonen <oasalonen@gmail.com>
2020-11-25 11:26:53 +0100

    Revert "Revive random string generation code."
    This reverts commit 476301f56d9982ce5854580d1ce180679e5a37cb.
    
    Signed-off-by: Olli Salonen <oasalonen@gmail.com>

--- filters/auth/grantflowstate.go
+++ filters/auth/grantflowstate.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"math/rand"
 	"time"
 
 	"github.com/zalando/skipper/secrets"
@@ -25,37 +24,6 @@ type flowState struct {
 }
 
 var errExpiredAuthState = errors.New("expired auth state")
-
-const (
-	secretSize    = 20
-	letterBytes   = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	letterIdxBits = 6                    // 6 bits to represent a letter index
-	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
-	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
-)
-
-var (
-	src = rand.NewSource(time.Now().UnixNano())
-)
-
-// https://stackoverflow.com/questions/22892120/how-to-generate-a-random-string-of-a-fixed-length-in-golang
-func randString(n int) string {
-	b := make([]byte, n)
-	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
-	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
-		if remain == 0 {
-			cache, remain = src.Int63(), letterIdxMax
-		}
-		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
-			b[i] = letterBytes[idx]
-			i--
-		}
-		cache >>= letterIdxBits
-		remain--
-	}
-
-	return string(b)
-}
 
 func newFlowState(secrets *secrets.Registry, secretsFile string) *flowState {
 	return &flowState{
