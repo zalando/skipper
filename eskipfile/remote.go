a1c81110a83aeeea7a8fa9c2b7528b27dd70dfec
Xavi Ivars <xivarsribes@vistaprint.com>
2021-03-09 15:25:53 +0100

    Support for watched remote eskip files (#1735)
    * First draft of remote files
    
    Signed-off-by: Xavier Ivars-Ribes <xivarsribes@vistaprint.com>
    
    * Improve tests
    
    Signed-off-by: Xavier Ivars-Ribes <xivarsribes@vistaprint.com>
    
    * Fix fmt
    
    Signed-off-by: Xavier Ivars-Ribes <xivarsribes@vistaprint.com>
    
    * Some cleanup
    
    Signed-off-by: Xavier Ivars-Ribes <xivarsribes@vistaprint.com>

---
+++ eskipfile/remote.go
@@ -0,0 +1,163 @@
+package eskipfile
+
+import (
+	"errors"
+	"github.com/zalando/skipper/eskip"
+	"github.com/zalando/skipper/routing"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type remoteEskipFile struct {
+	preloaded       bool
+	remotePath      string
+	localPath       string
+	eskipFileClient *WatchClient
+	threshold       int
+	verbose         bool
+}
+
+type RemoteWatchOptions struct {
+	remoteFile    string
+	threshold     int
+	verbose       bool
+	failOnStartup bool
+}
+
+// RemoteWatch creates a route configuration client with (remote) file watching. Watch doesn't follow file system nodes,
+// it always reads (or re-downloads) from the file identified by the initially provided file name.
+func RemoteWatch(o *RemoteWatchOptions) (routing.DataClient, error) {
+	if !isFileRemote(o.remoteFile) {
+		return Watch(o.remoteFile), nil
+	}
+
+	tempFilename, err := ioutil.TempFile("", "routes")
+
+	if err != nil {
+		return nil, err
+	}
+
+	dataClient := &remoteEskipFile{
+		remotePath: o.remoteFile,
+		localPath:  tempFilename.Name(),
+		threshold:  o.threshold,
+		verbose:    o.verbose,
+	}
+
+	if o.failOnStartup {
+		err = dataClient.DownloadRemoteFile()
+
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		f, err := os.OpenFile(tempFilename.Name(), os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+		if err == nil {
+			err = f.Close()
+		}
+
+		if err != nil {
+			return nil, err
+		}
+		dataClient.preloaded = true
+	}
+
+	dataClient.eskipFileClient = Watch(tempFilename.Name())
+
+	return dataClient, nil
+}
+
+// LoadAll returns the parsed route definitions found in the file.
+func (client *remoteEskipFile) LoadAll() ([]*eskip.Route, error) {
+
+	var err error = nil
+
+	if client.preloaded {
+		client.preloaded = false
+	} else {
+		err = client.DownloadRemoteFile()
+	}
+
+	if err != nil {
+		log.Errorf("LoadAll from remote %s failed. Continue using the last loaded routes", client.remotePath)
+		return nil, err
+	}
+
+	if client.verbose {
+		log.Debugf("New routes file %s was downloaded", client.remotePath)
+	}
+
+	return client.eskipFileClient.LoadAll()
+}
+
+// LoadUpdate returns differential updates when a remote file has changed.
+func (client *remoteEskipFile) LoadUpdate() ([]*eskip.Route, []string, error) {
+
+	err := client.DownloadRemoteFile()
+
+	if err != nil {
+		log.Errorf("LoadUpdate from remote %s failed. Trying to LoadAll", client.remotePath)
+		return nil, nil, err
+	}
+
+	newRoutes, deletedRoutes, err := client.eskipFileClient.LoadUpdate()
+	if err == nil {
+		if client.verbose {
+			log.Infof("New routes were loaded. New: %d; deleted: %d", len(newRoutes), len(deletedRoutes))
+
+			if client.threshold >= 0 {
+				if len(newRoutes) > client.threshold || len(deletedRoutes) > client.threshold {
+					log.Warnf("Significant amount of routes was updated. New: %d; deleted: %d", len(newRoutes), len(deletedRoutes))
+				}
+			}
+		}
+	} else {
+		log.Errorf("RemoteEskipFile LoadUpdate %s failed. Skipper continues to serve the last successfully updated routes. Error: %s",
+			client.remotePath, err)
+	}
+
+	return newRoutes, deletedRoutes, err
+}
+
+func isFileRemote(remotePath string) bool {
+	return strings.HasPrefix(remotePath, "http://") || strings.HasPrefix(remotePath, "https://")
+}
+
+func (client *remoteEskipFile) DownloadRemoteFile() error {
+
+	data, err := client.getRemoteData()
+	if err != nil {
+		return err
+	}
+	defer data.Close()
+
+	out, err := os.OpenFile(client.localPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		return err
+	}
+
+	if _, err = io.Copy(out, data); err != nil {
+		_ = out.Close()
+		return err
+	}
+
+	return out.Close()
+}
+
+func (client *remoteEskipFile) getRemoteData() (io.ReadCloser, error) {
+
+	resp, err := http.Get(client.remotePath)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != 200 {
+		return nil, errors.New("download file failed")
+	}
+
+	return resp.Body, nil
+}
---
+++ eskipfile/remote_test.go
@@ -0,0 +1,178 @@
+package eskipfile
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+
+	"github.com/sanity-io/litter"
+	"github.com/zalando/skipper/eskip"
+)
+
+const routeBody string = `Path("/") -> setPath("/homepage/") -> "https://example.com/"`
+
+func TestIsRemoteFile(t *testing.T) {
+	for _, test := range []struct {
+		title    string
+		file     string
+		expected bool
+	}{
+		{
+			title:    "HTTP file",
+			file:     "http://example.com/foo",
+			expected: true,
+		},
+		{
+			title:    "HTTPS file",
+			file:     "https://example.com/foo",
+			expected: true,
+		},
+		{
+			title:    "Windows file",
+			file:     "c:\folder\foo",
+			expected: false,
+		},
+		{
+			title:    "UNIX file",
+			file:     "/var/tmp/foo",
+			expected: false,
+		},
+	} {
+		t.Run(test.title, func(t *testing.T) {
+			result := isFileRemote(test.file)
+
+			if result != test.expected {
+				t.Error("isRemoteFile failed")
+				t.Log(test)
+			}
+		})
+	}
+}
+
+func TestLoadAll(t *testing.T) {
+
+	for _, test := range []struct {
+		title           string
+		routeContent    string
+		routeStatusCode int
+		expected        []*eskip.Route
+		fail            bool
+	}{{
+		title:           "Download not existing remote file fails in NewRemoteEskipFile",
+		routeContent:    "",
+		routeStatusCode: 404,
+		fail:            true,
+	}, {
+		title:           "Download valid remote file",
+		routeContent:    fmt.Sprintf("VALID: %v;", routeBody),
+		routeStatusCode: 200,
+		expected: []*eskip.Route{{
+			Id:   "VALID",
+			Path: "/",
+			Filters: []*eskip.Filter{{
+				Name: "setPath",
+				Args: []interface{}{
+					"/homepage/",
+				},
+			}},
+			BackendType: eskip.NetworkBackend,
+			Shunt:       false,
+			Backend:     "https://example.com/",
+		}},
+	},
+	} {
+		s := createTestServer(test.routeContent, test.routeStatusCode)
+		defer s.Close()
+
+		t.Run(test.title, func(t *testing.T) {
+			options := &RemoteWatchOptions{remoteFile: s.URL, threshold: 10, verbose: true, failOnStartup: true}
+			client, err := RemoteWatch(options)
+			if err == nil && test.fail {
+				t.Error("failed to fail")
+				return
+			} else if err != nil && !test.fail {
+				t.Error(err)
+				return
+			} else if test.fail {
+				return
+			}
+
+			r, err := client.LoadAll()
+			if err != nil {
+				t.Error(err)
+				return
+			}
+
+			if len(r) == 0 {
+				r = nil
+			}
+
+			if !reflect.DeepEqual(r, test.expected) {
+				t.Error("invalid routes received")
+				t.Log("got:     ", litter.Sdump(r))
+				t.Log("expected:", litter.Sdump(test.expected))
+			}
+		})
+	}
+}
+
+func TestLoadAllAndUpdate(t *testing.T) {
+
+	for _, test := range []struct {
+		title               string
+		validRouteContent   string
+		invalidRouteContent string
+		expectedToFail      bool
+		fail                bool
+	}{{
+		title:               "Download invalid update and all routes returns routes nil",
+		validRouteContent:   fmt.Sprintf("VALID: %v;", routeBody),
+		invalidRouteContent: fmt.Sprintf("MISSING_SEMICOLON: %v\nVALID: %v;", routeBody, routeBody),
+		expectedToFail:      true,
+	},
+	} {
+		t.Run(test.title, func(t *testing.T) {
+
+			testValidServer := createTestServer(test.validRouteContent, 200)
+			defer testValidServer.Close()
+
+			options := &RemoteWatchOptions{remoteFile: testValidServer.URL, threshold: 10, verbose: true, failOnStartup: true}
+			client, err := RemoteWatch(options)
+			if err == nil && test.fail {
+				t.Error("failed to fail")
+				return
+			} else if err != nil && !test.fail {
+				t.Error(err)
+				return
+			} else if test.fail {
+				return
+			}
+
+			testInvalidServer := createTestServer(test.invalidRouteContent, 200)
+			defer testInvalidServer.Close()
+
+			client.(*remoteEskipFile).remotePath = testInvalidServer.URL
+			_, _, err = client.LoadUpdate()
+			if test.expectedToFail && err == nil {
+				t.Error(err)
+				return
+			}
+
+			_, err = client.LoadAll()
+			if test.expectedToFail && err == nil {
+				t.Error(err)
+				return
+			}
+		})
+	}
+}
+
+func createTestServer(c string, sc int) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(sc)
+		io.WriteString(w, c)
+	}))
+}
