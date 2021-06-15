c86256acc2e66cb28ff45a9a1eeb2f0beb1bbacb
Sandor Szücs <sandor.szuecs@zalando.de>
2021-06-15 17:12:39 +0200

    add comments to tune lua Options
    Signed-off-by: Sandor Szücs <sandor.szuecs@zalando.de>

--- script/script.go
+++ script/script.go
@@ -30,7 +30,15 @@ const (
 )
 
 func newState() (*lua.LState, error) {
-	L := lua.NewState()
+	L := lua.NewState(lua.Options{
+		// TODO: can be tuned: https://github.com/yuin/gopher-lua#registry
+		RegistrySize:     1024 * 20, // this is the initial size of the registry
+		RegistryMaxSize:  0,         //1024 * 80, // this is the maximum size that the registry can grow to. If set to `0` (the default) then the registry will not auto grow
+		RegistryGrowStep: 32,        // this is how much to step up the registry by each time it runs out of space. The default is `32`.
+		// TODO: can be tuned: https://github.com/yuin/gopher-lua#callstack
+		CallStackSize:       120,   // this is the maximum callstack size of this LState
+		MinimizeStackMemory: false, // Defaults to `false` if not specified. If set, the callstack will auto grow and shrink as needed up to a max of `CallStackSize`. If not set, the callstack will be fixed at `CallStackSize`.
+	})
 	L.PreloadModule("base64", base64.Loader)
 	L.PreloadModule("http", gluahttp.NewHttpModule(&http.Client{}).Loader)
 	L.PreloadModule("url", gluaurl.Loader)
