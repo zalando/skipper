67fb6115c3256173c1f607e90dd8aaf4f7f94684
Girish Chandrashekar <girish.chandrashekar@zalando.de>
2021-08-31 10:41:18 +0200

    Add unit tests for eastwest route ingress creation
    Signed-off-by: Girish Chandrashekar <girish.chandrashekar@zalando.de>

---
+++ dataclients/kubernetes/eastwest_test.go
@@ -0,0 +1,82 @@
+package kubernetes
+
+import (
+	"github.com/zalando/skipper/eskip"
+	"reflect"
+	"testing"
+)
+
+func TestCreateEastWestRouteIng(t *testing.T) {
+	type args struct {
+		eastWestDomainRegexpPostfix string
+		hostname                    string
+		namespace                   string
+		route                       *eskip.Route
+	}
+	tests := []struct {
+		name string
+		args args
+		want *eskip.Route
+	}{
+		{
+			name: "return nil if the route Id is prefixed with 'kubeew'",
+			args: args{
+				eastWestDomainRegexpPostfix: "yyy",
+				hostname:                    "serviceA",
+				namespace:                   "A",
+				route: &eskip.Route{
+					Id: "kubeew_foo__qux_a_0__www2_example_org_____",
+				},
+			},
+			want: nil,
+		},
+		{
+			name: "return nil if the namespace is empty",
+			args: args{
+				eastWestDomainRegexpPostfix: "yyy",
+				hostname:                    "serviceA",
+				namespace:                   "",
+				route: &eskip.Route{
+					Id: "kube_foo__qux__www3_example_org___a_path__bar",
+				},
+			},
+			want: nil,
+		},
+		{
+			name: "return nil if the hostname is empty",
+			args: args{
+				eastWestDomainRegexpPostfix: "yyy",
+				hostname:                    "",
+				namespace:                   "A",
+				route: &eskip.Route{
+					Id: "kube_foo__qux__www3_example_org___a_path__bar",
+				},
+			},
+			want: nil,
+		},
+		{
+			name: "return the route with modified route Id and HostRegexp",
+			args: args{
+				eastWestDomainRegexpPostfix: "[.]cluster[.]local",
+				hostname:                    "serviceA",
+				namespace:                   "default",
+				route: &eskip.Route{
+					Id:          "kube_foo__qux__www3_example_org___a_path__bar",
+					HostRegexps: []string{"www2[.]example[.]org"},
+				},
+			},
+			want: &eskip.Route{
+				Id:          "kubeew_foo__qux__www3_example_org___a_path__bar",
+				HostRegexps: []string{"^serviceA[.]default[.]cluster[.]local(:[0-9]+)?$"},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			eastWestRouteIng := createEastWestRouteIng(tt.args.eastWestDomainRegexpPostfix, tt.args.hostname, tt.args.namespace, tt.args.route)
+			if !reflect.DeepEqual(eastWestRouteIng, tt.want) {
+				t.Errorf("createEastWestRouteIng() = %v, want %v", eastWestRouteIng, tt.want)
+			}
+		})
+	}
+}
