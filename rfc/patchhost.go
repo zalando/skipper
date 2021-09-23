e2d751fccced5643031db9d0df11d79b6b91c19a
Sandor Szücs <sandor.szuecs@zalando.de>
2021-09-23 20:31:19 +0200

    add filter rfcHost() that removes the optional trailing dot from the incoming request to the ougoing request
    Signed-off-by: Sandor Szücs <sandor.szuecs@zalando.de>

---
+++ rfc/patchhost.go
@@ -0,0 +1,10 @@
+package rfc
+
+import "strings"
+
+// PatchHost returns a host string without trailing dot. For details
+// see also the discussion in
+// https://lists.w3.org/Archives/Public/ietf-http-wg/2016JanMar/0430.html.
+func PatchHost(host string) string {
+	return strings.TrimSuffix(host, ".")
+}
