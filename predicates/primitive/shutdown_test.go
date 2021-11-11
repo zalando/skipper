830a6b3029a099bc0efeae3d2b1db053eb7ae381
Alexander Yastrebov <alexander.yastrebov@zalando.de>
2021-11-11 17:31:51 +0100

    Adds `Shutdown` predicate
    `Shutdown` predicate evaluates to true if Skipper is shutting down after receiving
    `SIGTERM` signal and can be used to implement customized healthcheck.
    
    Updates #1899
    
    Signed-off-by: Alexander Yastrebov <alexander.yastrebov@zalando.de>

---
+++ predicates/primitive/shutdown_test.go
@@ -0,0 +1,28 @@
+package primitive
+
+import (
+	"net/http"
+	"syscall"
+	"testing"
+	"time"
+)
+
+func TestShutdown(t *testing.T) {
+	s, sigs := newShutdown()
+	p, err := s.Create([]interface{}{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	req, _ := http.NewRequest("GET", "https://www.example.org", nil)
+
+	if p.Match(req) {
+		t.Error("unexpected shutdown")
+	}
+
+	sigs <- syscall.SIGTERM
+	time.Sleep(100 * time.Millisecond)
+
+	if !p.Match(req) {
+		t.Error("expected shutdown")
+	}
+}
