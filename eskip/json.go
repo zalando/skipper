b90592f5bd8c7a217e5e59f5b99be69ce3901054
Marcin Zaremba <marcin.zaremba@zalando.de>
2021-11-29 10:49:47 +0100

    Get rid of unneeded stuff
    Signed-off-by: Marcin Zaremba <marcin.zaremba@zalando.de>
    Signed-off-by: Arpad Ryszka <arpad.ryszka@gmail.com>

--- eskip/json.go
+++ eskip/json.go
@@ -5,105 +5,96 @@ import (
 	"encoding/json"
 )
 
-func marshalJSONPredicates(r *Route) []*Predicate {
-	rjf := make([]*Predicate, 0, len(r.Predicates))
-
-	if r.Method != "" {
-		rjf = append(rjf, &Predicate{
-			Name: "Method",
-			Args: []interface{}{r.Method},
-		})
-	}
-
-	if r.Path != "" {
-		rjf = append(rjf, &Predicate{
-			Name: "Path",
-			Args: []interface{}{r.Path},
-		})
-	}
-
-	for _, h := range r.HostRegexps {
-		rjf = append(rjf, &Predicate{
-			Name: "HostRegexp",
-			Args: []interface{}{h},
-		})
-	}
-
-	for _, p := range r.PathRegexps {
-		rjf = append(rjf, &Predicate{
-			Name: "PathRegexp",
-			Args: []interface{}{p},
-		})
-	}
-
-	for k, v := range r.Headers {
-		rjf = append(rjf, &Predicate{
-			Name: "Header",
-			Args: []interface{}{k, v},
-		})
-	}
-
-	for k, list := range r.HeaderRegexps {
-		for _, v := range list {
-			rjf = append(rjf, &Predicate{
-				Name: "HeaderRegexp",
-				Args: []interface{}{k, v},
-			})
-		}
-	}
-
-	rjf = append(rjf, r.Predicates...)
-
-	return rjf
+type jsonNameArgs struct {
+	Name string        `json:"name"`
+	Args []interface{} `json:"args"`
 }
 
-func marshalNameArgs(name string, args []interface{}) ([]byte, error) {
+func newJSONNameArgs(name string, args []interface{}) *jsonNameArgs {
 	if args == nil {
 		args = []interface{}{}
 	}
 
-	return json.Marshal(&struct {
-		Name string        `json:"name"`
-		Args []interface{} `json:"args"`
-	}{
-		Name: name,
-		Args: args,
-	})
+	return &jsonNameArgs{Name: name, Args: args}
 }
 
-func (f *Filter) MarshalJSON() ([]byte, error) {
-	return marshalNameArgs(f.Name, f.Args)
+type jsonBackend struct {
+	Type      string   `json:"type"`
+	Address   string   `json:"address,omitempty"`
+	Algorithm string   `json:"algorithm,omitempty"`
+	Endpoints []string `json:"endpoints,omitempty"`
 }
 
-func (p *Predicate) MarshalJSON() ([]byte, error) {
-	return marshalNameArgs(p.Name, p.Args)
+type jsonRoute struct {
+	ID         string       `json:"id"`
+	Backend    *jsonBackend `json:"backend"`
+	Predicates []*Predicate `json:"predicates"`
+	Filters    []*Filter    `json:"filters"`
 }
 
-func (r *Route) MarshalJSON() ([]byte, error) {
-	backend := r.backendString()
+func newJSONRoute(r *Route) *jsonRoute {
+	cr := Canonical(r)
 
-	filters := r.Filters
-	if filters == nil {
-		filters = []*Filter{}
+	return &jsonRoute{
+		ID: cr.Id,
+		Backend: &jsonBackend{
+			Type:      cr.BackendType.String(),
+			Address:   cr.Backend,
+			Algorithm: cr.LBAlgorithm,
+			Endpoints: cr.LBEndpoints,
+		},
+		Predicates: cr.Predicates,
+		Filters:    cr.Filters,
 	}
+}
 
+func marshalJSONNoEscape(v interface{}) ([]byte, error) {
 	var buf bytes.Buffer
 	e := json.NewEncoder(&buf)
 	e.SetEscapeHTML(false)
 
-	if err := e.Encode(&struct {
-		Id         string       `json:"id"`
-		Backend    string       `json:"backend"`
-		Predicates []*Predicate `json:"predicates"`
-		Filters    []*Filter    `json:"filters"`
-	}{
-		Id:         r.Id,
-		Backend:    backend,
-		Predicates: marshalJSONPredicates(r),
-		Filters:    filters,
-	}); err != nil {
+	if err := e.Encode(v); err != nil {
 		return nil, err
 	}
 
 	return buf.Bytes(), nil
+}
+
+func (f *Filter) MarshalJSON() ([]byte, error) {
+	return marshalJSONNoEscape(newJSONNameArgs(f.Name, f.Args))
+}
+
+func (p *Predicate) MarshalJSON() ([]byte, error) {
+	return marshalJSONNoEscape(newJSONNameArgs(p.Name, p.Args))
+}
+
+func (r *Route) MarshalJSON() ([]byte, error) {
+	return marshalJSONNoEscape(newJSONRoute(r))
+}
+
+func (r *Route) UnmarshalJSON(b []byte) error {
+	jr := &jsonRoute{}
+	if err := json.Unmarshal(b, jr); err != nil {
+		return err
+	}
+
+	r.Id = jr.ID
+
+	bt, err := BackendTypeFromString(jr.Backend.Type)
+	if err != nil {
+		return err
+	}
+	r.BackendType = bt
+	switch bt {
+	case NetworkBackend:
+		r.Backend = jr.Backend.Address
+	case LBBackend:
+		r.LBAlgorithm = jr.Backend.Algorithm
+		r.LBEndpoints = jr.Backend.Endpoints
+	}
+
+	r.Filters = jr.Filters
+	r.Predicates = jr.Predicates
+
+	return nil
 }
--- eskip/json_test.go
+++ eskip/json_test.go
@@ -1,12 +1,56 @@
 package eskip
 
 import (
+	"bytes"
 	"encoding/json"
+	"reflect"
 	"testing"
 )
 
-func TestFilterUnmarshalJSON(t *testing.T) {
-	f := &Filter{}
-	json.Unmarshal([]byte(`{"name": "yolo", "args": ["hello"]}`), f)
-	t.Log(f)
+func TestMarshalUnmarshalJSON(t *testing.T) {
+	for _, tc := range []struct {
+		name   string
+		routes []*Route
+		json   string
+	}{
+		// {
+		// 	"network backend",
+		// 	[]*Route{{Id: "test", BackendType: NetworkBackend, Backend: "127.0.0.1"}},
+		// 	`[{"id":"test","backend":{"type":"network","address":"127.0.0.1"},"predicates":[],"filters":[]}]`,
+		// },
+		// {
+		// 	"lb backend",
+		// 	[]*Route{{Id: "test", BackendType: LBBackend, LBAlgorithm: "yolo", LBEndpoints: []string{"localhost"}}},
+		// 	`[{"id":"test","backend":{"type":"lb","algorithm":"yolo","endpoints":["localhost"]},"predicates":[],"filters":[]}]`,
+		// },
+		// {
+		// 	"shunt backend",
+		// 	[]*Route{{Id: "test", BackendType: ShuntBackend}},
+		// 	`[{"id":"test","backend":{"type":"shunt"},"predicates":[],"filters":[]}]`,
+		// },
+		{
+			"shunt backend legacy",
+			[]*Route{{Id: "test", Shunt: true}},
+			`[{"id":"test","backend":{"type":"shunt"},"predicates":[],"filters":[]}]`,
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			gotJSON, err := json.Marshal(tc.routes)
+			if err != nil {
+				t.Fatalf("cannot marshal %v, got error: %v", tc.routes, err)
+			}
+			b := []byte(tc.json)
+			if !bytes.Equal(gotJSON, b) {
+				t.Fatalf("marshal result incorrect, %s != %s", gotJSON, tc.json)
+			}
+			gotRoutes := make([]*Route, 0, len(tc.routes))
+			err = json.Unmarshal(b, &gotRoutes)
+			if err != nil {
+				t.Fatalf("cannot unmarshal %s, got error: %v", gotJSON, err)
+			}
+			if !reflect.DeepEqual(gotRoutes, tc.routes) {
+				t.Fatalf("unmarshal result incorrect, %v != %v", gotRoutes, tc.routes)
+			}
+		})
+	}
 }
