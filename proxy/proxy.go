166d3fecae6bd24dd1d8c8e3a042184df4c8b158
Sandor Szücs <sandor.szuecs@zalando.de>
2021-12-23 17:32:18 +0100

    use req.URL.Host instead of old ctx.route.Backend which is empty on lb backends
    Signed-off-by: Sandor Szücs <sandor.szuecs@zalando.de>

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -950,7 +950,7 @@ func (p *Proxy) makeBackendRequest(ctx *context, requestContext stdlibcontext.Co
 				status = http.StatusServiceUnavailable
 			}
 			p.tracing.setTag(ctx.proxySpan, HTTPStatusCodeTag, uint16(status))
-			return nil, &proxyError{err: fmt.Errorf("net.Error during backend roundtrip to %s: timeout=%v temporary=%v: %w", ctx.route.Backend, nerr.Timeout(), nerr.Temporary(), err), code: status}
+			return nil, &proxyError{err: fmt.Errorf("net.Error during backend roundtrip to %s: timeout=%v temporary=%v: %w", req.URL.Host, nerr.Timeout(), nerr.Temporary(), err), code: status}
 		}
 
 		return nil, &proxyError{err: fmt.Errorf("unexpected error from Go stdlib net/http package during roundtrip: %w", err)}
