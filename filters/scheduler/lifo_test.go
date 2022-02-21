zalando/skipper

commit 711395c50f61200169a2f1449ef2a2659850d4e6

filters/scheduler: add error tag to span on lifo error (#1944)
Signed-off-by: Alexander Yastrebov <alexander.yastrebov@zalando.de>

filters/scheduler/lifo_test.go
@@ -1,16 +1,26 @@
 package scheduler
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"sync"
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
 	"github.com/aryszka/jobqueue"
+	"github.com/opentracing/opentracing-go/mocktracer"
 	"github.com/zalando/skipper/eskip"
 	"github.com/zalando/skipper/filters"
+	"github.com/zalando/skipper/metrics/metricstest"
+	"github.com/zalando/skipper/proxy"
 	"github.com/zalando/skipper/proxy/proxytest"
+	"github.com/zalando/skipper/routing"
+	"github.com/zalando/skipper/routing/testdataclient"
 	"github.com/zalando/skipper/scheduler"
 )
 
@@ -289,4 +299,89 @@ func TestNewLIFO(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestLifoErrors(t *testing.T) {
+	backend := httptest.NewServer(http.HandlerFunc(func(_ http.ResponseWriter, _ *http.Request) {
+		time.Sleep(time.Second)
+	}))
+	defer backend.Close()
+
+	doc := fmt.Sprintf(`aroute: * -> lifo(5, 7, "100ms") -> "%s"`, backend.URL)
+
+	dc, err := testdataclient.NewDoc(doc)
+	require.NoError(t, err)
+
+	metrics := &metricstest.MockMetrics{}
+	reg := scheduler.RegistryWith(scheduler.Options{
+		Metrics:                metrics,
+		EnableRouteLIFOMetrics: true,
+	})
+	defer reg.Close()
+
+	fr := make(filters.Registry)
+	fr.Register(NewLIFO())
+
+	ro := routing.Options{
+		SignalFirstLoad: true,
+		FilterRegistry:  fr,
+		DataClients:     []routing.DataClient{dc},
+		PostProcessors:  []routing.PostProcessor{reg},
+	}
+
+	rt := routing.New(ro)
+	defer rt.Close()
+
+	<-rt.FirstLoad()
+
+	tracer := mocktracer.New()
+	pr := proxy.WithParams(proxy.Params{
+		Routing:     rt,
+		OpenTracing: &proxy.OpenTracingParams{Tracer: tracer},
+	})
+	defer pr.Close()
+
+	ts := httptest.NewServer(pr)
+	defer ts.Close()
+
+	requestSpike(t, 20, ts.URL)
+
+	codes := make(map[uint16]int)
+	for _, span := range tracer.FinishedSpans() {
+		if span.OperationName == "ingress" {
+			code := span.Tag("http.status_code").(uint16)
+			codes[code]++
+			if code >= 500 {
+				assert.Equal(t, true, span.Tag("error"))
+			} else {
+				assert.Nil(t, span.Tag("error"))
+			}
+		}
+	}
+
+	assert.Equal(t, map[uint16]int{
+		// 20 request in total, of which:
+		200: 5, // went straight to the backend
+		502: 7, // were queued and timed out as backend latency is greater than scheduling timeout
+		503: 8, // were refused due to full queue
+	}, codes)
+
+	metrics.WithCounters(func(counters map[string]int64) {
+		assert.Equal(t, int64(7), counters["lifo.aroute.error.timeout"])
+		assert.Equal(t, int64(8), counters["lifo.aroute.error.full"])
+	})
+}
+
+func requestSpike(t *testing.T, n int, url string) {
+	var wg sync.WaitGroup
+	wg.Add(n)
+	for i := 0; i < n; i++ {
+		go func() {
+			rsp, err := http.Get(url)
+			require.NoError(t, err)
+			defer rsp.Body.Close()
+			wg.Done()
+		}()
+	}
+	wg.Wait()
 }

filters/scheduler/lifo.go
@@ -5,6 +5,8 @@ import (
 	"time"
 
 	"github.com/aryszka/jobqueue"
+	"github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/ext"
 	log "github.com/sirupsen/logrus"
 	"github.com/zalando/skipper/filters"
 	"github.com/zalando/skipper/scheduler"
@@ -296,6 +298,9 @@ func request(q *scheduler.Queue, key string, ctx filters.FilterContext) {
 
 	done, err := q.Wait()
 	if err != nil {
+		if span := opentracing.SpanFromContext(ctx.Request().Context()); span != nil {
+			ext.Error.Set(span, true)
+		}
 		switch err {
 		case jobqueue.ErrStackFull:
 			log.Debugf("Failed to get an entry on to the queue to process QueueFull: %v for host %s", err, ctx.Request().Host)

commit 05437e39c36efa246264bb1c0b95dcf1c3df2015

filters/scheduler: wait for spans finishing in lifo errors test
The tests was introduced by #1944
It may happen that not all spans are finished by the time test verifies
response results (because spans are closed after response has been
served).

This change waits for all mock spans to finish and forces scheduler
metrics update.

Signed-off-by: Alexander Yastrebov <alexander.yastrebov@zalando.de>

scheduler/scheduler.go
@@ -409,6 +409,12 @@ func (r *Registry) updateMetrics() {
 	}
 }
 
+func (r *Registry) UpdateMetrics() {
+	if r.options.Metrics != nil {
+		r.updateMetrics()
+	}
+}
+
 // Close closes the registry, including graceful tearing down the stored queues.
 func (r *Registry) Close() {
 	r.mu.Lock()

filters/scheduler/lifo_test.go
@@ -6,6 +6,7 @@ import (
 	"net/http/httptest"
 	"net/url"
 	"sync"
+	"sync/atomic"
 	"testing"
 	"time"
 
@@ -13,6 +14,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/aryszka/jobqueue"
+	"github.com/opentracing/opentracing-go"
 	"github.com/opentracing/opentracing-go/mocktracer"
 	"github.com/zalando/skipper/eskip"
 	"github.com/zalando/skipper/filters"
@@ -301,6 +303,38 @@ func TestNewLIFO(t *testing.T) {
 	}
 }
 
+type testTracer struct {
+	*mocktracer.MockTracer
+	spans int32
+}
+
+func (t *testTracer) Reset() {
+	atomic.StoreInt32(&t.spans, 0)
+	t.MockTracer.Reset()
+}
+
+func (t *testTracer) StartSpan(operationName string, opts ...opentracing.StartSpanOption) opentracing.Span {
+	atomic.AddInt32(&t.spans, 1)
+	return t.MockTracer.StartSpan(operationName, opts...)
+}
+
+func (t *testTracer) FinishedSpans() []*mocktracer.MockSpan {
+	timeout := time.After(1 * time.Second)
+	retry := time.NewTicker(100 * time.Millisecond)
+	defer retry.Stop()
+	for {
+		finished := t.MockTracer.FinishedSpans()
+		if len(finished) == int(atomic.LoadInt32(&t.spans)) {
+			return finished
+		}
+		select {
+		case <-retry.C:
+		case <-timeout:
+			return nil
+		}
+	}
+}
+
 func TestLifoErrors(t *testing.T) {
 	backend := httptest.NewServer(http.HandlerFunc(func(_ http.ResponseWriter, _ *http.Request) {
 		time.Sleep(time.Second)
@@ -334,7 +368,7 @@ func TestLifoErrors(t *testing.T) {
 
 	<-rt.FirstLoad()
 
-	tracer := mocktracer.New()
+	tracer := &testTracer{MockTracer: mocktracer.New()}
 	pr := proxy.WithParams(proxy.Params{
 		Routing:     rt,
 		OpenTracing: &proxy.OpenTracingParams{Tracer: tracer},
@@ -365,6 +399,8 @@ func TestLifoErrors(t *testing.T) {
 		502: 7, // were queued and timed out as backend latency is greater than scheduling timeout
 		503: 8, // were refused due to full queue
 	}, codes)
+
+	reg.UpdateMetrics()
 
 	metrics.WithCounters(func(counters map[string]int64) {
 		assert.Equal(t, int64(7), counters["lifo.aroute.error.timeout"])