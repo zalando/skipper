5e09ac98ce41d039f6075189cdf30600a1fd4ee3
Ricardo Herrera <rickhl@outlook.com>
2022-03-11 23:03:47 +0000

    Use simple key value for cert registry
    Signed-off-by: Ricardo Herrera <rickhl@outlook.com>

--- certregistry/certregistry.go
+++ certregistry/certregistry.go
@@ -2,39 +2,56 @@ package certregistry
 
 import (
 	"crypto/tls"
+	"fmt"
+	"sync"
 
 	log "github.com/sirupsen/logrus"
-	"k8s.io/client-go/tools/cache"
 )
 
 type CertRegistry struct {
-	cache.ThreadSafeStore
+	lookup map[string]*tls.Certificate
+	mx     *sync.Mutex
 }
 
 func NewCertRegistry() *CertRegistry {
 	return &CertRegistry{
-		cache.NewThreadSafeStore(cache.Indexers{}, cache.Indices{}),
+		lookup: make(map[string]*tls.Certificate),
+		mx:     &sync.Mutex{},
 	}
 }
 
-func (r CertRegistry) GetCertByKey(key string) (*tls.Certificate, error) {
+func (r *CertRegistry) GetCertByKey(key string) (*tls.Certificate, error) {
 	var err error
-	cert, ok := r.Get(key)
-	if !ok {
+
+	r.mx.Lock()
+	defer r.mx.Unlock()
+
+	cert, ok := r.lookup[key]
+	if !ok || cert == nil {
 		log.Debugf("certificate not found in store - %s", key)
 		return nil, err
 	}
-	return cert.(*tls.Certificate), nil
+	fmt.Println(cert)
+	return cert, nil
 }
 
-func (r *CertRegistry) SyncCert(key string, cert *tls.Certificate) {
+func (r *CertRegistry) AddCert(key string, cert *tls.Certificate) {
+	r.mx.Lock()
+	defer r.mx.Unlock()
+
+	log.Debugf("adding certificate to registry - %s", key)
+
+	r.lookup[key] = cert
+}
+
+func (r *CertRegistry) SyncCert(key string, cert *tls.Certificate) {	
 	log.Debugf("syncing certificate to registry - %s", key)
 	_, err := r.GetCertByKey(key)
 	if err == nil {
 		log.Debugf("updating certificate in registry - %s", key)
-		r.Update(key, cert)
+		r.AddCert(key, cert)
 		return
 	}
-	log.Debugf("adding certificate to registry - %s", key)
-	r.Add(key, cert)
+	
+	r.AddCert(key, cert)
 }
