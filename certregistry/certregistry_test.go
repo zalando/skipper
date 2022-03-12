9ae02325178417b516c065525d2ef00510158c7b
Ricardo Herrera <rickhl@outlook.com>
2022-03-12 04:14:41 +0000

    Add tests for cert registry
    Signed-off-by: Ricardo Herrera <rickhl@outlook.com>

---
+++ certregistry/certregistry_test.go
@@ -0,0 +1,28 @@
+package certregistry
+
+import (
+	"crypto/tls"
+	"testing"
+)
+
+func TestCertRegistry(t *testing.T) {
+
+	cert := tls.Certificate{}
+
+	newcert := tls.Certificate{}
+
+    t.Run("sync new certificate", func(t *testing.T) {
+		cr := NewCertRegistry()
+		cr.SyncCert("example.org", &cert)
+		_, err := cr.GetCertByKey("example.org")
+		if err != nil {
+			t.Error("failed to read certificate")
+		}
+	})
+
+	t.Run("sync existing certificate", func(t *testing.T) {
+		cr := NewCertRegistry()
+		cr.SyncCert("example.org", &cert)
+		cr.SyncCert("example.org", &newcert)
+	})
+}
