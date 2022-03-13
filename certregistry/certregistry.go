54cb7012939aa777f9cdeaa0c9bca8abfae2f57c
Ricardo Herrera <rickhl@outlook.com>
2022-03-13 20:06:18 +0000

    Use struct for tls certificates
    Using a struct will enable us to save the list of hosts with the cert
    
    Signed-off-by: Ricardo Herrera <rickhl@outlook.com>

--- certregistry/certregistry.go
+++ certregistry/certregistry.go
@@ -13,24 +13,28 @@ var (
 	defaultHost     = "ingress.local"
 )
 
+type tlsCertificate struct {
+	hosts []string
+	cert  *tls.Certificate
+}
+
 type CertRegistry struct {
-	lookup map[string]*tls.Certificate
-	mx     *sync.Mutex
+	lookup         map[string]*tlsCertificate
+	mx             *sync.Mutex
+	defaultTLSCert *tls.Certificate
 }
 
 func NewCertRegistry() *CertRegistry {
-	cert := getFakeHostTLSCert(defaultHost)
-	l := make(map[string]*tls.Certificate)
-	
-	l[defaultHost] = cert
+	l := make(map[string]*tlsCertificate)
 
 	return &CertRegistry{
-		lookup: l,
-		mx:     &sync.Mutex{},
+		lookup:         l,
+		mx:             &sync.Mutex{},
+		defaultTLSCert: getFakeHostTLSCert(defaultHost),
 	}
 }
 
-func (r *CertRegistry) getCertByKey(key string) (*tls.Certificate, error) {
+func (r *CertRegistry) getCertByKey(key string) (*tlsCertificate, error) {
 	r.mx.Lock()
 	defer r.mx.Unlock()
 
@@ -43,30 +47,37 @@ func (r *CertRegistry) getCertByKey(key string) (*tls.Certificate, error) {
 	return cert, nil
 }
 
-func (r *CertRegistry) addCert(key string, cert *tls.Certificate) {
+func (r *CertRegistry) addCertToRegistry(key string, cert *tlsCertificate) {
 	r.mx.Lock()
 	defer r.mx.Unlock()
 
 	r.lookup[key] = cert
 }
 
-func (r *CertRegistry) SyncCert(key string, cert *tls.Certificate) {	
-	log.Debugf("syncing certificate to registry - %s", key)
+func (r *CertRegistry) SyncCert(key string, hosts []string, crt *tls.Certificate) {
+	cert := &tlsCertificate{
+		hosts: hosts, 
+		cert: crt,
+	}	
+
 	_, err := r.getCertByKey(key)
 	if err == nil {
 		log.Debugf("updating certificate in registry - %s", key)
-		r.addCert(key, cert)
+		r.addCertToRegistry(key, cert)
 		return
 	}
 
 	log.Debugf("adding certificate to registry - %s", key)
-	r.addCert(key, cert)
+	r.addCertToRegistry(key, cert)
 }
 
 func (r *CertRegistry) GetCertFromHello(hello *tls.ClientHelloInfo) (*tls.Certificate, error) {
-	cert, err := r.getCertByKey(hello.ServerName)
-	if err != nil {
-		return r.getCertByKey(defaultHost)
+	for _, cert := range r.lookup {
+		for _, host := range cert.hosts {
+			if hello.ServerName == host {
+				return cert.cert, nil
+			}
+		}
 	}
-	return cert, nil
+	return r.defaultTLSCert, nil
 }
--- certregistry/certregistry_test.go
+++ certregistry/certregistry_test.go
@@ -10,45 +10,49 @@ import (
 func TestCertRegistry(t *testing.T) {
 
 	cert := getFakeHostTLSCert("foo.org")
+	hosts := make([]string, 1)
+	hosts[0] = "foo.org"
 
-	newcert := getFakeHostTLSCert("bar.org")
-
-	hello := tls.ClientHelloInfo{
-		ServerName: "example.org",
+	hello := &tls.ClientHelloInfo{
+		ServerName: "foo.org",
 	}
 
     t.Run("sync new certificate", func(t *testing.T) {
 		cr := NewCertRegistry()
-		cr.SyncCert("example.org", cert)
-		_, err := cr.getCertByKey("example.org")
+		cr.SyncCert("foo", hosts, cert)
+		_, err := cr.getCertByKey("foo")
 		if err != nil {
 			t.Error("failed to read certificate")
 		}
 	})
 
 	t.Run("sync existing certificate", func(t *testing.T) {
-		cr := NewCertRegistry()
-		cr.SyncCert("example.org", cert)
-		cr.SyncCert("example.org", newcert)
-	})
+		newcert := getFakeHostTLSCert("bar.org")
+		newhosts := make([]string, 1)
+		newhosts[0] = "foo.org"
 
-	t.Run("get default certificate", func(t *testing.T) {
 		cr := NewCertRegistry()
-		_, err := cr.getCertByKey(defaultHost)
-		if err != nil {
-			t.Error("failed to read certificate")
-		}
+		cr.SyncCert("foo", hosts, cert)
+		cr.SyncCert("foo", newhosts, newcert)
 	})
 
 	t.Run("get non existent cert", func(t *testing.T) {
 		cr := NewCertRegistry()
-		_, err := cr.getCertByKey("foobar.org")
+		_, err := cr.getCertByKey("foobar")
         require.Error(t, err)
+	})
+
+	t.Run("get cert from hello", func(t *testing.T) {
+		cr := NewCertRegistry()
+		_, err := cr.GetCertFromHello(hello)
+		if err != nil {
+			t.Error("failed to read certificate from hello")
+		}
 	})
 
 	t.Run("get default cert from hello", func(t *testing.T) {
 		cr := NewCertRegistry()
-		_, err := cr.GetCertFromHello(&hello)
+		_, err := cr.GetCertFromHello(hello)
 		if err != nil {
 			t.Error("failed to read certificate from hello")
 		}
