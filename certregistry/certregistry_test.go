1a63a4540d2390148448c1aa9e9a676198e9e2f4
Ricardo Herrera <rickhl@outlook.com>
2022-03-13 04:13:07 +0000

    Update tests for cert registry
    Signed-off-by: Ricardo Herrera <rickhl@outlook.com>

--- certregistry/certregistry_test.go
+++ certregistry/certregistry_test.go
@@ -3,17 +3,23 @@ package certregistry
 import (
 	"crypto/tls"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestCertRegistry(t *testing.T) {
 
-	cert := tls.Certificate{}
+	cert := getFakeHostTLSCert("foo.org")
 
-	newcert := tls.Certificate{}
+	newcert := getFakeHostTLSCert("bar.org")
+
+	hello := tls.ClientHelloInfo{
+		ServerName: "example.org",
+	}
 
     t.Run("sync new certificate", func(t *testing.T) {
 		cr := NewCertRegistry()
-		cr.SyncCert("example.org", &cert)
+		cr.SyncCert("example.org", cert)
 		_, err := cr.getCertByKey("example.org")
 		if err != nil {
 			t.Error("failed to read certificate")
@@ -22,8 +28,8 @@ func TestCertRegistry(t *testing.T) {
 
 	t.Run("sync existing certificate", func(t *testing.T) {
 		cr := NewCertRegistry()
-		cr.SyncCert("example.org", &cert)
-		cr.SyncCert("example.org", &newcert)
+		cr.SyncCert("example.org", cert)
+		cr.SyncCert("example.org", newcert)
 	})
 
 	t.Run("get default certificate", func(t *testing.T) {
@@ -31,6 +37,20 @@ func TestCertRegistry(t *testing.T) {
 		_, err := cr.getCertByKey(defaultHost)
 		if err != nil {
 			t.Error("failed to read certificate")
+		}
+	})
+
+	t.Run("get non existent cert", func(t *testing.T) {
+		cr := NewCertRegistry()
+		_, err := cr.getCertByKey("foobar.org")
+        require.Error(t, err)
+	})
+
+	t.Run("get default cert from hello", func(t *testing.T) {
+		cr := NewCertRegistry()
+		_, err := cr.GetCertFromHello(&hello)
+		if err != nil {
+			t.Error("failed to read certificate from hello")
 		}
 	})
 }
