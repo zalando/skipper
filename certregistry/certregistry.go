81a7b76e3e3e449db42dfa1a7a2221b830cec613
Ricardo Herrera <rickhl@outlook.com>
2022-03-14 00:29:33 +0000

    Add comments to exported functions
    Signed-off-by: Ricardo Herrera <rickhl@outlook.com>

--- certregistry/certregistry.go
+++ certregistry/certregistry.go
@@ -18,12 +18,16 @@ type tlsCertificate struct {
 	cert  *tls.Certificate
 }
 
+// CertRegistry object holds TLS certificates to be used to terminate TLS connections
+// We ensure ensure syncronized access to them and hold a default certificate. 
 type CertRegistry struct {
 	lookup         map[string]*tlsCertificate
 	mx             *sync.Mutex
 	defaultTLSCert *tls.Certificate
 }
 
+// NewCertRegistry initializes the certificate registry with an empty map 
+// and a generated default certificate. 
 func NewCertRegistry() *CertRegistry {
 	l := make(map[string]*tlsCertificate)
 
@@ -54,6 +58,8 @@ func (r *CertRegistry) addCertToRegistry(key string, cert *tlsCertificate) {
 	r.lookup[key] = cert
 }
 
+// SyncCert takes a TLS certificate and list of hosts and saves them to the registry with the 
+// provided key. If the object already exists it will be updated or added otherwise.
 func (r *CertRegistry) SyncCert(key string, hosts []string, crt *tls.Certificate) {
 	cert := &tlsCertificate{
 		hosts: hosts, 
@@ -71,6 +77,8 @@ func (r *CertRegistry) SyncCert(key string, hosts []string, crt *tls.Certificate
 	r.addCertToRegistry(key, cert)
 }
 
+// GetCertFromHello reads the SNI from a TLS client and returns the appropriate certificate.
+// If no certificate is found for the host it will return a default certificate.
 func (r *CertRegistry) GetCertFromHello(hello *tls.ClientHelloInfo) (*tls.Certificate, error) {
 	for _, cert := range r.lookup {
 		for _, host := range cert.hosts {
