e10a676e32d4deddb362e6ca75bed061accf62b1
Ricardo Herrera <rickhl@outlook.com>
2022-03-22 18:17:32 +0000

    Return best matching certificate if > 1 found
    Matching SAN names we can return the best matching certificate
    when the same host has been configured for different ingresses.
    
    Signed-off-by: Ricardo Herrera <rickhl@outlook.com>

--- secrets/certregistry/certregistry.go
+++ secrets/certregistry/certregistry.go
@@ -2,13 +2,27 @@ package certregistry
 
 import (
 	"crypto/tls"
+	"crypto/x509"
+	"errors"
+	"strings"
 	"sync"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 )
 
 var (
 	defaultHost = "ingress.local"
+	currentTime = time.Now
+	// ErrNoMatchingCertificateFound is used if there is no matching certificate found
+	ErrNoMatchingCertificateFound = errors.New("no matching certificate found")
+)
+
+const (
+	// used as wildcard char in Cert Hostname/AltName matches
+	glob = "*"
+	// minimal time period for the NotAfter attribute of a Cert to be in the future
+	minimalCertValidityPeriod = 7 * 24 * time.Hour
 )
 
 type tlsCertificate struct {
@@ -85,12 +99,124 @@ func (r *CertRegistry) SyncCert(key string, hosts []string, crt *tls.Certificate
 // GetCertFromHello reads the SNI from a TLS client and returns the appropriate certificate.
 // If no certificate is found for the host it will return a default certificate.
 func (r *CertRegistry) GetCertFromHello(hello *tls.ClientHelloInfo) (*tls.Certificate, error) {
+	var certList = make([]*tlsCertificate, 0)
 	for _, cert := range r.lookup {
 		for _, host := range cert.hosts {
 			if hello.ServerName == host {
-				return cert.cert, nil
+				certList = append(certList, cert)
 			}
 		}
 	}
-	return r.defaultTLSCert, nil
+
+	switch len(certList) {
+	case 0:
+		// no certs found so returning the default certificate
+		return r.defaultTLSCert, nil
+	case 1:
+		// only one certificate found, retuning it
+		return certList[0].cert, nil
+	default:
+		// more that one cert found, returning the best match
+		cert, err := getBestMatchingCertificate(hello.ServerName, certList)
+		if err != nil {
+			return nil, err
+		}
+		return cert, nil
+	}
+}
+
+// getBestMatchingCertificate uses a suffix search, best match operation, in order to find the best matching
+// certificate for a given hostname.
+func getBestMatchingCertificate(host string, certList []*tlsCertificate) (*tls.Certificate, error) {
+	candidate := &tls.Certificate{}
+	longestMatch := -1
+	now := currentTime()
+
+	for _, cert := range certList {
+		curr, err := x509.ParseCertificate(cert.cert.Certificate[0])
+		if err != nil {
+			return nil, err
+		}
+
+		notAfter := curr.NotAfter
+		notBefore := curr.NotBefore
+
+		for _, altName := range curr.DNSNames {
+			if prefixGlob(altName, host) {
+				nameLength := len(altName)
+
+				switch {
+				case longestMatch < 0:
+					// first matching found
+					longestMatch = nameLength
+					candidate = cert.cert
+				case longestMatch < nameLength:
+					if notBefore.Before(now) && notAfter.Add(-minimalCertValidityPeriod).After(now) {
+						// more specific valid cert found: *.example.org -> foo.example.org
+						longestMatch = nameLength
+						candidate = cert.cert
+					}
+				case longestMatch == nameLength:
+					if notBefore.After(candidate.Leaf.NotBefore) &&
+						!notAfter.Add(-minimalCertValidityPeriod).Before(now) {
+						// cert is newer than curBestCert and is not invalid in 7 days
+						longestMatch = nameLength
+						candidate = cert.cert
+					} else if notBefore.Equal(candidate.Leaf.NotBefore) && !candidate.Leaf.NotAfter.After(notAfter) {
+						// cert has the same issue date, but is longer valid
+						longestMatch = nameLength
+						candidate = cert.cert
+					} else if notBefore.Before(candidate.Leaf.NotBefore) &&
+						candidate.Leaf.NotAfter.Add(-minimalCertValidityPeriod).Before(now) &&
+						notAfter.After(candidate.Leaf.NotAfter) {
+						// cert is older than curBestCert but curBestCert is invalid in 7 days and cert is longer valid
+						longestMatch = nameLength
+						candidate = cert.cert
+					}
+				case longestMatch > nameLength:
+					if candidate.Leaf.NotAfter.Add(-minimalCertValidityPeriod).Before(now) &&
+						now.Before(candidate.Leaf.NotBefore) &&
+						notBefore.Before(now) &&
+						now.Before(notAfter.Add(-minimalCertValidityPeriod)) {
+						// foo.example.org -> *.example.org degradation when NotAfter requires a downgrade
+						longestMatch = nameLength
+						candidate = cert.cert
+					}
+				}
+				candidate.Leaf, err = x509.ParseCertificate(candidate.Certificate[0])
+				if err != nil {
+					return nil, err
+				}
+			}
+		}
+	}
+
+	if longestMatch == -1 {
+		return nil, ErrNoMatchingCertificateFound
+	}
+
+	return candidate, nil
+}
+
+func prefixGlob(pattern, subj string) bool {
+	// Empty pattern can only match empty subject
+	if pattern == "" {
+		return subj == pattern
+	}
+
+	// If the pattern _is_ a glob, it matches everything
+	if pattern == glob {
+		return true
+	}
+
+	leadingGlob := strings.HasPrefix(pattern, glob)
+
+	if !leadingGlob {
+		// No globs in pattern, so test for equality
+		return subj == pattern
+	}
+
+	pat := string(pattern[1:])
+	trimmedSubj := strings.TrimSuffix(subj, pat)
+	return !strings.Contains(trimmedSubj, ".")
 }
--- secrets/certregistry/certregistry_test.go
+++ secrets/certregistry/certregistry_test.go
@@ -1,10 +1,149 @@
 package certregistry
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
 	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"fmt"
+	"math/big"
 	"reflect"
+	"sync"
 	"testing"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/require"
 )
+
+const (
+	tenYears = time.Hour * 24 * 365 * 10
+)
+
+type caInfra struct {
+	sync.Once
+	err       error
+	chainKey  *rsa.PrivateKey
+	chainCert *x509.Certificate
+}
+
+func certValidMatchFunction(err error, expect, c *tls.Certificate) bool {
+	return err != nil || c != expect
+}
+
+func certInvalidMatchFunction(err error, expect, c *tls.Certificate) bool {
+	return err == nil && c == expect
+}
+
+type certCondition func(error, *tls.Certificate, *tls.Certificate) bool
+
+var ca = caInfra{}
+
+func createDummyCertDetail(t *testing.T, arn string, altNames []string, notBefore, notAfter time.Time) *tlsCertificate {
+	ca.Do(func() {
+		caKey, err := rsa.GenerateKey(rand.Reader, 2048)
+		if err != nil {
+			ca.err = fmt.Errorf("unable to generate CA key: %v", err)
+			return
+		}
+
+		caCert := x509.Certificate{
+			SerialNumber: big.NewInt(1),
+			Subject: pkix.Name{
+				Organization: []string{"Testing CA"},
+			},
+			NotBefore: time.Time{},
+			NotAfter:  time.Now().Add(tenYears),
+
+			KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+			BasicConstraintsValid: true,
+
+			IsCA: true,
+		}
+		caBody, err := x509.CreateCertificate(rand.Reader, &caCert, &caCert, caKey.Public(), caKey)
+		if err != nil {
+			ca.err = fmt.Errorf("unable to generate CA certificate: %v", err)
+			return
+		}
+		caReparsed, err := x509.ParseCertificate(caBody)
+		if err != nil {
+			ca.err = fmt.Errorf("unable to parse CA certificate: %v", err)
+			return
+		}
+		roots := x509.NewCertPool()
+		roots.AddCert(caReparsed)
+
+		chainKey, err := rsa.GenerateKey(rand.Reader, 2048)
+		if err != nil {
+			ca.err = fmt.Errorf("unable to generate sub-CA key: %v", err)
+			return
+		}
+		chainCert := x509.Certificate{
+			SerialNumber: big.NewInt(2),
+			Subject: pkix.Name{
+				Organization: []string{"Testing Sub-CA"},
+			},
+			NotBefore: time.Time{},
+			NotAfter:  time.Now().Add(tenYears),
+
+			KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+			BasicConstraintsValid: true,
+
+			IsCA: true,
+		}
+		chainBody, err := x509.CreateCertificate(rand.Reader, &chainCert, caReparsed, chainKey.Public(), caKey)
+		if err != nil {
+			ca.err = fmt.Errorf("unable to generate sub-CA certificate: %v", err)
+			return
+		}
+		chainReparsed, err := x509.ParseCertificate(chainBody)
+		if err != nil {
+			ca.err = fmt.Errorf("unable to parse sub-CA certificate: %v", err)
+			return
+		}
+
+		ca.chainKey = chainKey
+		ca.chainCert = chainReparsed
+	})
+
+	certKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		require.NoErrorf(t, err, "unable to generate certificate key")
+	}
+	cert := x509.Certificate{
+		SerialNumber: big.NewInt(3),
+		DNSNames:     altNames,
+		NotBefore:    notBefore,
+		NotAfter:     notAfter,
+
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+	}
+
+	body, err := x509.CreateCertificate(rand.Reader, &cert, ca.chainCert, certKey.Public(), ca.chainKey)
+	if err != nil {
+		require.NoErrorf(t, err, "unable to generate certificate")
+	}
+
+	crt := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: body})
+
+	key := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(certKey)})
+
+	certificate, err := tls.X509KeyPair([]byte(crt), []byte(key))
+	if err != nil {
+		log.Errorf("failed to generate fake serial number: %v", err)
+	}
+
+	tls := &tlsCertificate{
+		hosts: altNames,
+		cert:  &certificate,
+	}
+
+	return tls
+}
 
 func TestCertRegistry(t *testing.T) {
 
@@ -77,4 +216,312 @@ func TestCertRegistry(t *testing.T) {
 			t.Error("failed to read default certificate from hello")
 		}
 	})
+
+	t.Run("get cert from hello - multiple matches", func(t *testing.T) {
+		newcert := getFakeHostTLSCert("foo.org")
+
+		cr := NewCertRegistry()
+		cr.SyncCert("foo", hosts, cert)
+		cr.SyncCert("bar", hosts, newcert)
+		reply, err := cr.GetCertFromHello(hello)
+		if err != nil {
+			t.Error("failed to certificate from hello")
+		}
+		if !reflect.DeepEqual(reply, newcert) {
+			t.Error("failed to read best certificate from hello")
+		}
+	})
+}
+
+func TestFindBestMatchingCertificate(t *testing.T) {
+	domain := "example.org"
+	wildcardDomain := "*." + domain
+	invalidDomain := "invalid.org"
+	invalidWildcardDomain := "*." + invalidDomain
+	validHostname := "foo." + domain
+	invalidHostname := "foo." + invalidDomain
+
+	now := time.Now().Truncate(time.Millisecond)
+	currentTime = func() time.Time { return now }
+
+	before := now.Add(-time.Hour * 24 * 7)
+	after := now.Add(time.Hour*24*7 + 1*time.Second)
+	dummyArn := "DUMMY"
+
+	// simple cert
+	validCert := createDummyCertDetail(t, dummyArn, []string{validHostname}, before, after)
+	validWildcardCert := createDummyCertDetail(t, dummyArn, []string{wildcardDomain}, before, after)
+	invalidDomainCert := createDummyCertDetail(t, dummyArn, []string{invalidDomain}, before, after)
+	invalidWildcardCert := createDummyCertDetail(t, dummyArn, []string{invalidWildcardDomain}, before, after)
+
+	// AlternateName certs
+	saValidCert := createDummyCertDetail(t, dummyArn, []string{validHostname, invalidDomain, invalidHostname, invalidWildcardDomain}, before, after)
+	saValidWildcardCert := createDummyCertDetail(t, dummyArn, []string{invalidDomain, invalidHostname, invalidWildcardDomain, wildcardDomain}, before, after)
+	saMultipleValidCert := createDummyCertDetail(t, dummyArn, []string{wildcardDomain, validHostname, invalidDomain, invalidHostname, invalidWildcardDomain}, before, after)
+
+	// simple invalid time cases
+	invalidTimeCert1 := createDummyCertDetail(t, dummyArn, []string{domain}, after, before)
+	invalidTimeCert2 := createDummyCertDetail(t, dummyArn, []string{domain}, after, after)
+	invalidTimeCert3 := createDummyCertDetail(t, dummyArn, []string{domain}, before, before)
+
+	// tricky times with multiple valid certs
+	validCertForOneDay := createDummyCertDetail(t, dummyArn, []string{validHostname}, before, now.Add(time.Hour*24))
+	validCertForSixDays := createDummyCertDetail(t, dummyArn, []string{validHostname}, before, now.Add(time.Hour*24*6))
+	validCertForTenDays := createDummyCertDetail(t, dummyArn, []string{validHostname}, before, now.Add(time.Hour*24*6))
+	validCertForOneYear := createDummyCertDetail(t, dummyArn, []string{validHostname}, before, now.Add(time.Hour*24*365))
+	validCertSinceOneDay := createDummyCertDetail(t, dummyArn, []string{validHostname}, now.Add(-time.Hour*24), after)
+	validCertSinceSixDays := createDummyCertDetail(t, dummyArn, []string{validHostname}, now.Add(-time.Hour*24*6), after)
+	validCertSinceOneYear := createDummyCertDetail(t, dummyArn, []string{validHostname}, now.Add(-time.Hour*24*365), after)
+	validCertForOneYearSinceOneDay := createDummyCertDetail(t, dummyArn, []string{validHostname}, now.Add(-time.Hour*24), now.Add(time.Hour*24*365))
+	validCertForOneYearSinceSixDays := createDummyCertDetail(t, dummyArn, []string{validHostname}, now.Add(-time.Hour*24*6), now.Add(time.Hour*24*365))
+	validCertForOneYearSinceOneYear := createDummyCertDetail(t, dummyArn, []string{validHostname}, now.Add(-time.Hour*24*365), now.Add(time.Hour*24*365))
+
+	validCertFor6dUntill1y := createDummyCertDetail(t, dummyArn, []string{validHostname}, now.Add(-time.Hour*24*6), now.Add(time.Hour*24*365))
+	validCertFor6dUntill6d := createDummyCertDetail(t, dummyArn, []string{validHostname}, now.Add(-time.Hour*24*6), now.Add(time.Hour*24*6))
+	validCertFor6dUntill10d := createDummyCertDetail(t, dummyArn, []string{validHostname}, now.Add(-time.Hour*24*6), now.Add(time.Hour*24*10))
+	validCertFor1dUntill6d := createDummyCertDetail(t, dummyArn, []string{validHostname}, now.Add(-time.Hour*24*1), now.Add(time.Hour*24*6))
+	validCertFor1dUntill7d1sLess := createDummyCertDetail(t, dummyArn, []string{validHostname}, now.Add(-time.Hour*24*1), now.Add(time.Hour*24*7-time.Second*1))
+	validCertFor1dUntill7d1s := createDummyCertDetail(t, dummyArn, []string{validHostname}, now.Add(-time.Hour*24*1), now.Add(time.Hour*24*7+time.Second*1))
+	validCertFor1dUntill10d := createDummyCertDetail(t, dummyArn, []string{validHostname}, now.Add(-time.Hour*24*1), now.Add(time.Hour*24*10))
+
+	for _, ti := range []struct {
+		msg       string
+		hostname  string
+		cert      []*tlsCertificate
+		expect    *tls.Certificate
+		condition certCondition
+	}{
+		{
+			msg:       "Not found best match",
+			hostname:  validHostname,
+			cert:      []*tlsCertificate{validCert},
+			expect:    validCert.cert,
+			condition: certValidMatchFunction,
+		}, {
+			msg:       "Not found wildcard as best match",
+			hostname:  validHostname,
+			cert:      []*tlsCertificate{validWildcardCert},
+			expect:    validWildcardCert.cert,
+			condition: certValidMatchFunction,
+		}, {
+			msg:       "Not found best match of multiple valid certs",
+			hostname:  validHostname,
+			cert:      []*tlsCertificate{validCert, validWildcardCert},
+			expect:    validCert.cert,
+			condition: certValidMatchFunction,
+		}, {
+			msg:       "Not found best match of multiple certs one wildcard valid",
+			hostname:  validHostname,
+			cert:      []*tlsCertificate{invalidDomainCert, validWildcardCert, invalidWildcardCert},
+			expect:    validWildcardCert.cert,
+			condition: certValidMatchFunction,
+		}, {
+			msg:       "Not found best match of multiple certs one valid",
+			hostname:  validHostname,
+			cert:      []*tlsCertificate{invalidDomainCert, validCert, invalidWildcardCert},
+			expect:    validCert.cert,
+			condition: certValidMatchFunction,
+		}, {
+			msg:       "Found best match for invalid hostname",
+			hostname:  invalidHostname,
+			cert:      []*tlsCertificate{validCert},
+			expect:    nil,
+			condition: certInvalidMatchFunction,
+		}, {
+			msg:       "Found best match for invalid cert",
+			hostname:  validHostname,
+			cert:      []*tlsCertificate{invalidDomainCert},
+			expect:    nil,
+			condition: certInvalidMatchFunction,
+		}, {
+			msg:       "Found best match for invalid wildcardcert",
+			hostname:  validHostname,
+			cert:      []*tlsCertificate{invalidWildcardCert},
+			expect:    nil,
+			condition: certInvalidMatchFunction,
+		}, {
+			msg:       "Found best match for multiple invalid certs",
+			hostname:  validHostname,
+			cert:      []*tlsCertificate{invalidWildcardCert, invalidDomainCert},
+			expect:    nil,
+			condition: certInvalidMatchFunction,
+		}, {
+			msg:       "Not found best match of AlternateName cert with one valid and multiple invalid names",
+			hostname:  validHostname,
+			cert:      []*tlsCertificate{saValidCert},
+			expect:    saValidCert.cert,
+			condition: certValidMatchFunction,
+		}, {
+			msg:       "Not found best match of AlternateName cert with one valid wildcard and multiple invalid names",
+			hostname:  validHostname,
+			cert:      []*tlsCertificate{saValidWildcardCert},
+			expect:    saValidWildcardCert.cert,
+			condition: certValidMatchFunction,
+		}, {
+			msg:       "Not found best match of AlternateName cert with multiple valid and multiple invalid names",
+			hostname:  validHostname,
+			cert:      []*tlsCertificate{saMultipleValidCert},
+			expect:    saMultipleValidCert.cert,
+			condition: certValidMatchFunction,
+		}, {
+			msg:       "Found best match for invalid time cert 1",
+			hostname:  validHostname,
+			cert:      []*tlsCertificate{invalidTimeCert1},
+			expect:    nil,
+			condition: certInvalidMatchFunction,
+		}, {
+			msg:       "Found best match for invalid time cert 2",
+			hostname:  validHostname,
+			cert:      []*tlsCertificate{invalidTimeCert2},
+			expect:    nil,
+			condition: certInvalidMatchFunction,
+		}, {
+			msg:       "Found best match for invalid time cert 3",
+			hostname:  validHostname,
+			cert:      []*tlsCertificate{invalidTimeCert3},
+			expect:    nil,
+			condition: certInvalidMatchFunction,
+		}, {
+			msg:       "Not found best match tricky cert NotAfter 1 day compared to 6 days",
+			hostname:  validHostname,
+			cert:      []*tlsCertificate{validCertForOneDay, validCertForSixDays},
+			expect:    validCertForSixDays.cert,
+			condition: certValidMatchFunction,
+		}, {
+			msg:       "Not found best match tricky cert NotAfter 365 days compared to 1 day",
+			hostname:  validHostname,
+			cert:      []*tlsCertificate{validCertForOneYear, validCertForOneDay},
+			expect:    validCertForOneYear.cert,
+			condition: certValidMatchFunction,
+		}, {
+			msg:       "Not found best match tricky cert NotAfter 365 days compared to 6 day",
+			hostname:  validHostname,
+			cert:      []*tlsCertificate{validCertForOneYear, validCertForSixDays},
+			expect:    validCertForOneYear.cert,
+			condition: certValidMatchFunction,
+		}, {
+			msg:       "Not found best match tricky cert NotAfter 365 days compared to 10 day",
+			hostname:  validHostname,
+			cert:      []*tlsCertificate{validCertForTenDays, validCertForOneYear},
+			expect:    validCertForOneYear.cert,
+			condition: certValidMatchFunction,
+		}, {
+			msg:       "Not found best match (newest first) tricky cert NotBefore 6 days compared to 1 day",
+			hostname:  validHostname,
+			cert:      []*tlsCertificate{validCertSinceOneDay, validCertSinceSixDays}, // FIXME: this is by order
+			expect:    validCertSinceOneDay.cert,
+			condition: certValidMatchFunction,
+		}, {
+			msg:       "Not found best match (newest first) tricky cert NotBefore 6 days compared to 365 days",
+			hostname:  validHostname,
+			cert:      []*tlsCertificate{validCertSinceSixDays, validCertSinceOneYear},
+			expect:    validCertSinceSixDays.cert,
+			condition: certValidMatchFunction,
+		}, {
+			msg:       "Not found best match (newest first) tricky cert NotBefore 6 days compared to 365 days another order by cert",
+			hostname:  validHostname,
+			cert:      []*tlsCertificate{validCertSinceOneYear, validCertSinceSixDays},
+			expect:    validCertSinceSixDays.cert,
+			condition: certValidMatchFunction,
+		}, {
+			msg:       "Not found best match (newest first) tricky cert NotBefore 1 days compared to 365 days and both valid for 1 year",
+			hostname:  validHostname,
+			cert:      []*tlsCertificate{validCertForOneYearSinceOneDay, validCertForOneYearSinceOneYear},
+			expect:    validCertForOneYearSinceOneDay.cert,
+			condition: certValidMatchFunction,
+		}, {
+			msg:       "Not found best match (newest first) tricky cert NotBefore 6 days compared to 365 days and both valid for 1 year",
+			hostname:  validHostname,
+			cert:      []*tlsCertificate{validCertForOneYearSinceOneYear, validCertForOneYearSinceSixDays},
+			expect:    validCertForOneYearSinceSixDays.cert,
+			condition: certValidMatchFunction,
+		}, {
+			msg:       "Not found best match (newest first) tricky cert NotBefore/NotAfter 6d/1y compared to 1d/10d",
+			hostname:  validHostname,
+			cert:      []*tlsCertificate{validCertFor6dUntill1y, validCertFor1dUntill10d},
+			expect:    validCertFor1dUntill10d.cert,
+			condition: certValidMatchFunction,
+		}, {
+			msg:       "Not found best match (newer first) tricky cert NotBefore/NotAfter 1d/7d1s compared to 6d/10d",
+			hostname:  validHostname,
+			cert:      []*tlsCertificate{validCertFor1dUntill7d1s, validCertFor6dUntill10d},
+			expect:    validCertFor1dUntill7d1s.cert,
+			condition: certValidMatchFunction,
+		}, {
+			msg:       "Not found best match (longer first) tricky cert NotBefore/NotAfter 6d/6d compared to 6d/1y",
+			hostname:  validHostname,
+			cert:      []*tlsCertificate{validCertFor6dUntill6d, validCertForOneYearSinceSixDays},
+			expect:    validCertForOneYearSinceSixDays.cert,
+			condition: certValidMatchFunction,
+		}, {
+			msg:       "Not found best match (longer first) tricky cert NotBefore/NotAfter 1d/6d compared to 6d/10d",
+			hostname:  validHostname,
+			cert:      []*tlsCertificate{validCertFor1dUntill6d, validCertFor6dUntill10d},
+			expect:    validCertFor6dUntill10d.cert,
+			condition: certValidMatchFunction,
+		}, {
+			msg:       "Not found best match (longer first) tricky cert NotBefore/NotAfter 6d/10d compared to 1d/7d-1s",
+			hostname:  validHostname,
+			cert:      []*tlsCertificate{validCertFor6dUntill10d, validCertFor1dUntill7d1sLess},
+			expect:    validCertFor6dUntill10d.cert,
+			condition: certValidMatchFunction,
+		},
+	} {
+		t.Run(ti.msg, func(t *testing.T) {
+			if c, err := getBestMatchingCertificate(ti.hostname, ti.cert); ti.condition(err, c, ti.expect) {
+				t.Errorf("%s: for host: %s expected %v, got %v, err: %v", ti.msg, ti.hostname, ti.expect, c, err)
+			}
+
+		})
+	}
+
+}
+
+func TestGlob(t *testing.T) {
+	for _, ti := range []struct {
+		msg     string
+		pattern string
+		subj    string
+		expect  bool
+	}{
+		{
+			msg:     "Not found exact match",
+			pattern: "www.foo.org",
+			subj:    "www.foo.org",
+			expect:  true,
+		}, {
+			msg:     "Not found simple glob",
+			pattern: "*",
+			subj:    "www.foo.org",
+			expect:  true,
+		}, {
+			msg:     "Not found simple match",
+			pattern: "*.foo.org",
+			subj:    "www.foo.org",
+			expect:  true,
+		}, {
+			msg:     "Found wrong simple match prefix",
+			pattern: "www.foo.org",
+			subj:    "wwww.foo.org",
+			expect:  false,
+		}, {
+			msg:     "Found wrong simple match suffix",
+			pattern: "www.foo.org",
+			subj:    "www.foo.orgg",
+			expect:  false,
+		}, {
+			msg:     "Found wrong complex match",
+			pattern: "*.foo.org",
+			subj:    "www.baz.foo.org",
+			expect:  false,
+		},
+	} {
+		t.Run(ti.msg, func(t *testing.T) {
+			if prefixGlob(ti.pattern, ti.subj) != ti.expect {
+				t.Errorf("%s: for pattern: %s and subj: %s, expected %v", ti.msg, ti.pattern, ti.subj, ti.expect)
+			}
+
+		})
+	}
+
 }
