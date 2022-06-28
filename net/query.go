d2f736f5909d7e644440dc365126872d34c0946d
Mustafa Abdelrahman <mustafa.abdelrahman@zalando.de>
2022-06-28 16:05:55 +0200

    print raw query
    Signed-off-by: Mustafa Abdelrahman <mustafa.abdelrahman@zalando.de>

--- net/query.go
+++ net/query.go
@@ -13,7 +13,7 @@ type ValidateQueryHandler struct {
 
 func (q *ValidateQueryHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if _, err := url.ParseQuery(r.URL.RawQuery); err != nil {
-		log.Errorf("Invalid query: %s", r.URL.RawFragment)
+		log.Errorf("Invalid query: %s", r.URL.RawQuery)
 		http.Error(w, "Invalid query", http.StatusBadRequest)
 		return
 	}
