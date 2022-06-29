63a77f9da49b41c89663c166e3e00e3ef991acc2
Sandor Szücs <sandor.szuecs@zalando.de>
2022-06-29 11:48:15 +0200

    pull out logging query into separate handler such that we can log without blocking
    Signed-off-by: Sandor Szücs <sandor.szuecs@zalando.de>

--- net/query_test.go
+++ net/query_test.go
@@ -53,3 +53,51 @@ func TestQueryHandler(t *testing.T) {
 		})
 	}
 }
+
+func TestQueryLogHandler(t *testing.T) {
+	for _, ti := range []struct {
+		name     string
+		routes   string
+		query    string
+		expected int
+	}{
+		{
+			name:     "request without query",
+			routes:   `r: * -> inlineContent("OK") -> <shunt>;`,
+			query:    "",
+			expected: http.StatusOK,
+		},
+		{
+			name:     "request with query",
+			routes:   `r1: Query("foo") -> status(400) -> inlineContent("FAIL") -> <shunt> ;r2: * -> inlineContent("OK") -> <shunt>;`,
+			query:    "foo=bar",
+			expected: http.StatusOK,
+		},
+		{
+			name:     "request with bad query does not block",
+			routes:   `r1: Query("foo") -> status(400) -> inlineContent("FAIL") -> <shunt> ;r2: * -> inlineContent("OK") -> <shunt>;`,
+			query:    "foo=bar;",
+			expected: http.StatusOK,
+		},
+	} {
+		t.Run(ti.name, func(t *testing.T) {
+
+			req, err := http.NewRequest("GET", "/path", nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+			req.URL.RawQuery = ti.query
+
+			noop := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
+			recorder := httptest.NewRecorder()
+			h := &ValidateQueryLogHandler{
+				Handler: noop,
+			}
+			h.ServeHTTP(recorder, req)
+
+			if recorder.Code != ti.expected {
+				t.Fatalf("Failed to get expected code %d, got %d", ti.expected, recorder.Code)
+			}
+		})
+	}
+}
