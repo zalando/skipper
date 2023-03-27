f11a83b7368d91b5d3089488fbe938b24f8c5d99
Mustafa Abdelrahman <mustafa.abdelrahman@zalando.de>
2023-03-27 14:48:13 +0200

    Address comments
    Signed-off-by: Mustafa Abdelrahman <mustafa.abdelrahman@zalando.de>

--- routesrv/redishandler.go
+++ routesrv/redishandler.go
@@ -32,15 +32,14 @@ func (rh *RedisHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func getRedisAddresses(namespace, name string, kdc *kubernetes.Client) func() []byte {
 	return func() []byte {
-		endpoints := make([]RedisEndpoint, 0)
-		RedisEndpoints := RedisEndpoints{Endpoints: endpoints}
+		result := RedisEndpoints{}
 		a := kdc.GetEndpointAddresses(namespace, name)
 		log.Infof("Redis updater called and found %d redis endpoints", len(a))
 		for i := 0; i < len(a); i++ {
 			a[i] = strings.TrimPrefix(a[i], "TCP://")
-			RedisEndpoints.Endpoints = append(RedisEndpoints.Endpoints, RedisEndpoint{Address: a[i]})
+			result.Endpoints = append(result.Endpoints, RedisEndpoint{Address: a[i]})
 		}
-		data, err := json.Marshal(RedisEndpoints)
+		data, err := json.Marshal(result)
 
 		if err != nil {
 			log.Errorf("Failed to marshal json data %v", err)
