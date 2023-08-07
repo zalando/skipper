zalando/skipper

commit 13ccac4a9e7767e3ee80bbe3d33367a4ab67718e

Fix goroutine leaks in tests (#2499)
* ratelimit: fix goroutine leaks in tests
* queuelistener: fix goroutine leaks in tests
* filters/shedder: fix goroutine leaks in tests

Signed-off-by: Alexander Yastrebov <alexander.yastrebov@zalando.de>

queuelistener/listener_test.go
@@ -355,6 +355,8 @@ func TestInterface(t *testing.T) {
 
 	t.Run("wrapped listener returns temporary error, logs and retries", func(t *testing.T) {
 		log := loggingtest.New()
+		defer log.Close()
+
 		l, err := listenWith(&testListener{failNextTemporary: true}, Options{
 			Log: log,
 		})
@@ -404,6 +406,7 @@ func TestInterface(t *testing.T) {
 		if err != nil {
 			t.Fatal(err)
 		}
+		defer l.Close()
 
 		if l.Addr() != addr {
 			t.Error("failed to return the right address")
@@ -984,6 +987,8 @@ func TestTeardown(t *testing.T) {
 func TestMonitoring(t *testing.T) {
 	t.Run("logs the temporary errors", func(t *testing.T) {
 		log := loggingtest.New()
+		defer log.Close()
+
 		l, err := listenWith(&testListener{failNextTemporary: true}, Options{
 			Log: log,
 		})
@@ -1062,6 +1067,7 @@ func TestMonitoring(t *testing.T) {
 		if err != nil {
 			t.Fatal(err)
 		}
+		defer l.Close()
 
 		done := make(chan struct{})
 		defer func() { close(done) }()

filters/shedder/admission_test.go
@@ -5,7 +5,6 @@ import (
 	"math/rand"
 	"net/http"
 	"net/http/httptest"
-	"net/url"
 	"sort"
 	"testing"
 	"time"
@@ -13,7 +12,6 @@ import (
 	"github.com/zalando/skipper/eskip"
 	"github.com/zalando/skipper/filters"
 	"github.com/zalando/skipper/filters/builtin"
-	"github.com/zalando/skipper/net"
 	"github.com/zalando/skipper/proxy/proxytest"
 	"github.com/zalando/skipper/routing"
 	"github.com/zalando/skipper/routing/testdataclient"
@@ -129,29 +127,37 @@ func TestAdmissionControl(t *testing.T) {
 					w.WriteHeader(http.StatusOK)
 				}
 			}))
+			defer backend.Close()
 
-			spec := NewAdmissionControl(Options{})
+			spec := NewAdmissionControl(Options{}).(*AdmissionControlSpec)
+
 			args := make([]interface{}, 0, 6)
 			args = append(args, "testmetric", ti.mode, ti.d.String(), ti.windowsize, ti.minRequests, ti.successThreshold, ti.maxrejectprobability, ti.exponent)
-			_, err := spec.CreateFilter(args)
+
+			f, err := spec.CreateFilter(args)
 			if err != nil {
 				t.Logf("args: %+v", args...)
 				t.Fatalf("error creating filter: %v", err)
 				return
 			}
+			defer f.(*admissionControl).Close()
 
 			fr := make(filters.Registry)
 			fr.Register(spec)
 			r := &eskip.Route{Filters: []*eskip.Filter{{Name: spec.Name(), Args: args}}, Backend: backend.URL}
 
-			proxy := proxytest.New(fr, r)
-			reqURL, err := url.Parse(proxy.URL)
-			if err != nil {
-				t.Fatalf("Failed to parse url %s: %v", proxy.URL, err)
-			}
+			dc := testdataclient.New([]*eskip.Route{r})
+			defer dc.Close()
 
-			client := net.NewClient(net.Options{})
-			req, err := http.NewRequest("GET", reqURL.String(), nil)
+			proxy := proxytest.WithRoutingOptions(fr, routing.Options{
+				DataClients:    []routing.DataClient{dc},
+				PostProcessors: []routing.PostProcessor{spec.PostProcessor()},
+				PreProcessors:  []routing.PreProcessor{spec.PreProcessor()},
+			})
+			defer proxy.Close()
+
+			client := proxy.Client()
+			req, err := http.NewRequest("GET", proxy.URL, nil)
 			if err != nil {
 				t.Error(err)
 				return
@@ -224,9 +230,12 @@ func TestAdmissionControlCleanupModes(t *testing.T) {
 			backend1 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				w.WriteHeader(http.StatusInternalServerError)
 			}))
+			defer backend1.Close()
+
 			backend2 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				w.WriteHeader(http.StatusOK)
 			}))
+			defer backend2.Close()
 
 			fspec := NewAdmissionControl(Options{})
 			spec, ok := fspec.(*AdmissionControlSpec)
@@ -238,11 +247,12 @@ func TestAdmissionControlCleanupModes(t *testing.T) {
 
 			args := make([]interface{}, 0, 6)
 			args = append(args, "testmetric", ti.mode, "10ms", 5, 1, 0.1, 0.95, 0.5)
-			_, err := spec.CreateFilter(args)
+			f, err := spec.CreateFilter(args)
 			if err != nil {
 				t.Fatalf("error creating filter: %v", err)
 				return
 			}
+			f.(*admissionControl).Close()
 
 			fr := make(filters.Registry)
 			fr.Register(spec)
@@ -259,11 +269,13 @@ func TestAdmissionControlCleanupModes(t *testing.T) {
 			}
 
 			dc := testdataclient.New([]*eskip.Route{r1})
+			defer dc.Close()
+
 			proxy := proxytest.WithRoutingOptions(fr, routing.Options{
 				DataClients:    []routing.DataClient{dc},
 				PostProcessors: []routing.PostProcessor{postProcessor, validationPostProcessor},
 				PreProcessors:  []routing.PreProcessor{preProcessor},
-			}, nil)
+			})
 			defer proxy.Close()
 
 			var tuple tuple
@@ -391,9 +403,10 @@ func TestAdmissionControlCleanupMultipleFilters(t *testing.T) {
 				ch: ch,
 			}
 
-			backend1 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			backend := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				w.WriteHeader(http.StatusOK)
 			}))
+			defer backend.Close()
 
 			fspec := NewAdmissionControl(Options{})
 			spec, ok := fspec.(*AdmissionControlSpec)
@@ -403,7 +416,7 @@ func TestAdmissionControlCleanupMultipleFilters(t *testing.T) {
 			preProcessor := spec.PreProcessor()
 			postProcessor := spec.PostProcessor()
 
-			r := eskip.MustParse(fmt.Sprintf(ti.doc, backend1.URL))
+			r := eskip.MustParse(fmt.Sprintf(ti.doc, backend.URL))
 
 			fr := make(filters.Registry)
 			fr.Register(fspec)
@@ -411,14 +424,14 @@ func TestAdmissionControlCleanupMultipleFilters(t *testing.T) {
 			fr.Register(builtin.NewStatus())
 
 			dc := testdataclient.New(r)
+			defer dc.Close()
+
 			proxy := proxytest.WithRoutingOptions(fr, routing.Options{
 				DataClients:    []routing.DataClient{dc},
 				PostProcessors: []routing.PostProcessor{postProcessor, validationProcessor},
 				PreProcessors:  []routing.PreProcessor{preProcessor},
-			}, r...)
+			})
 			defer proxy.Close()
-
-			dc.Update(r, nil)
 
 			result := <-ch
 			if result == nil {

filters/shedder/main_test.go
@@ -1,0 +1,12 @@
+package shedder
+
+import (
+	"os"
+	"testing"
+
+	"github.com/AlexanderYastrebov/noleak"
+)
+
+func TestMain(m *testing.M) {
+	os.Exit(noleak.CheckMain(m))
+}

queuelistener/main_test.go
@@ -1,0 +1,12 @@
+package queuelistener
+
+import (
+	"os"
+	"testing"
+
+	"github.com/AlexanderYastrebov/noleak"
+)
+
+func TestMain(m *testing.M) {
+	os.Exit(noleak.CheckMain(m))
+}

ratelimit/cluster_test.go
@@ -21,6 +21,8 @@ func Test_newClusterRateLimiter(t *testing.T) {
 			Addrs: []string{redisAddr},
 		},
 	)
+	defer myring.Close()
+
 	fake, err := newFakeSwarm("foo01", 3*time.Second)
 	if err != nil {
 		t.Fatalf("Failed to create fake swarm to test: %v", err)
@@ -73,7 +75,11 @@ func Test_newClusterRateLimiter(t *testing.T) {
 			want:     newClusterRateLimiterSwim(settings, fake, "mygroup"),
 		}} {
 		t.Run(tt.name, func(t *testing.T) {
+			defer tt.want.Close()
+
 			got := newClusterRateLimiter(tt.settings, tt.swarm, tt.ring, tt.group)
+			defer got.Close()
+
 			// internals in swim are created and won't be equal with reflect.Deepequal
 			gotT := fmt.Sprintf("%T", got)
 			wantT := fmt.Sprintf("%T", tt.want)

ratelimit/main_test.go
@@ -1,0 +1,12 @@
+package ratelimit
+
+import (
+	"os"
+	"testing"
+
+	"github.com/AlexanderYastrebov/noleak"
+)
+
+func TestMain(m *testing.M) {
+	os.Exit(noleak.CheckMain(m))
+}

ratelimit/ratelimit_test.go
@@ -82,11 +82,15 @@ func TestLocalRatelimit(t *testing.T) {
 
 	t.Run("new local ratelimitter", func(t *testing.T) {
 		rl := newRatelimit(s, nil, nil)
+		defer rl.Close()
+
 		checkNotRatelimitted(t, rl, client1)
 	})
 
 	t.Run("does not rate limit unless we have enough calls", func(t *testing.T) {
 		rl := newRatelimit(s, nil, nil)
+		defer rl.Close()
+
 		for i := 0; i < s.MaxHits; i++ {
 			checkNotRatelimitted(t, rl, client1)
 		}
@@ -97,6 +101,8 @@ func TestLocalRatelimit(t *testing.T) {
 
 	t.Run("does not rate limit if TimeWindow is over", func(t *testing.T) {
 		rl := newRatelimit(s, nil, nil)
+		defer rl.Close()
+
 		for i := 0; i < s.MaxHits-1; i++ {
 			checkNotRatelimitted(t, rl, client1)
 		}
@@ -108,6 +114,8 @@ func TestLocalRatelimit(t *testing.T) {
 		s := s
 		s.MaxHits = 0
 		rl := newRatelimit(s, nil, nil)
+		defer rl.Close()
+
 		checkRatelimitted(t, rl, client1)
 	})
 }

ratelimit/registry_test.go
@@ -30,6 +30,7 @@ func TestRegistry(t *testing.T) {
 
 	t.Run("no settings", func(t *testing.T) {
 		r := NewRegistry(Settings{})
+		defer r.Close()
 
 		rl := r.Get(Settings{})
 		checkNil(t, rl)
@@ -37,6 +38,7 @@ func TestRegistry(t *testing.T) {
 	t.Run("with settings", func(t *testing.T) {
 		s := createSettings(3)
 		r := NewRegistry(s)
+		defer r.Close()
 
 		rl := r.Get(s)
 		checkNotNil(t, rl)

commit fe2f7ad4c3aef3bf2c58fc1c29e941b627dd71e8

queuelistener: fix testListener goroutine leak
Queuelistener continously calls Accept on the testListener which keeps
a buffer of last accepted testConnections in the conns channel.

The test code races to receive from the same channel so Accept goroutine
may occasionally block receiving from the empty channel which would be
detected by goroutine leak detector introduced by the #2499.

The problem could be reproduced e.g. by running:
```
GODEBUG=tracebackancestors=10 go test ./queuelistener/ -run=TestTeardown/connections_accepted_from_the_wrapped_listener_closed_after_tear_down -count=1000

noleak: 1 active

goroutine 2655 [chan receive]:
github.com/zalando/skipper/queuelistener.(*testListener).Accept(0xc0000e7830)
...
```

This change avoids blocking Accept on potentially empty channel.

Signed-off-by: Alexander Yastrebov <alexander.yastrebov@zalando.de>

queuelistener/listener_test.go
@@ -80,8 +80,13 @@ func (l *testListener) Accept() (net.Conn, error) {
 		select {
 		case l.conns <- c:
 		default:
-			// drop one if cannot store the latest
-			<-l.conns
+			// Drop one if cannot store the latest.
+			// The test might have received a connection in the meantime so do not block.
+			// Sending is safe as Accept is called from a single goroutine.
+			select {
+			case <-l.conns:
+			default:
+			}
 			l.conns <- c
 		}
 	}