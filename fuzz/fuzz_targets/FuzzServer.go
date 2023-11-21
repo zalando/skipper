0180de0c723afa7d57a55001af65db373589774e
Sepehrdad Sh <26747519+sepehrdaddev@users.noreply.github.com>
2023-11-21 13:22:54 +0100

    fix makefile cleanup, fix FuzzServer with Fuzz_FORKS
    Signed-off-by: Sepehrdad Sh <26747519+sepehrdaddev@users.noreply.github.com>

--- fuzz/fuzz_targets/FuzzServer.go
+++ fuzz/fuzz_targets/FuzzServer.go
@@ -13,11 +13,26 @@ import (
 	"github.com/zalando/skipper/config"
 )
 
-var initialized = false
+var (
+	initialized = false
+	address     = ""
+)
+
+func findAddress() (string, error) {
+	l, err := net.ListenTCP("tcp6", &net.TCPAddr{})
+
+	if err != nil {
+		return "", err
+	}
+
+	defer l.Close()
+
+	return l.Addr().String(), nil
+}
 
 func connect(host string) (net.Conn, error) {
 	for i := 0; i < 15; i++ {
-		conn, err := net.Dial("tcp", host)
+		conn, err := net.Dial("tcp6", host)
 
 		if err != nil {
 			continue
@@ -31,20 +46,29 @@ func connect(host string) (net.Conn, error) {
 
 func FuzzServer(data []byte) int {
 	if !initialized {
+		addr, err := findAddress()
+
+		if err != nil {
+			log.Printf("failed to find address: %v\n", err)
+			return -1
+		}
+
 		cfg := config.NewConfig()
 		cfg.InlineRoutes = `r: * -> status(200) -> inlineContent("ok") -> <shunt>`
 		cfg.ApplicationLogLevel = logrus.PanicLevel
 		cfg.AccessLogDisabled = true
 		cfg.ApplicationLog = "/dev/null"
+		cfg.Address = addr
 
 		go func() {
 			log.Fatal(skipper.Run(cfg.ToOptions()))
 		}()
 
 		initialized = true
+		address = cfg.Address
 	}
 
-	conn, err := connect("localhost:9090")
+	conn, err := connect(address)
 
 	if err != nil {
 		log.Printf("failed to dial: %v\n", err)
