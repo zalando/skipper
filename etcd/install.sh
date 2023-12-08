5dd9494cfa708b3ac6c891415eccbf8b61317c5b
Sepehrdad Sh <26747519+sepehrdaddev@users.noreply.github.com>
2023-12-08 12:01:37 +0100

    install.sh: make version number and shasum mandatory.
    Signed-off-by: Sepehrdad Sh <26747519+sepehrdaddev@users.noreply.github.com>

--- etcd/install.sh
+++ etcd/install.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
 set -eo pipefail
 
-ETCD_VERSION=v3.5.11
-if [ $# -gt 0 ]; then
-    ETCD_VERSION="$1"
+if [ $# -lt 2 ]; then
+    echo "usage: $0 <version> <sha512sum>"
+    exit 1
 fi
 
-ETCD_CHECKSUM=4fb304f384dd4d6e491e405fed8375a09ea1c6c2596b93f97cb31844202e620df160f87f18611e84f17675e7b7245e40d1aa23571ecdb507cb094ba04d378171
-if [ $# -gt 1 ]; then
-    ETCD_CHECKSUM="$2"
-fi
+ETCD_VERSION="$1"
+ETCD_CHECKSUM="$2"
+ETCD_URL="https://github.com/etcd-io/etcd/releases/download/${ETCD_VERSION}/etcd-${ETCD_VERSION}-linux-amd64.tar.gz"
 
 LOCAL_GOBIN=
+
 if [ -n "${GOBIN}" ]; then
     LOCAL_GOBIN="$GOBIN"
 elif [ -n "${GOPATH}" ]; then
@@ -19,9 +19,8 @@ elif [ -n "${GOPATH}" ]; then
 fi
 
 mkdir -p .bin
-wget \
-    "https://github.com/etcd-io/etcd/releases/download/${ETCD_VERSION}/etcd-${ETCD_VERSION}-linux-amd64.tar.gz" \
-    -O ./.bin/etcd.tar.gz
+
+curl -LsSfo ./.bin/etcd.tar.gz "${ETCD_URL}"
 
 echo "${ETCD_CHECKSUM} ./.bin/etcd.tar.gz" | sha512sum -c
 
