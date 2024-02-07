ddbd84cb08db52af26014fbba0f12d3feec70faf
Alexander Yastrebov <alexander.yastrebov@zalando.de>
2024-02-07 13:24:56 +0100

    filters/builtin: do not allocate filter slice (#2910)
    Also use value receiver for empty struct type.
    
    Followup on #2905
    
    Signed-off-by: Alexander Yastrebov <alexander.yastrebov@zalando.de>

--- filters/builtin/comment.go
+++ filters/builtin/comment.go
@@ -1,6 +1,8 @@
 package builtin
 
 import (
+	"slices"
+
 	"github.com/zalando/skipper/filters"
 	"github.com/zalando/skipper/routing"
 )
@@ -9,32 +11,28 @@ type comment struct{}
 
 // NewComment is a filter to comment a filter chain. It does nothing
 func NewComment() filters.Spec {
-	return &comment{}
+	return comment{}
 }
 
-func (*comment) Name() string {
+func (comment) Name() string {
 	return filters.CommentName
 }
 
-func (c *comment) CreateFilter(args []interface{}) (filters.Filter, error) {
+func (c comment) CreateFilter(args []interface{}) (filters.Filter, error) {
 	return c, nil
 }
 
-func (*comment) Request(filters.FilterContext) {}
+func (comment) Request(filters.FilterContext) {}
 
-func (*comment) Response(filters.FilterContext) {}
+func (comment) Response(filters.FilterContext) {}
 
 type CommentPostProcessor struct{}
 
 func (CommentPostProcessor) Do(routes []*routing.Route) []*routing.Route {
 	for _, r := range routes {
-		var ff []*routing.RouteFilter
-		for _, f := range r.Filters {
-			if f.Name != filters.CommentName {
-				ff = append(ff, f)
-			}
-		}
-		r.Filters = ff
+		r.Filters = slices.DeleteFunc(r.Filters, func(f *routing.RouteFilter) bool {
+			return f.Name == filters.CommentName
+		})
 	}
 	return routes
 }
