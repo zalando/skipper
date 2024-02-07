8e39046c857ac3b7c5c20242e608b683f14468b5
Alexander Yastrebov <alexander.yastrebov@zalando.de>
2024-02-07 12:19:21 +0100

    filters/builtin: preallocate filter slice
    Also use value receiver for empty strtuct type.
    
    Followup on #2905
    
    Signed-off-by: Alexander Yastrebov <alexander.yastrebov@zalando.de>

--- filters/builtin/comment.go
+++ filters/builtin/comment.go
@@ -9,26 +9,26 @@ type comment struct{}
 
 // NewComment is a filter to comment a filter chain. It does nothing
 func NewComment() filters.Spec {
-	return &comment{}
+	return comment{}
 }
 
-func (*comment) Name() string {
+func (comment) Name() string {
 	return filters.CommentName
 }
 
-func (c *comment) CreateFilter(args []interface{}) (filters.Filter, error) {
+func (c comment) CreateFilter(args []interface{}) (filters.Filter, error) {
 	return c, nil
 }
 
-func (*comment) Request(filters.FilterContext) {}
+func (comment) Request(filters.FilterContext) {}
 
-func (*comment) Response(filters.FilterContext) {}
+func (comment) Response(filters.FilterContext) {}
 
 type CommentPostProcessor struct{}
 
 func (CommentPostProcessor) Do(routes []*routing.Route) []*routing.Route {
 	for _, r := range routes {
-		var ff []*routing.RouteFilter
+		ff := make([]*routing.RouteFilter, 0, len(r.Filters))
 		for _, f := range r.Filters {
 			if f.Name != filters.CommentName {
 				ff = append(ff, f)
