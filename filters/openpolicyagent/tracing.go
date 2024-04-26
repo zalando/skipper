884263eeb77758d009a26100679e5324146c0f7b
Magnus Jungsbluth <magnus.jungsbluth@zalando.de>
2024-04-26 09:55:30 +0200

    OPA: Add tracing for outbound http calls (#3034)
    Signed-off-by: Magnus Jungsbluth <magnus.jungsbluth@zalando.de>

--- filters/openpolicyagent/tracing.go
+++ filters/openpolicyagent/tracing.go
@@ -3,8 +3,15 @@ package openpolicyagent
 import (
 	"net/http"
 
+	"github.com/open-policy-agent/opa/plugins"
 	opatracing "github.com/open-policy-agent/opa/tracing"
 	"github.com/opentracing/opentracing-go"
+	"github.com/zalando/skipper/logging"
+	"github.com/zalando/skipper/proxy"
+)
+
+const (
+	spanNameHttpOut = "open-policy-agent.http"
 )
 
 func init() {
@@ -14,15 +21,48 @@ func init() {
 type tracingFactory struct{}
 
 type transport struct {
-	opa     *OpenPolicyAgentInstance
+	tracer     opentracing.Tracer
+	bundleName string
+	manager    *plugins.Manager
+
 	wrapped http.RoundTripper
 }
 
+func WithTracingOptTracer(tracer opentracing.Tracer) func(*transport) {
+	return func(t *transport) {
+		t.tracer = tracer
+	}
+}
+
+func WithTracingOptBundleName(bundleName string) func(*transport) {
+	return func(t *transport) {
+		t.bundleName = bundleName
+	}
+}
+
+func WithTracingOptManager(manager *plugins.Manager) func(*transport) {
+	return func(t *transport) {
+		t.manager = manager
+	}
+}
+
 func (*tracingFactory) NewTransport(tr http.RoundTripper, opts opatracing.Options) http.RoundTripper {
-	return &transport{
-		opa:     opts[0].(*OpenPolicyAgentInstance),
+	log := &logging.DefaultLog{}
+
+	wrapper := &transport{
 		wrapped: tr,
 	}
+
+	for _, o := range opts {
+		opt, ok := o.(func(*transport))
+		if !ok {
+			log.Warnf("invalid type for OPA tracing option, expected func(*transport) got %T, tracing information might be incomplete", o)
+		} else {
+			opt(wrapper)
+		}
+	}
+
+	return wrapper
 }
 
 func (*tracingFactory) NewHandler(f http.Handler, label string, opts opatracing.Options) http.Handler {
@@ -32,15 +72,36 @@ func (*tracingFactory) NewHandler(f http.Handler, label string, opts opatracing.
 func (tr *transport) RoundTrip(req *http.Request) (*http.Response, error) {
 	ctx := req.Context()
 	parentSpan := opentracing.SpanFromContext(ctx)
+	var span opentracing.Span
 
 	if parentSpan != nil {
-		span := parentSpan.Tracer().StartSpan("http.send", opentracing.ChildOf(parentSpan.Context()))
+		span = parentSpan.Tracer().StartSpan(spanNameHttpOut, opentracing.ChildOf(parentSpan.Context()))
+	} else if tr.tracer != nil {
+		span = tr.tracer.StartSpan(spanNameHttpOut)
+	}
+
+	if span != nil {
 		defer span.Finish()
+
+		span.SetTag(proxy.HTTPMethodTag, req.Method)
+		span.SetTag(proxy.HTTPUrlTag, req.URL.String())
+		span.SetTag(proxy.HostnameTag, req.Host)
+		span.SetTag(proxy.HTTPPathTag, req.URL.Path)
+		span.SetTag(proxy.ComponentTag, "skipper")
+		span.SetTag(proxy.SpanKindTag, proxy.SpanKindClient)
+
+		setSpanTags(span, tr.bundleName, tr.manager)
 		req = req.WithContext(opentracing.ContextWithSpan(ctx, span))
 
 		carrier := opentracing.HTTPHeadersCarrier(req.Header)
 		span.Tracer().Inject(span.Context(), opentracing.HTTPHeaders, carrier)
 	}
 
-	return tr.wrapped.RoundTrip(req)
+	resp, err := tr.wrapped.RoundTrip(req)
+	if err != nil && span != nil {
+		span.SetTag("error", true)
+		span.LogKV("event", "error", "message", err.Error())
+	}
+
+	return resp, err
 }
