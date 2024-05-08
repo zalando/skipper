f97919ef06c7cfbac6a942cbac0d1599e0b633b7
Alexander Yastrebov <alexander.yastrebov@zalando.de>
2024-05-08 10:58:07 +0200

    config: add yaml flag (#3069)
    YAML flag parses (preferably flow-style) YAML value from the command line
    or unmarshals object yaml value from the yaml config file.
    
    Example value:
    ```
    bin/skipper -foo-flag='{foo: hello, bar: [world, "1"], baz: 2, qux: {baz: 3}}'
    ```
    
    and equivalent branch in config yaml:
    ```yaml
    foo-flag:
      foo: hello
      bar:
        - world
        - "1"
      baz: 2
      qux:
        baz: 3
    ```
    
    This will be useful for #2104
    
    It is also a better alternative to manual parsing of micro-syntax like
    e.g. implemented in #2888
    
    Signed-off-by: Alexander Yastrebov <alexander.yastrebov@zalando.de>

---
+++ config/yamlflag.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"fmt"
+
+	"gopkg.in/yaml.v2"
+)
+
+type yamlFlag[T any] struct {
+	Ptr   **T
+	value string // only for Set
+}
+
+func newYamlFlag[T any](ptr **T) *yamlFlag[T] {
+	return &yamlFlag[T]{Ptr: ptr}
+}
+
+func (yf *yamlFlag[T]) Set(value string) error {
+	var opts T
+	if err := yaml.Unmarshal([]byte(value), &opts); err != nil {
+		return fmt.Errorf("failed to parse yaml: %w", err)
+	}
+	*yf.Ptr = &opts
+	yf.value = value
+	return nil
+}
+
+func (yf *yamlFlag[T]) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	var opts T
+	if err := unmarshal(&opts); err != nil {
+		return err
+	}
+	*yf.Ptr = &opts
+	return nil
+}
+
+func (yf *yamlFlag[T]) String() string {
+	if yf == nil {
+		return ""
+	}
+	return yf.value
+}
