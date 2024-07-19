24a1d8603822f6ce3206dda4addb5b820cc6bbc9
Janardhan Sharma <janardhan.sharma@zalando.de>
2024-07-19 16:30:42 +0200

    rebase
    Signed-off-by: Janardhan Sharma <janardhan.sharma@zalando.de>

--- filters/openpolicyagent/evaluation.go
+++ filters/openpolicyagent/evaluation.go
@@ -23,7 +23,11 @@ func (opa *OpenPolicyAgentInstance) Eval(ctx context.Context, req *ext_authz_v3.
 		return nil, err
 	}
 
-	setDecisionIdInRequest(req, decisionId)
+	err = setDecisionIdInRequest(req, decisionId)
+	if err != nil {
+		opa.Logger().WithFields(map[string]interface{}{"err": err}).Error("Unable to set decision ID in Request.")
+		return nil, err
+	}
 
 	result, stopeval, err := envoyauth.NewEvalResult(withDecisionID(decisionId))
 	if err != nil {
@@ -68,21 +72,27 @@ func (opa *OpenPolicyAgentInstance) Eval(ctx context.Context, req *ext_authz_v3.
 	return result, nil
 }
 
-func setDecisionIdInRequest(req *ext_authz_v3.CheckRequest, decisionId string) {
+func setDecisionIdInRequest(req *ext_authz_v3.CheckRequest, decisionId string) error {
 	if req.Attributes.MetadataContext == nil {
 		req.Attributes.MetadataContext = &ext_authz_v3_core.Metadata{
 			FilterMetadata: map[string]*pbstruct.Struct{},
 		}
 	}
-	req.Attributes.MetadataContext.FilterMetadata["open_policy_agent"] = FormOpenPolicyAgentMetaDataObject(decisionId)
+
+	filterMeta, err := formOpenPolicyAgentMetaDataObject(decisionId)
+	if err != nil {
+		return err
+	}
+	req.Attributes.MetadataContext.FilterMetadata["open_policy_agent"] = filterMeta
+	return nil
 }
 
-func FormOpenPolicyAgentMetaDataObject(decisionId string) *pbstruct.Struct {
+func formOpenPolicyAgentMetaDataObject(decisionId string) (*pbstruct.Struct, error) {
+
 	innerFields := make(map[string]interface{})
 	innerFields["decision_id"] = decisionId
 
-	openPolicyAgentMetaDataObject, _ := pbstruct.NewStruct(innerFields)
-	return openPolicyAgentMetaDataObject
+	return pbstruct.NewStruct(innerFields)
 }
 
 func (opa *OpenPolicyAgentInstance) logDecision(ctx context.Context, input interface{}, result *envoyauth.EvalResult, err error) error {
