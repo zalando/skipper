deb82fbec0285b30e4974f7a455a1b5f88ed0475
Mikko Valkonen <mikko.valkonen@zalando.fi>
2024-09-12 10:42:33 +0300

    Handle the path normalization manually
    path.Clean() may break certain edge-cases where the path-variables
    would use `.` or `..` characters. This commit removes the usage of
    path.Clean() and implements the normalization manually.
    
    Signed-off-by: Mikko Valkonen <mikko.valkonen@zalando.fi>

--- filters/normalizepath/normalizepath.go
+++ filters/normalizepath/normalizepath.go
@@ -1,7 +1,7 @@
 package normalizepath
 
 import (
-	"path"
+	"strings"
 
 	"github.com/zalando/skipper/filters"
 )
@@ -22,7 +22,22 @@ func (spec normalizePath) CreateFilter(config []interface{}) (filters.Filter, er
 
 func (f normalizePath) Request(ctx filters.FilterContext) {
 	req := ctx.Request()
-	req.URL.Path = path.Clean(req.URL.Path)
+
+	segments := strings.Split(req.URL.Path, "/")
+	var filteredSegments []string
+	for _, seg := range segments {
+		if seg != "" {
+			filteredSegments = append(filteredSegments, seg)
+		}
+	}
+	normalizedPath := "/" + strings.Join(filteredSegments, "/")
+
+	// Ensure there's no trailing slash, unless the path is just "/"
+	if len(normalizedPath) > 1 && strings.HasSuffix(normalizedPath, "/") {
+		normalizedPath = normalizedPath[:len(normalizedPath)-1]
+	}
+
+	req.URL.Path = normalizedPath
 }
 
 func (f normalizePath) Response(ctx filters.FilterContext) {}
