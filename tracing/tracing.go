dc33e96fef8ab2edfbb49fb005bafeb886c95f68
viveksing <viveksing.patil@gmail.com>
2025-01-09 15:57:59 +0100

    Add opentelemetry bridge - add tests
    Signed-off-by: viveksing <viveksing.patil@gmail.com>

--- tracing/tracing.go
+++ tracing/tracing.go
@@ -53,7 +53,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"github.com/zalando/skipper/tracing/tracers/otel"
+	"github.com/zalando/skipper/tracing/tracers/lightstepotelbridge"
 	"path/filepath"
 	"plugin"
 
@@ -87,8 +87,8 @@ func InitTracer(opts []string) (tracer ot.Tracer, err error) {
 		return instana.InitTracer(opts)
 	case "jaeger":
 		return jaeger.InitTracer(opts)
-	case "otel":
-		return otel.InitTracer(opts), nil
+	case "lightstep-otel-bridge":
+		return lightstepotelbridge.InitTracer(opts), nil
 	case "lightstep":
 		return lightstep.InitTracer(opts)
 	default:
