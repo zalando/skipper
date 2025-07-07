519b9184b9e03028cc6f1d807f9211cbb3fe3601
Xavi Ivars <xavi.ivars@gmail.com>
2025-07-07 14:38:46 +0200

    Add filter using the functionality
    Signed-off-by: Xavi Ivars <xavi.ivars@gmail.com>

---
+++ filters/builtin/loopbackifstatus.go
@@ -0,0 +1,63 @@
+package builtin
+
+import "github.com/zalando/skipper/filters"
+
+type loopbackIfStatusSpec struct {
+}
+type loopbackIfStatusFilter struct {
+	statusCode int
+	path       string
+}
+
+// NewLoopbackIfStatus Creates a filter spec for the loopbackIfStatus() filter.
+//
+//	r: * -> loopbackIfStatus(401, "/new-path") -> "https://www.example.org";
+//
+// It accepts two arguments: the statusCode code to and the path to change the request to.
+//
+// The filter replaces the response coming from the backend or the following filters.
+func NewLoopbackIfStatus() filters.Spec {
+	return &loopbackIfStatusSpec{}
+}
+
+func (c loopbackIfStatusFilter) Request(ctx filters.FilterContext) {
+
+}
+
+func (c loopbackIfStatusFilter) Response(context filters.FilterContext) {
+	if context.Response().StatusCode == c.statusCode {
+		context.Request().URL.Path = c.path
+		context.LoopbackWithResponse()
+	}
+}
+
+func (s *loopbackIfStatusSpec) CreateFilter(args []interface{}) (filters.Filter, error) {
+	var (
+		f  loopbackIfStatusFilter
+		ok bool
+	)
+
+	f.statusCode, ok = args[0].(int)
+	if !ok {
+		floatStatusCode, ok := args[0].(float64)
+		if !ok {
+			return nil, filters.ErrInvalidFilterParameters
+		}
+		f.statusCode = int(floatStatusCode)
+	}
+
+	if f.statusCode < 100 || f.statusCode >= 600 {
+		return nil, filters.ErrInvalidFilterParameters
+	}
+
+	f.path, ok = args[1].(string)
+	if !ok {
+		return nil, filters.ErrInvalidFilterParameters
+	}
+
+	return f, nil
+}
+
+func (s *loopbackIfStatusSpec) Name() string {
+	return "loopbackIfStatus"
+}
