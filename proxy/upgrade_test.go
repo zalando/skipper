zalando/skipper

commit c30246837aa33e6e3b51539b67cd6dad21fe10cc

fix: upgrade headers if hop headers are removed (#3553)
fix: upgrade headers if hop headers are removed

fix: https://github.com/zalando/skipper/issues/3548

Signed-off-by: Sandor Szücs <sandor.szuecs@zalando.de>

proxy/upgrade_test.go
@@ -95,6 +95,7 @@ func TestServeHTTP(t *testing.T) {
 		backendClosesConnection    bool
 		backendHangs               bool
 		noBackend                  bool
+		dropHopHeaders             bool
 		backendStatusCode          int
 		expectedResponseStatusCode int
 		expectedResponseBody       string
@@ -110,6 +111,13 @@ func TestServeHTTP(t *testing.T) {
 			msg:               "Simple route",
 			route:             `route: Path("/ws") -> "%s";`,
 			method:            http.MethodGet,
+			backendStatusCode: http.StatusSwitchingProtocols,
+		},
+		{
+			msg:               "Simple route with hop-headers dropped",
+			route:             `route: Path("/ws") -> "%s";`,
+			method:            http.MethodGet,
+			dropHopHeaders:    true,
 			backendStatusCode: http.StatusSwitchingProtocols,
 		},
 		{
@@ -247,7 +255,14 @@ func TestServeHTTP(t *testing.T) {
 				defer backend.Close()
 			}
 
-			tp, err := newTestProxyWithParams(routes, Params{ExperimentalUpgrade: true})
+			flags := FlagsNone
+			if ti.dropHopHeaders {
+				flags = HopHeadersRemoval
+			}
+			tp, err := newTestProxyWithParams(routes, Params{
+				ExperimentalUpgrade: true,
+				Flags:               flags,
+			})
 			require.NoError(t, err)
 
 			defer tp.close()

proxy/proxy.go
@@ -967,6 +967,8 @@ func (p *Proxy) makeUpgradeRequest(ctx *context, req *http.Request) {
 }
 
 func (p *Proxy) makeBackendRequest(ctx *context, requestContext stdlibcontext.Context) (*http.Response, *proxyError) {
+	payloadProtocol := getUpgradeRequest(ctx.Request())
+
 	req, endpointMetrics, err := p.mapRequest(ctx, requestContext)
 	if err != nil {
 		return nil, &proxyError{err: fmt.Errorf("could not map backend request: %w", err)}
@@ -981,7 +983,10 @@ func (p *Proxy) makeBackendRequest(ctx *context, requestContext stdlibcontext.Co
 		defer endpointMetrics.DecInflightRequest()
 	}
 
-	if p.experimentalUpgrade && isUpgradeRequest(req) {
+	if p.experimentalUpgrade && payloadProtocol != "" {
+		// see also https://github.com/golang/go/blob/9159cd4ec6b0e9475dc9c71c830035c1c4c13483/src/net/http/httputil/reverseproxy.go#L423-L428
+		req.Header.Set("Connection", "Upgrade")
+		req.Header.Set("Upgrade", payloadProtocol)
 		p.makeUpgradeRequest(ctx, req)
 
 		// We are not owner of the connection anymore.

commit 9c8669f524552cf1ce4fadbec33413541fa91418

fix: Websocket requests fail if case of value in Connection header and case of key in Upgrade header do not match (#3563)
When a client sends a request to upgrade to a Websocket connection
and presents the value of the header `Connection` in lower-case
then Skipper fails to upgrade the request.

This change fixes the issue by using a function to read the header that
is case insensitive.

The bug was likely introduced in #3553.

Signed-off-by: Markus Meyer <m.meyer@mytaxi.com>

proxy/upgrade.go
@@ -32,7 +32,7 @@ func isUpgradeRequest(req *http.Request) bool {
 func getUpgradeRequest(req *http.Request) string {
 	for _, h := range req.Header[http.CanonicalHeaderKey("Connection")] {
 		if strings.Contains(strings.ToLower(h), "upgrade") {
-			return strings.Join(req.Header[h], " ")
+			return strings.Join(req.Header.Values(h), " ")
 		}
 	}
 	return ""

proxy/upgrade_test.go
@@ -87,6 +87,19 @@ func TestValidGetUpgradeRequest(t *testing.T) {
 
 }
 
+func TestValidGetUpgradeRequestCaseInsensitivity(t *testing.T) {
+	req, prot := getValidUpgradeRequest()
+	req.Header.Set("Connection", "uPgRaDe")
+	if !isUpgradeRequest(req) {
+		t.Errorf("Request has an upgrade header, but isUpgradeRequest returned false for %+v", req)
+	}
+	gotProt := getUpgradeRequest(req)
+	if gotProt != prot {
+		t.Errorf("%s != %s for %+v", gotProt, prot, req)
+	}
+
+}
+
 func TestServeHTTP(t *testing.T) {
 	for _, ti := range []struct {
 		msg                        string