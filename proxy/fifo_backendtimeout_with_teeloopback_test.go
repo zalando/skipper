5e03f98c161fd74ae73fededceff6ce5ed046bc2
Sandor Szücs <sandor.szuecs@zalando.de>
2025-11-03 20:47:20 +0100

    refactor: extract functions
    Signed-off-by: Sandor Szücs <sandor.szuecs@zalando.de>

--- proxy/fifo_backendtimeout_with_teeloopback_test.go
+++ proxy/fifo_backendtimeout_with_teeloopback_test.go
@@ -256,8 +256,8 @@ func TestBackendTimeoutWithSlowBodyShadow(t *testing.T) {
 	defer slowBackend.Close()
 
 	doc := fmt.Sprintf(`
-main: Path("/") -> fifo(5000, 20, "1s") -> teeLoopback("tag") -> "%s";
-shadow: Path("/") && Tee("tag") -> fifo(5, 40, "150ms") -> backendTimeout("20ms") -> "%s";`,
+main: PathSubtree("/") -> fifo(5000, 20, "1s") -> teeLoopback("tag") -> "%s";
+shadow: PathSubtree("/") && Tee("tag") -> fifo(5, 40, "150ms") -> backendTimeout("20ms") -> "%s";`,
 		backend.URL, slowBackend.URL)
 
 	fr := builtin.MakeRegistry()
@@ -299,8 +299,6 @@ shadow: Path("/") && Tee("tag") -> fifo(5, 40, "150ms") -> backendTimeout("20ms"
 	defer p.Close()
 
 	N := 500 //500000
-	wg := sync.WaitGroup{}
-	sometimes := rate.Sometimes{First: 3, Interval: time.Second}
 	resCH := make(chan int, N)
 	client := skpnet.NewClient(skpnet.Options{
 		Timeout:               80 * time.Millisecond,
@@ -312,56 +310,16 @@ shadow: Path("/") && Tee("tag") -> fifo(5, 40, "150ms") -> backendTimeout("20ms"
 	})
 	defer client.Close()
 	defer client.CloseIdleConnections()
-
-	for range N {
-		time.Sleep(100 * time.Microsecond)
-		wg.Add(1)
-		go func(ch chan<- int) {
-			defer wg.Done()
-			bodyData := strings.Repeat("A", 1023) + "\n"
-			buf := bytes.NewBufferString(bodyData)
-			sr := NewSlowReader(buf, 1*time.Microsecond)
-			req, err := http.NewRequest("PUT", p.URL, sr)
-			if err != nil {
-				t.Logf("Failed to create request: %v", err)
-				return
-			}
-
-			rsp, err := client.Do(req)
-			if err != nil {
-				t.Logf("Failed to get response: %v", err)
-				return
-			}
-			if rsp.StatusCode != 200 {
-				sometimes.Do(func() { t.Logf("response: %s", rsp.Status) })
-			}
-			io.Copy(io.Discard, rsp.Body)
-			ch <- rsp.StatusCode
-		}(resCH)
-	}
-	wg.Wait()
-
+	sendRequests(t, N, p, client, resCH)
 	logFifoMetrics(t, mockMetrics)
-
 	close(resCH)
-	sometimes = rate.Sometimes{First: 3, Interval: time.Second}
-
-	count := 0
-	for code := range resCH {
-		if code != 200 {
-			sometimes.Do(func() { t.Errorf("request wrong status: %d", code) })
-		}
-		count++
-	}
-	if count != N {
-		// expected
-		t.Logf("Failed to get the same amount of responses want %d got %d", N, count)
-	}
+	checkStatusCode(t, resCH, N)
 
 	// check that we can hit the main route now again correctly
 	bodyData := strings.Repeat("A", 1023) + "\n"
 	buf := bytes.NewBufferString(bodyData)
 	sr := NewSlowReader(buf, 1*time.Microsecond)
+
 	req, err := http.NewRequest("PUT", p.URL, sr)
 	if err != nil {
 		t.Logf("Failed to create request: %v", err)
@@ -414,8 +372,8 @@ func TestBackendTimeoutWithSlowBodyWriterShadow(t *testing.T) {
 	defer slowBackend.Close()
 
 	doc := fmt.Sprintf(`
-main: Path("/") -> fifo(5000, 20, "1s") -> teeLoopback("tag") -> "%s";
-shadow: Path("/") && Tee("tag") -> fifo(5, 40, "150ms") -> backendTimeout("20ms") -> "%s";`,
+main: PathSubtree("/") -> fifo(5000, 20, "1s") -> teeLoopback("tag") -> "%s";
+shadow: PathSubtree("/") && Tee("tag") -> fifo(5, 40, "150ms") -> backendTimeout("20ms") -> "%s";`,
 		backend.URL, slowBackend.URL)
 
 	fr := builtin.MakeRegistry()
@@ -457,8 +415,6 @@ shadow: Path("/") && Tee("tag") -> fifo(5, 40, "150ms") -> backendTimeout("20ms"
 	defer p.Close()
 
 	N := 500 //500000
-	wg := sync.WaitGroup{}
-	sometimes := rate.Sometimes{First: 3, Interval: time.Second}
 	resCH := make(chan int, N)
 	client := skpnet.NewClient(skpnet.Options{
 		Timeout:               80 * time.Millisecond,
@@ -470,51 +426,10 @@ shadow: Path("/") && Tee("tag") -> fifo(5, 40, "150ms") -> backendTimeout("20ms"
 	})
 	defer client.Close()
 	defer client.CloseIdleConnections()
-
-	for range N {
-		time.Sleep(100 * time.Microsecond)
-		wg.Add(1)
-		go func(ch chan<- int) {
-			defer wg.Done()
-			bodyData := strings.Repeat("A", 1023) + "\n"
-			buf := bytes.NewBufferString(bodyData)
-			sr := NewSlowReader(buf, 1*time.Microsecond)
-			req, err := http.NewRequest("PUT", p.URL, sr)
-			if err != nil {
-				t.Logf("Failed to create request: %v", err)
-				return
-			}
-
-			rsp, err := client.Do(req)
-			if err != nil {
-				t.Logf("Failed to get response: %v", err)
-				return
-			}
-			if rsp.StatusCode != 200 {
-				sometimes.Do(func() { t.Logf("response: %s", rsp.Status) })
-			}
-			io.Copy(io.Discard, rsp.Body)
-			ch <- rsp.StatusCode
-		}(resCH)
-	}
-	wg.Wait()
-
+	sendRequests(t, N, p, client, resCH)
 	logFifoMetrics(t, mockMetrics)
-
 	close(resCH)
-	sometimes = rate.Sometimes{First: 3, Interval: time.Second}
-
-	count := 0
-	for code := range resCH {
-		if code != 200 {
-			sometimes.Do(func() { t.Errorf("request wrong status: %d", code) })
-		}
-		count++
-	}
-	if count != N {
-		// expected
-		t.Logf("Failed to get the same amount of responses want %d got %d", N, count)
-	}
+	checkStatusCode(t, resCH, N)
 
 	// check that we can hit the main route now again correctly
 	bodyData := strings.Repeat("A", 1023) + "\n"
@@ -550,19 +465,6 @@ shadow: Path("/") && Tee("tag") -> fifo(5, 40, "150ms") -> backendTimeout("20ms"
 
 }
 
-func logFifoMetrics(t *testing.T, mockMetrics *metricstest.MockMetrics) {
-	for _, route := range []string{"main", "shadow"} {
-		for _, kfmt := range []string{"fifo.%s.active", "fifo.%s.queued", "fifo.%s.error.full", "fifo.%s.error.other", "fifo.%s.error.timeout"} {
-			k := fmt.Sprintf(kfmt, route)
-			if v, ok := mockMetrics.Gauge(k); ok {
-				t.Logf("metric %q: %v", k, v)
-			} else {
-				t.Logf("metric not found: %q", k)
-			}
-		}
-	}
-}
-
 func TestBackendTimeoutWithConnectTimingOutShadow(t *testing.T) {
 	proxyLog := proxy.NewTestLog()
 	defer proxyLog.Close()
@@ -581,7 +483,7 @@ func TestBackendTimeoutWithConnectTimingOutShadow(t *testing.T) {
 	defer backend.Close()
 
 	slowBackend := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		//println("HERE:", r.URL.Path)
+		//println("We should not be HERE:", r.URL.Path)
 		sr := NewSlowReader(r.Body, 1*time.Microsecond)
 		io.ReadAll(sr)
 		w.WriteHeader(599)
@@ -643,8 +545,6 @@ shadow: PathSubtree("/") && Tee("tag") -> fifo(5, 40, "150ms") -> backendTimeout
 	defer p.Close()
 
 	N := 500 //500000
-	wg := sync.WaitGroup{}
-	sometimes := rate.Sometimes{First: 3, Interval: time.Second}
 	resCH := make(chan int, N)
 	client := skpnet.NewClient(skpnet.Options{
 		Timeout:               80 * time.Millisecond,
@@ -656,6 +556,76 @@ shadow: PathSubtree("/") && Tee("tag") -> fifo(5, 40, "150ms") -> backendTimeout
 	})
 	defer client.Close()
 	defer client.CloseIdleConnections()
+	sendRequests(t, N, p, client, resCH)
+	logFifoMetrics(t, mockMetrics)
+	close(resCH)
+	checkStatusCode(t, resCH, N)
+
+	l.(*slowAcceptListener).Delay(time.Microsecond)
+
+	// check that we can hit the main route now again correctly
+	bodyData := strings.Repeat("A", 1023) + "\n"
+	buf := bytes.NewBufferString(bodyData)
+	sr := NewSlowReader(buf, 1*time.Microsecond)
+
+	req, err := http.NewRequest("PUT", p.URL, sr)
+	if err != nil {
+		t.Logf("Failed to create request: %v", err)
+		return
+	}
+	rsp, err := client.Do(req)
+	if err != nil {
+		t.Fatalf("Failed to get response: %v", err)
+	}
+	io.Copy(io.Discard, rsp.Body)
+	rsp.Body.Close()
+	if rsp.StatusCode != 200 {
+		t.Fatalf("Failed to get 200 response code: %d", rsp.StatusCode)
+	} else {
+		t.Logf("response code: %d", rsp.StatusCode)
+	}
+
+	// if err := proxyLog.WaitFor("failed to execute loopback request: dialing failed false: context deadline exceeded", time.Second); err != nil {
+	// 	t.Fatalf("Failed to get expected error: %v", err)
+	// } else {
+	// 	t.Log("Found error log")
+	// }
+}
+
+func checkStatusCode(t *testing.T, resCH chan int, N int) {
+	t.Helper()
+	sometimes := rate.Sometimes{First: 3, Interval: time.Second}
+	count := 0
+	for code := range resCH {
+		if code != 200 {
+			sometimes.Do(func() { t.Errorf("request wrong status: %d", code) })
+		}
+		count++
+	}
+	if count != N {
+		// expected
+		t.Logf("Failed to get the same amount of responses want %d got %d", N, count)
+	}
+}
+
+func logFifoMetrics(t *testing.T, mockMetrics *metricstest.MockMetrics) {
+	t.Helper()
+	for _, route := range []string{"main", "shadow"} {
+		for _, kfmt := range []string{"fifo.%s.active", "fifo.%s.queued", "fifo.%s.error.full", "fifo.%s.error.other", "fifo.%s.error.timeout"} {
+			k := fmt.Sprintf(kfmt, route)
+			if v, ok := mockMetrics.Gauge(k); ok {
+				t.Logf("metric %q: %v", k, v)
+			} else {
+				t.Logf("metric not found: %q", k)
+			}
+		}
+	}
+}
+
+func sendRequests(t *testing.T, N int, p *proxytest.TestProxy, client *skpnet.Client, resCH chan int) {
+	t.Helper()
+	wg := sync.WaitGroup{}
+	sometimes := rate.Sometimes{First: 3, Interval: time.Second}
 
 	for i := range N {
 		time.Sleep(100 * time.Microsecond)
@@ -684,49 +654,4 @@ shadow: PathSubtree("/") && Tee("tag") -> fifo(5, 40, "150ms") -> backendTimeout
 		}(resCH)
 	}
 	wg.Wait()
-
-	logFifoMetrics(t, mockMetrics)
-
-	close(resCH)
-	sometimes = rate.Sometimes{First: 3, Interval: time.Second}
-
-	count := 0
-	for code := range resCH {
-		if code != 200 {
-			sometimes.Do(func() { t.Errorf("request wrong status: %d", code) })
-		}
-		count++
-	}
-	if count != N {
-		// expected
-		t.Logf("Failed to get the same amount of responses want %d got %d", N, count)
-	}
-
-	l.(*slowAcceptListener).Delay(time.Microsecond)
-	// check that we can hit the main route now again correctly
-	bodyData := strings.Repeat("A", 1023) + "\n"
-	buf := bytes.NewBufferString(bodyData)
-	sr := NewSlowReader(buf, 1*time.Microsecond)
-	req, err := http.NewRequest("PUT", p.URL, sr)
-	if err != nil {
-		t.Logf("Failed to create request: %v", err)
-		return
-	}
-	rsp, err := client.Do(req)
-	if err != nil {
-		t.Fatalf("Failed to get response: %v", err)
-	}
-	io.Copy(io.Discard, rsp.Body)
-	rsp.Body.Close()
-	if rsp.StatusCode != 200 {
-		t.Fatalf("Failed to get 200 response code: %d", rsp.StatusCode)
-	} else {
-		t.Logf("response code: %d", rsp.StatusCode)
-	}
-
-	// if err := proxyLog.WaitFor("failed to execute loopback request: dialing failed false: context deadline exceeded", time.Second); err != nil {
-	// 	t.Fatalf("Failed to get expected error: %v", err)
-	// } else {
-	// 	t.Log("Found error log")
-	// }
 }
