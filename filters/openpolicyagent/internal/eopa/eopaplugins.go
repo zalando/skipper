5c700894f08165590c87518d7efa0bfd6198df5a
nuwandi-wickramasinghe_zse <nuwandi.wickramasinghe@zalando.de>
2025-11-19 14:54:02 +0100

    remove eopa data plugin
    Signed-off-by: nuwandi-wickramasinghe_zse <nuwandi.wickramasinghe@zalando.de>

--- filters/openpolicyagent/internal/eopa/eopaplugins.go
+++ filters/openpolicyagent/internal/eopa/eopaplugins.go
@@ -8,7 +8,8 @@ import (
 
 func All() map[string]plugins.Factory {
 	return map[string]plugins.Factory{
-		//data.Name:       data.Factory(), // disabled as we currently do not use this plugin and due to the unexpected impact on the opa body parsing filter.
+		// data plugin is commented out as we currently do not use this and due to the unexpected impact on the opa body parsing filter.
+		//data.Name:       data.Factory(),
 		dl.DLPluginName: dl.Factory(),
 	}
 }
