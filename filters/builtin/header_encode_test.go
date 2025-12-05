f930dd1b23c400754b30c36445c31e0f8d44ee47
Sandor Szücs <sandor.szuecs@zalando.de>
2025-12-05 09:39:53 +0100

    fix: linter finding
    Signed-off-by: Sandor Szücs <sandor.szuecs@zalando.de>

--- filters/builtin/header_encode_test.go
+++ filters/builtin/header_encode_test.go
@@ -24,184 +24,175 @@ func TestEncodeRequestHeaderNameUknown(t *testing.T) {
 func TestCreateFilterEncodeRequestHeader(t *testing.T) {
 	for _, tt := range []struct {
 		name    string
-		sargs   []interface{}
+		args    []interface{}
 		wantErr error
 	}{
 		{
 			name:    "ISO8859_1",
-			sargs:   []interface{}{"X-foo", "ISO8859_1"},
+			args:    []interface{}{"X-foo", "ISO8859_1"},
 			wantErr: nil,
 		},
 		{
 			name:    "ISO8859_10",
-			sargs:   []interface{}{"X-foo", "ISO8859_10"},
+			args:    []interface{}{"X-foo", "ISO8859_10"},
 			wantErr: nil,
 		},
 		{
 			name:    "ISO8859_13",
-			sargs:   []interface{}{"X-foo", "ISO8859_13"},
+			args:    []interface{}{"X-foo", "ISO8859_13"},
 			wantErr: nil,
 		},
 		{
 			name:    "ISO8859_14",
-			sargs:   []interface{}{"X-foo", "ISO8859_14"},
+			args:    []interface{}{"X-foo", "ISO8859_14"},
 			wantErr: nil,
 		},
 		{
 			name:    "ISO8859_15",
-			sargs:   []interface{}{"X-foo", "ISO8859_15"},
+			args:    []interface{}{"X-foo", "ISO8859_15"},
 			wantErr: nil,
 		},
 		{
 			name:    "ISO8859_16",
-			sargs:   []interface{}{"X-foo", "ISO8859_16"},
+			args:    []interface{}{"X-foo", "ISO8859_16"},
 			wantErr: nil,
 		},
 		{
 			name:    "ISO8859_2",
-			sargs:   []interface{}{"X-foo", "ISO8859_2"},
+			args:    []interface{}{"X-foo", "ISO8859_2"},
 			wantErr: nil,
 		},
 		{
 			name:    "ISO8859_3",
-			sargs:   []interface{}{"X-foo", "ISO8859_3"},
+			args:    []interface{}{"X-foo", "ISO8859_3"},
 			wantErr: nil,
 		},
 		{
 			name:    "ISO8859_4",
-			sargs:   []interface{}{"X-foo", "ISO8859_4"},
+			args:    []interface{}{"X-foo", "ISO8859_4"},
 			wantErr: nil,
 		},
 		{
 			name:    "ISO8859_5",
-			sargs:   []interface{}{"X-foo", "ISO8859_5"},
+			args:    []interface{}{"X-foo", "ISO8859_5"},
 			wantErr: nil,
 		},
 		{
 			name:    "ISO8859_6",
-			sargs:   []interface{}{"X-foo", "ISO8859_6"},
+			args:    []interface{}{"X-foo", "ISO8859_6"},
 			wantErr: nil,
 		},
 		{
 			name:    "ISO8859_7",
-			sargs:   []interface{}{"X-foo", "ISO8859_7"},
+			args:    []interface{}{"X-foo", "ISO8859_7"},
 			wantErr: nil,
 		},
 		{
 			name:    "ISO8859_8",
-			sargs:   []interface{}{"X-foo", "ISO8859_8"},
+			args:    []interface{}{"X-foo", "ISO8859_8"},
 			wantErr: nil,
 		},
 		{
 			name:    "ISO8859_9",
-			sargs:   []interface{}{"X-foo", "ISO8859_9"},
+			args:    []interface{}{"X-foo", "ISO8859_9"},
 			wantErr: nil,
 		},
 		{
 			name:    "KOI8R",
-			sargs:   []interface{}{"X-foo", "KOI8R"},
+			args:    []interface{}{"X-foo", "KOI8R"},
 			wantErr: nil,
 		},
 		{
 			name:    "KOI8U",
-			sargs:   []interface{}{"X-foo", "KOI8U"},
+			args:    []interface{}{"X-foo", "KOI8U"},
 			wantErr: nil,
 		},
 		{
 			name:    "Macintosh",
-			sargs:   []interface{}{"X-foo", "Macintosh"},
+			args:    []interface{}{"X-foo", "Macintosh"},
 			wantErr: nil,
 		},
 		{
 			name:    "MacintoshCyrillic",
-			sargs:   []interface{}{"X-foo", "MacintoshCyrillic"},
+			args:    []interface{}{"X-foo", "MacintoshCyrillic"},
 			wantErr: nil,
 		},
 		{
 			name:    "Windows1250",
-			sargs:   []interface{}{"X-foo", "Windows1250"},
+			args:    []interface{}{"X-foo", "Windows1250"},
 			wantErr: nil,
 		},
 		{
 			name:    "Windows1251",
-			sargs:   []interface{}{"X-foo", "Windows1251"},
+			args:    []interface{}{"X-foo", "Windows1251"},
 			wantErr: nil,
 		},
 		{
 			name:    "Windows1252",
-			sargs:   []interface{}{"X-foo", "Windows1252"},
+			args:    []interface{}{"X-foo", "Windows1252"},
 			wantErr: nil,
 		},
 		{
 			name:    "Windows1253",
-			sargs:   []interface{}{"X-foo", "Windows1253"},
+			args:    []interface{}{"X-foo", "Windows1253"},
 			wantErr: nil,
 		},
 		{
 			name:    "Windows1254",
-			sargs:   []interface{}{"X-foo", "Windows1254"},
+			args:    []interface{}{"X-foo", "Windows1254"},
 			wantErr: nil,
 		},
 		{
 			name:    "Windows1255",
-			sargs:   []interface{}{"X-foo", "Windows1255"},
+			args:    []interface{}{"X-foo", "Windows1255"},
 			wantErr: nil,
 		},
 		{
 			name:    "Windows1256",
-			sargs:   []interface{}{"X-foo", "Windows1256"},
+			args:    []interface{}{"X-foo", "Windows1256"},
 			wantErr: nil,
 		},
 		{
 			name:    "Windows1257",
-			sargs:   []interface{}{"X-foo", "Windows1257"},
+			args:    []interface{}{"X-foo", "Windows1257"},
 			wantErr: nil,
 		},
 		{
 			name:    "Windows1258",
-			sargs:   []interface{}{"X-foo", "Windows1258"},
+			args:    []interface{}{"X-foo", "Windows1258"},
 			wantErr: nil,
 		},
 		{
 			name:    "Windows874",
-			sargs:   []interface{}{"X-foo", "Windows874"},
+			args:    []interface{}{"X-foo", "Windows874"},
 			wantErr: nil,
 		},
 		{
 			name:    "unknown",
-			sargs:   []interface{}{"X-foo", "unknown"},
+			args:    []interface{}{"X-foo", "unknown"},
 			wantErr: filters.ErrInvalidFilterParameters,
 		},
 		{
 			name:    "error not enough arguments",
-			sargs:   []interface{}{"X-foo"},
+			args:    []interface{}{"X-foo"},
 			wantErr: filters.ErrInvalidFilterParameters,
 		},
 		{
 			name:    "type error key",
-			sargs:   []interface{}{5, "X-foo"},
+			args:    []interface{}{5, "X-foo"},
 			wantErr: filters.ErrInvalidFilterParameters,
 		},
 		{
 			name:    "type error value",
-			sargs:   []interface{}{"X-foo", 5},
+			args:    []interface{}{"X-foo", 5},
 			wantErr: filters.ErrInvalidFilterParameters,
 		}} {
 		t.Run(tt.name, func(t *testing.T) {
-			args := make([]interface{}, 0)
-			for _, v := range tt.sargs {
-				if s, ok := v.(interface{}); ok {
-					args = append(args, s)
-				} else {
-					t.Fatalf("Failed to convert %q to interface{}", v)
-				}
-			}
-
 			for specT := range []encodeTyp{requestEncoder, responseEncoder} {
 				spec := encodeHeaderSpec{typ: encodeTyp(specT)}
-				f, err := spec.CreateFilter(args)
+				f, err := spec.CreateFilter(tt.args)
 				if err != tt.wantErr {
-					t.Fatalf("Failed to create filter with args %v want %v, got: %v", args, tt.wantErr, err)
+					t.Fatalf("Failed to create filter with args %v want %v, got: %v", tt.args, tt.wantErr, err)
 				}
 				if tt.wantErr == nil && f == nil {
 					t.Fatal("Failed to get filter, got nil instead")
