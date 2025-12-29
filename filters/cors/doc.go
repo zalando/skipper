9086aaf701bab55302cc8be65fc78fda47ce985c
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-12-29 10:01:39 -0500

    spelling: otherwise,
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- filters/cors/doc.go
+++ filters/cors/doc.go
@@ -4,7 +4,7 @@ Package cors implements the origin header for CORS.
 # How It Works
 
 The filter accepts an optional variadic list of acceptable origin parameters. If the input argument list is empty, the header
-will always be set to '*' which means any origin is acceptable. Otherwise the header is only set if the request contains
+will always be set to '*' which means any origin is acceptable. Otherwise, the header is only set if the request contains
 an Origin header and its value matches one of the elements in the input list. The header is only set on the response.
 
 Usage
