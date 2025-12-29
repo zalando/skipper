a0cab343c3eccc2936213480f8da0f0fd24164b7
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-12-29 09:48:00 -0500

    spelling: above
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- proxy/doc.go
+++ proxy/doc.go
@@ -124,7 +124,7 @@ When configured, skipper can use circuit breakers for the backend
 requests. It asks the registry for a matching circuit breaker for
 every request, and if there is any, checks if it is closed before
 sending out the backend request. On connection errors and responses
-with a status code higher than 499, it reports a failure to the
+with a status code above 499, it reports a failure to the
 current breaker; otherwise, it reports a success.
 
 For details, see: https://pkg.go.dev/github.com/zalando/skipper/circuit.
