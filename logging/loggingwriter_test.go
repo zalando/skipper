8b5543a2aa7d9dcb5427a6080fbdc07aba6d6024
Sandor Szuecs <sandor.szuecs@zalando.de>
2026-02-28 22:27:43 +0100

    test: loggingwriter hijacker test
    Signed-off-by: Sandor Szuecs <sandor.szuecs@zalando.de>

--- logging/loggingwriter_test.go
+++ logging/loggingwriter_test.go
@@ -35,6 +35,12 @@ func TestWritesAndStoresStatusCode(t *testing.T) {
 	if w.GetCode() != http.StatusTeapot {
 		t.Error("failed to get status code")
 	}
+
+	w.WriteHeader(0)
+	if w.GetCode() != http.StatusOK {
+		t.Errorf("Failed to get default status %d", http.StatusOK)
+	}
+
 }
 
 func TestReturnsUnderlyingHeader(t *testing.T) {
@@ -54,4 +60,39 @@ func TestFlushesPartialPayload(t *testing.T) {
 	if !rr.Flushed {
 		t.Error("failed to flush underlying writer")
 	}
+}
+
+func TestHijack(t *testing.T) {
+	rr := httptest.NewRecorder()
+	w := NewLoggingWriter(rr)
+	w.Write([]byte("Hello, world!"))
+	_, _, err := w.Hijack()
+	if err == nil {
+		t.Fatal("Failed to get hijack error")
+	}
+
+	backend := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		hijacker, ok := w.(http.Hijacker)
+		if !ok {
+			t.Fatal("Failed to cast responsewriter to Hijacker")
+		}
+		conn, rw, err := hijacker.Hijack()
+		if err != nil {
+			t.Fatalf("Failed to get hijacker: %v", err)
+		}
+		defer conn.Close()
+
+		rw.Write([]byte("foo"))
+		buf := make([]byte, 0, 3)
+		n, err := conn.Read(buf)
+		if err != nil {
+			t.Fatalf("Failed to read from conn: %v", err)
+		}
+		if n != 3 {
+			t.Fatalf("Failed to read 3 bytes, read %d bytes", n)
+		}
+	}))
+	defer backend.Close()
+
 }
