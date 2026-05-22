18549ac63450e37d88743d04aa8ebdc233129153
Abdelghafour Mourchid <Abdelghafour01@users.noreply.github.com>
2026-05-22 11:33:56 +0000

    Add decompressRequest filter for handling compressed request bodies using existing decoders (gzip, deflate, br, zstd)
    Signed-off-by: Abdelghafour Mourchid <Abdelghafour01@users.noreply.github.com>

--- filters/filters.go
+++ filters/filters.go
@@ -251,6 +251,7 @@ const (
 	StatusName                                 = "status"
 	CompressName                               = "compress"
 	DecompressName                             = "decompress"
+	DecompressRequestName                      = "decompressRequest"
 	SetQueryName                               = "setQuery"
 	DropQueryName                              = "dropQuery"
 	InlineContentName                          = "inlineContent"
