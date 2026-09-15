7f06bfdcb3ccb4c488de9e79993f5902ad359a5f
Sandor Szücs <sandor.szuecs@zalando.de>
2026-09-15 12:09:04 +0200

    revert: opa startup simplification
    reverts 00dadfc329b8fe71f51a6cb28c289c394c4510fd
    causing TestServerResponseFilter
    
    Signed-off-by: Sandor Szücs <sandor.szuecs@zalando.de>

--- filters/openpolicyagent/openpolicyagent.go
+++ filters/openpolicyagent/openpolicyagent.go
@@ -821,11 +821,8 @@ func (registry *OpenPolicyAgentRegistry) new(store storage.Store, bundleName str
 }
 
 func allPluginsReady(allPluginsStatus map[string]*plugins.Status, pluginNames ...string) bool {
-	if len(pluginNames) == 0 {
-		return true
-	}
 	for pluginName, status := range allPluginsStatus {
-		if !slices.Contains(pluginNames, pluginName) {
+		if pluginNames != nil && !slices.Contains(pluginNames, pluginName) {
 			continue
 		}
 
