6c0bbd263e87c03601add66f56cdca1e97e2cc4e
Mustafa Abdelrahman <mustafa.abdelrahman@zalando.de>
2026-09-25 15:50:54 +0200

    letsencrypt: fix wildcard match
    HostWhiteList only allow exact domain match https://pkg.go.dev/golang.org/x/crypto/acme/autocert#HostWhitelist
    
    Signed-off-by: Mustafa Abdelrahman <mustafa.abdelrahman@zalando.de>

--- net/letsencrypt_test.go
+++ net/letsencrypt_test.go
@@ -128,3 +128,35 @@ func TestLetsencrypt(t *testing.T) {
 	defer li.Close()
 	t.Logf("listener %v", li.Addr())
 }
+
+func TestLetsencryptWildcardHostPolicy(t *testing.T) {
+	le := NewLetsencrypt(
+		&InmemoryCache{},
+		"skipper@example.org",
+		"https://acme-staging-v02.api.letsencrypt.org/directory",
+		"skipper-test TestLetsencryptWildcardHostPolicy",
+		[]string{"*.example.org", "example.com"},
+	)
+	defer le.Close()
+
+	for _, tt := range []struct {
+		name    string
+		host    string
+		allowed bool
+	}{
+		{"wildcard matches single-label subdomain", "foo.example.org", true},
+		{"wildcard does not match wrong bare domain", "example.org", false},
+		{"wildcard does not match multi-label subdomain", "a.b.example.org", false},
+		{"exact domain matches", "example.com", true},
+		{"exact domain does not match subdomain", "foo.example.com", false},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			err := le.manager.HostPolicy(context.Background(), tt.host)
+			if tt.allowed {
+				require.NoError(t, err, "host %q should be allowed", tt.host)
+			} else {
+				require.Error(t, err, "host %q should be rejected", tt.host)
+			}
+		})
+	}
+}
